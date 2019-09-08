72549b49b406b9834f9b12c968566a487d904e26
pixelqm <303176530@qq.com>
2019-09-08 23:37:11 +0800

    引入 element、

--- QMPlusVuePage/src/view/login/login.vue
+++ QMPlusVuePage/src/view/login/login.vue
@@ -1,7 +1,12 @@
 <template>
-    <div>
-        登录
-    </div>
+        <el-container class="login-box">
+            <el-header>登录</el-header>
+                <el-main>输入框在这里
+                    <el-button>
+                        登录
+                    </el-button>
+                </el-main>
+        </el-container>
 </template>
 
 <script>
@@ -10,6 +15,8 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .login-box{
+        background: #409EFF
+    }
 </style>

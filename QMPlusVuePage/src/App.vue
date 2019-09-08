6ac4e72981e63bc15f99c1f20787887e19b92c82
pixelqm <303176530@qq.com>
2019-09-08 23:37:11 +0800

    引入 element、

--- QMPlusVuePage/src/App.vue
+++ QMPlusVuePage/src/App.vue
@@ -14,13 +14,7 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+// 引入初始化样式
+  @import '@/style/main.scss'
 </style>
--- QMPlusVuePage/src/main.js
+++ QMPlusVuePage/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+// 引入element
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+// 引入封装的router
 import router from '@/router/index'
 
 Vue.config.productionTip = false

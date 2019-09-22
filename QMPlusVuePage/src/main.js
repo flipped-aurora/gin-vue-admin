717ccb7d41f77ac36745fea5cbecba8ab1319f8b
pixel <303176530@qq.com>
2019-09-22 17:09:38 +0800

    文档修复

--- QMPlusVuePage/src/main.js
+++ QMPlusVuePage/src/main.js
@@ -5,18 +5,19 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 // 引入封装的router
+import router from '@/router/index'
 
+// canvas背景插件
 import vueParticleLine from 'vue-particle-line'
 import 'vue-particle-line/dist/vue-particle-line.css'
 Vue.use(vueParticleLine)
-    // canvas背景插件
 
-import router from '@/router/index'
+
 import '@/permission'
 import { store } from '@/store/index'
 Vue.config.productionTip = false
 
-
+// 路由守卫
 import Bus from '@/utils/bus.js'
 Vue.use(Bus)
 new Vue({

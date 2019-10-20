de03d1e82f57ada045e969626b7421b6b6cf6a6e
pixel <303176530@qq.com>
2019-10-20 11:42:56 +0800

    加入示例模板文件

---
+++ QMPlusVuePage/src/view/example/excel/excel.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        excel导入导出
+    </div>
+</template>
+<script>
+export default {
+    name:'Excel'
+}
+</script>
---
+++ QMPlusVuePage/src/view/example/rte/rte.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        多种富文本编辑器
+    </div>
+</template>
+<script>
+export default {
+    name:'Rte'
+}
+</script>

0b7eb989d3703ad577ba88522ca8945c05a66b72
klausY <emosick@qq.com>
2019-10-25 17:18:35 +0800

    form 示例

--- QMPlusVuePage/src/view/example/form/form.vue
+++ QMPlusVuePage/src/view/example/form/form.vue
@@ -1,10 +1,103 @@
 <template>
     <div>
-        多种表单示例
+        <el-form ref="form" :model="form" label-width="80px">
+            <el-form-item label="活动名称">
+                <el-input v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-select label="活动区域" v-model="form.region" placeholder="请选择活动区域">
+                    <el-option label="上海" value="shanghai"></el-option>
+                    <el-option label="北京" value="beijing"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="活动时间">
+                <el-col :span="11">
+                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+                </el-col>
+                <el-col class="line" style="text-align: center" :span="2">——</el-col>
+                <el-col :span="11">
+                    <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+                </el-col>
+            </el-form-item>
+            <el-form-item label="即时配送">
+                <el-switch v-model="form.delivery"></el-switch>
+            </el-form-item>
+            <el-form-item label="活动性质">
+                <el-checkbox-group v-model="form.type">
+                    <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
+                    <el-checkbox label="地推活动" name="type"></el-checkbox>
+                    <el-checkbox label="线下主题活动" name="type"></el-checkbox>
+                    <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+                </el-checkbox-group>
+            </el-form-item>
+            <el-form-item label="特殊资源">
+                <el-radio-group v-model="form.resource">
+                    <el-radio label="线上品牌商赞助"></el-radio>
+                    <el-radio label="线下场地免费"></el-radio>
+                </el-radio-group>
+            </el-form-item>
+            <el-form-item label="活动形式">
+                <el-input type="textarea" v-model="form.desc"></el-input>
+            </el-form-item>
+            <el-form-item label="穿梭框">
+                <el-transfer
+                        filterable
+                        :filter-method="filterMethod"
+                        filter-placeholder="请输入城市拼音"
+                        v-model="value"
+                        :data="data">
+                </el-transfer>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">立即创建</el-button>
+                <el-button>取消</el-button>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
 <script>
 export default {
-    name:'Form'
+
+    data() {
+        const generateData = _ => {
+            const data = [];
+            const cities = ['上海', '北京', '广州', '深圳', '南京', '西安', '成都'];
+            const pinyin = ['shanghai', 'beijing', 'guangzhou', 'shenzhen', 'nanjing', 'xian', 'chengdu'];
+            cities.forEach((city, index) => {
+                data.push({
+                label: city,
+                key: index,
+                pinyin: pinyin[index]
+            });
+        });
+            return data;
+        };
+        return {
+            form: {
+                name: '',
+                region: '',
+                date1: '',
+                date2: '',
+                delivery: false,
+                type: [],
+                resource: '',
+                desc: ''
+            },
+            data: generateData(),
+            value: [],
+            filterMethod(query, item) {
+                return item.pinyin.indexOf(query) > -1;
+            },
+            methods : {
+                onSubmit() {
+                    this.$message({
+                        message: '创建成功',
+                        type: 'success'
+                    });
+                }
+            }
+        }
+    }
+
 }
 </script>

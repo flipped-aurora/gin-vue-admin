20156cb5a016b4c0ffb4b5c77bdea5e43d84342e
klausY <emosick@qq.com>
2019-10-30 17:40:33 +0800

    css样式bug修复

--- QMPlusVuePage/src/view/example/rte/rte.vue
+++ QMPlusVuePage/src/view/example/rte/rte.vue
@@ -37,12 +37,5 @@
 </script>
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+
 </style>

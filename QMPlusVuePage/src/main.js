a9d47143e8996ee009bb9d84cf9ca8fb31326de6
klausY <emosick@qq.com>
2019-11-10 15:45:04 +0800

    form表单示例

--- QMPlusVuePage/src/main.js
+++ QMPlusVuePage/src/main.js
@@ -12,13 +12,19 @@ import vueParticleLine from 'vue-particle-line'
 import 'vue-particle-line/dist/vue-particle-line.css'
 Vue.use(vueParticleLine)
 
-//富文本插件
+// 富文本插件
 import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 
 Vue.use(VueQuillEditor)
+
+// markdown插件
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+
+Vue.use(mavonEditor)
 
 import '@/permission'
 import { store } from '@/store/index'

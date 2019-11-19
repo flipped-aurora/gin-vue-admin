2bc7617fd2f2b9d76db4cd208bbad89694e971d0
pixel <303176530@qq.com>
2019-11-19 09:46:50 +0800

    工作流表初步设计

--- QMPlusServer/model/dbModel/worfFlow.go
+++ QMPlusServer/model/dbModel/worfFlow.go
@@ -7,38 +7,43 @@ import (
 
 type Application struct {
 	gorm.Model
-	WorkFlowID	string  // 所属工作流
-	WorkFlowStepInfoID string // 当前节点ID
-	ApplicationName string  // 申请人姓名
-	ApplicationCause	string // 请假原因
+	WorkFlowID           string    // 所属工作流ID
+	WorkFlowStepInfoID   string    // 当前节点ID
+	ApplicationName      string    // 申请人姓名
+	ApplicationCause     string    // 请假原因
 	ApplicationStartData time.Time // 请假开始日期
-	ApplicationEndData time.Time // 请假开始日期
+	ApplicationEndData   time.Time // 请假开始日期
 
 }
 
+// 流转表
 type ApplicationWorkFlowProcess struct {
 	gorm.Model
-	ApplicationId uint  // 当前工作流所属申请的ID
-	CurrentNode string   // 当前进度节点
-	HistoricalNode string  //上一个进度节点
-	CurrentUser string   // 当前进度操作人
+	ApplicationId  uint // 当前工作流所属申请的ID
+	WorkflowID     uint
+	CurrentNode    string // 当前进度节点
+	HistoricalNode string //上一个进度节点
+	CurrentUser    string // 当前进度操作人
 	HistoricalUser string // 上一个进度的操作人
-	State bool  // 状态 是否是正在进行的状态
+	State          bool   // 状态 是否是正在进行的状态
 }
 
+//工作流属性表
 type Workflow struct {
 	gorm.Model
-	WorkflowName string // 工作流名称
-	WorkflowDescription string //工作流描述
-	WorkflowStep []WorkflowStepInfo //工作流步骤
+	WorkflowNickName    string             // 工作流名称
+	WorkflowName        string             // 工作流英文id
+	WorkflowDescription string             //工作流描述
+	WorkflowStep        []WorkflowStepInfo //工作流步骤
 }
 
+// 工作流状态表
 type WorkflowStepInfo struct {
 	gorm.Model
-	WorkflowID uint
-	IsStrat	bool
-	StepName string
-	StepNo	float64
-	StepAuthorityId string
-	IsEnd bool
+	WorkflowID      uint    // 所属工作流ID
+	IsStrat         bool    // 是否是开始流节点
+	StepName        string  // 工作流名称
+	StepNo          float64 // 步骤id （第几步）
+	StepAuthorityId string  // 操作者级别id
+	IsEnd           bool    // 是否是完结流节点
 }

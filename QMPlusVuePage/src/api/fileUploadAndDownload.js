137143d91efab3da874ad0f98d51c1f32a31ae22
pixel <303176530@qq.com>
2019-11-24 11:13:55 +0800

    增加了文件删除功能和前端日期格式化功能

--- QMPlusVuePage/src/api/fileUploadAndDownload.js
+++ QMPlusVuePage/src/api/fileUploadAndDownload.js
@@ -14,3 +14,18 @@ export const getFileList = (data) => {
         data
     })
 }
+
+// @Tags FileUploadAndDownload
+// @Summary 删除文件
+// @Security ApiKeyAuth
+// @Produce  application/json
+// @Param data body dbModel.FileUploadAndDownload true "传入文件里面id即可"
+// @Success 200 {string} json "{"success":true,"data":{},"msg":"返回成功"}"
+// @Router /fileUploadAndDownload/deleteFile [post]
+export const deleteFile = (data) => {
+    return service({
+        url: "/fileUploadAndDownload/deleteFile",
+        method: "post",
+        data
+    })
+}

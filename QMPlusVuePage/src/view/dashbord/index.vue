cafb2d4dd2c1cfcef59c55bf0b315172fba057f6
pixel <303176530@qq.com>
2019-11-27 11:30:28 +0800

    增加 开发者联系方式

--- QMPlusVuePage/src/view/dashbord/index.vue
+++ QMPlusVuePage/src/view/dashbord/index.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
-    <h2 style="text-align:center">
-    gin-vue-admin by 奇淼 And Spike
-    </h2>
-    <h4 style="text-align:center">
-      微信：shouzi_1994
-    </h4>
-    
-    <Animition />
+    <h2 style="text-align:center">gin-vue-admin by 奇淼 And krank666</h2>
+    <h2 style="text-align:center">点击动画获取联系方式</h2>
+
+    <el-drawer
+      :before-close="handleClose"
+      :direction="direction"
+      :visible.sync="drawer"
+      title="联系方式"
+    >
+      <div style="text-align:center;margin-top:30px;">
+        <img src="http://qmplusimg.henrongyi.top/yx.jpg" width="180" />
+        <H3>krank666微信</H3>
+      </div>
+      <div style="text-align:center;margin-top:30px;">
+        <img src="http://qmplusimg.henrongyi.top/jjz.jpg" width="180" />
+        <H3>Mr.奇淼微信</H3>
+      </div>
+    </el-drawer>
+    <div @click="drawer = true">
+      <Animition />
+    </div>
   </div>
 </template>
 
@@ -16,6 +29,11 @@ import { mapGetters } from 'vuex'
 import Animition from '@/view/dashbord/component/animition.vue'
 export default {
   name: 'Dashbord',
+  data() {
+    return {
+      drawer: false
+    }
+  },
   computed: {
     ...mapGetters('user', ['userInfo'])
   },
@@ -26,5 +44,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 </style>

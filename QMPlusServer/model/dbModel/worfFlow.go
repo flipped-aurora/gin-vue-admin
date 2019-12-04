0f43e73770f188e937920ef432123c4cf3d0f8a4
pixel <303176530@qq.com>
2019-12-04 11:17:15 +0800

    工作流定义前端页面bug处理

--- QMPlusServer/model/dbModel/worfFlow.go
+++ QMPlusServer/model/dbModel/worfFlow.go
@@ -8,21 +8,21 @@ import (
 //工作流属性表
 type Workflow struct {
 	gorm.Model
-	WorkflowNickName    string             // 工作流名称
-	WorkflowName        string             // 工作流英文id
-	WorkflowDescription string             // 工作流描述
-	WorkflowStep        []WorkflowStepInfo // 工作流步骤
+	WorkflowNickName    string             `json:"workflowNickName"`    // 工作流名称
+	WorkflowName        string             `json:"workflowName"`        // 工作流英文id
+	WorkflowDescription string             `json:"workflowDescription"` // 工作流描述
+	WorkflowStep        []WorkflowStepInfo `json:"workflowStep"`        // 工作流步骤
 }
 
 // 工作流状态表
 type WorkflowStepInfo struct {
 	gorm.Model
-	WorkflowID      uint    `json:"-"` // 所属工作流ID
-	IsStrat         bool    // 是否是开始流节点
-	StepName        string  // 工作流名称
-	StepNo          float64 // 步骤id （第几步）
-	StepAuthorityID string  // 操作者级别id
-	IsEnd           bool    // 是否是完结流节点
+	WorkflowID      uint    `json:"workflowID"`      // 所属工作流ID
+	IsStrat         bool    `json:"isStrat"`         // 是否是开始流节点
+	StepName        string  `json:"stepName"`        // 工作流名称
+	StepNo          float64 `json:"stepNo"`          // 步骤id （第几步）
+	StepAuthorityID string  `json:"stepAuthorityID"` // 操作者级别id
+	IsEnd           bool    `json:"isEnd"`           // 是否是完结流节点
 }
 
 //创建工作流

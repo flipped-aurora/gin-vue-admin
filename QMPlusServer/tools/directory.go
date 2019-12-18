77a1ff1d29aab083ad1843e874434e43e7286b0c
Grant Yin <granty1@163.com>
2019-12-18 21:53:13 +0800

    Add check for log directory.

---
+++ QMPlusServer/tools/directory.go
@@ -0,0 +1,14 @@
+package tools
+
+import "os"
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}

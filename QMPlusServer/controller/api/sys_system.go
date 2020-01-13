35661ff735463f7159583c1a7d09c6ac6f037ca9
pixel <303176530@qq.com>
2020-01-13 15:51:04 +0800

    增加了 配置管理功能

--- QMPlusServer/controller/api/sys_system.go
+++ QMPlusServer/controller/api/sys_system.go
@@ -22,7 +22,6 @@ func GetSystemConfig(c *gin.Context) {
 	}
 }
 
-
 // @Tags system
 // @Summary 设置配置文件内容
 // @Security ApiKeyAuth
@@ -31,25 +30,6 @@ func GetSystemConfig(c *gin.Context) {
 // @Success 200 {string} string "{"success":true,"data":{},"msg":"返回成功"}"
 // @Router /system/setSystemConfig [post]
 func SetSystemConfig(c *gin.Context) {
-	var sys sysModel.System
-	_ = c.ShouldBind(&sys)
-	err := sys.SetSystemConfig()
-	if err != nil {
-		servers.ReportFormat(c, false, fmt.Sprintf("设置失败：%v", err), gin.H{})
-	} else {
-		servers.ReportFormat(c, true, "设置成功", gin.H{})
-	}
-}
-
-
-// @Tags system
-// @Summary 设置配置文件内容
-// @Security ApiKeyAuth
-// @Produce  application/json
-// @Param data body sysModel.System true
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"返回成功"}"
-// @Router /system/ReloadSystem [post]
-func ReloadSystem(c *gin.Context) {
 	var sys sysModel.System
 	_ = c.ShouldBind(&sys)
 	err := sys.SetSystemConfig()

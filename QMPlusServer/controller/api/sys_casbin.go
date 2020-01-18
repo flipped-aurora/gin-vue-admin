4960e269c01f38548ed2e81546087708147bcc5c
pixel <303176530@qq.com>
2020-01-18 11:08:54 +0800

    api 增加 GET PUT DELETE POST 四种可选声明方式

--- QMPlusServer/controller/api/sys_casbin.go
+++ QMPlusServer/controller/api/sys_casbin.go
@@ -7,23 +7,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type CasbinInReceive struct {
-	AuthorityId string   `json:"authorityId"`
-	Paths       []string `json:paths`
-}
-
 // @Tags casbin
 // @Summary 更改角色api权限
 // @Security ApiKeyAuth
 // @accept application/json
 // @Produce application/json
-// @Param data body api.CreateAuthorityParams true "更改角色api权限"
+// @Param data body sysModel.CasbinInReceive true "更改角色api权限"
 // @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
 // @Router /casbin/casbinPUpdata [post]
 func CasbinPUpdata(c *gin.Context) {
-	var cmr CasbinInReceive
+	var cmr sysModel.CasbinInReceive
 	_ = c.ShouldBind(&cmr)
-	err := new(sysModel.CasbinModel).CasbinPUpdata(cmr.AuthorityId, cmr.Paths)
+	err := new(sysModel.CasbinModel).CasbinPUpdata(cmr.AuthorityId, cmr.CasbinInfos)
 	if err != nil {
 		servers.ReportFormat(c, false, fmt.Sprintf("添加规则失败，%v", err), gin.H{})
 	} else {
@@ -40,7 +35,7 @@ func CasbinPUpdata(c *gin.Context) {
 // @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
 // @Router /casbin/getPolicyPathByAuthorityId [post]
 func GetPolicyPathByAuthorityId(c *gin.Context) {
-	var cmr CasbinInReceive
+	var cmr sysModel.CasbinInReceive
 	_ = c.ShouldBind(&cmr)
 	paths := new(sysModel.CasbinModel).GetPolicyPathByAuthorityId(cmr.AuthorityId)
 	servers.ReportFormat(c, true, "获取规则成功", gin.H{"paths": paths})

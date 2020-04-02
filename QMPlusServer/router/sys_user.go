aef29b138de2c7ee99b2a81644e80d0e35384994
QM303176530 <303176530@qq.com>
2020-04-02 22:12:05 +0800

    拼写错误修改

--- QMPlusServer/router/sys_user.go
+++ QMPlusServer/router/sys_user.go
@@ -2,12 +2,12 @@ package router
 
 import (
 	"gin-vue-admin/controller/api"
-	"gin-vue-admin/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func InitUserRouter(Router *gin.RouterGroup) {
-	UserRouter := Router.Group("user").Use(middleware.JWTAuth()).Use(middleware.CasbinHandler())
+	UserRouter := Router.Group("user")
+	//.Use(middleware.JWTAuth()).Use(middleware.CasbinHandler())
 	{
 		UserRouter.POST("changePassword", api.ChangePassword)     // 修改密码
 		UserRouter.POST("uploadHeaderImg", api.UploadHeaderImg)   //上传头像

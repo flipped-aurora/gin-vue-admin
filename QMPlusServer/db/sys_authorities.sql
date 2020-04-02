f1b3a37dc3bf79fd9b3fc5e995b6ae1d6119f069
QM303176530 <303176530@qq.com>
2020-04-02 22:37:58 +0800

    增加总数据库sql和单表sql 此次拼写错误设计到 sys_api casbin两个表

---
+++ QMPlusServer/db/sys_authorities.sql
@@ -0,0 +1,3 @@
+INSERT INTO `sys_authorities` VALUES (2, '2019-09-08 16:18:45', '2019-09-08 16:18:45', NULL, '888', '普通用户', '0');
+INSERT INTO `sys_authorities` VALUES (6, '2019-09-18 22:23:33', '2019-09-18 22:23:33', NULL, '9528', '测试角色', '0');
+INSERT INTO `sys_authorities` VALUES (8, '2019-12-28 18:19:13', '2019-12-28 18:19:13', NULL, '8881', '普通用户子角色', '888');
---
+++ QMPlusServer/db/sys_data_authority_id.sql
@@ -0,0 +1,8 @@
+INSERT INTO `sys_data_authority_id` VALUES (2, 2);
+INSERT INTO `sys_data_authority_id` VALUES (2, 6);
+INSERT INTO `sys_data_authority_id` VALUES (2, 8);
+INSERT INTO `sys_data_authority_id` VALUES (6, 2);
+INSERT INTO `sys_data_authority_id` VALUES (6, 6);
+INSERT INTO `sys_data_authority_id` VALUES (8, 2);
+INSERT INTO `sys_data_authority_id` VALUES (8, 6);
+INSERT INTO `sys_data_authority_id` VALUES (8, 8);

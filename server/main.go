2f0b39af3dcfec86cfb5a36d97de567eb15d8abd
pixel <303176530@qq.com>
2020-04-13 10:30:26 +0800

    注释sqlite 防止gcc错误 需要使用自行打开注释即可 感谢 sqlitet提供者 [rikugun] 作者github： https://github.com/rikugun

--- server/main.go
+++ server/main.go
@@ -11,8 +11,8 @@ func main() {
 	switch global.GVA_CONFIG.System.DbType {
 	case "mysql":
 		initialize.Mysql()
-	case "sqlite":
-		initialize.Sqlite()
+	//case "sqlite":
+	//	initialize.Sqlite()  // sqlite需要gcc支持 windows用户需要自行安装gcc 如需使用打开注释即可
 	default:
 		initialize.Mysql()
 	}

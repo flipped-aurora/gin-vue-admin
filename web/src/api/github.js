2b194b76117f3a93c47332b668d6e2bfe21bc86b
pixel <303176530@qq.com>
2020-04-27 18:11:12 +0800

    解决文件上传存在的bug

--- web/src/api/github.js
+++ web/src/api/github.js
@@ -5,27 +5,29 @@ let loadingInstance;
 let service = axios.create();
 
 service.interceptors.request.use((config) => {
-  loadingInstance = Loading.service({ fullscreen: true });
-  return config;
+    loadingInstance = Loading.service({ fullscreen: true });
+    return config;
 });
 
 service.interceptors.response.use((resp) => {
-  loadingInstance.close();
-  return resp;
+    loadingInstance.close();
+    return resp;
+}, (error) => {
+    loadingInstance.close();
+    return error;
 });
 
 export function Commits(page) {
-  return service({
-    url:
-      "https://api.github.com/repos/flipped-aurora/gin-vue-admin/commits?page=" +
-      page,
-    method: "get",
-  });
+    return service({
+        url: "https://api.github.com/repos/flipped-aurora/gin-vue-admin/commits?page=" +
+            page,
+        method: "get",
+    });
 }
 
 export function Members() {
-  return service({
-    url: "https://api.github.com/orgs/FLIPPED-AURORA/members",
-    method: "get",
-  });
+    return service({
+        url: "https://api.github.com/orgs/FLIPPED-AURORA/members",
+        method: "get",
+    });
 }

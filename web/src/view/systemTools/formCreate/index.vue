9d9404b3c53eb5e2c4445642a70c047980c077c0
pixel <303176530@qq.com>
2020-05-15 10:10:19 +0800

    修复表单生成器加载问题

--- web/src/view/systemTools/formCreate/index.vue
+++ web/src/view/systemTools/formCreate/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="height:80vh">
-    <iframe width="100%" height="100%" :src="basePath+'/form-generator/#/'" frameborder="0"></iframe>
+    <iframe width="100%" height="100%" :src="'http://127.0.0.1:8888/form-generator/#/'" frameborder="0"></iframe>
   </div>
 </template>
 

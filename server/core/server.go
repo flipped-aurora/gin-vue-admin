5cb66082dff0185beb10ceda37f6da1d8b5fb8d8
徐志伟 <juwan.xu@qq.com>
2020-05-22 16:37:03 +0800

    改为使用endless
    改为使用endless

--- server/core/server.go
+++ server/core/server.go
@@ -6,6 +6,7 @@ import (
 	"gin-vue-admin/initialize"
 	"net/http"
 	"time"
+	"github.com/fvbock/endless"
 )
 
 func RunWindowsServer() {
@@ -16,13 +17,10 @@ func RunWindowsServer() {
 	Router := initialize.Routers()
 	Router.Static("/form-generator", "./resource/page")
 	address := fmt.Sprintf(":%d", global.GVA_CONFIG.System.Addr)
-	s := &http.Server{
-		Addr:           address,
-		Handler:        Router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
+	s := endless.NewServer(address, Router)
+	s.ReadHeaderTimeout = 10 * time.Millisecond
+	s.WriteTimeout = 10 * time.Second
+	s.MaxHeaderBytes = 1 << 20
 	// 保证文本顺序输出
 	// In order to ensure that the text order output can be deleted
 	time.Sleep(10 * time.Microsecond)

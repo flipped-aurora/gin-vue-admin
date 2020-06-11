46858200f7f8c3c4de611218da420415075233f4
Granty1 <granty1@163.com>
2020-06-11 11:40:21 +0800

    Format project code & upgrade some dependence

--- server/model/sys_auto_code.go
+++ server/model/sys_auto_code.go
@@ -5,17 +5,17 @@ type AutoCodeStruct struct {
 	StructName         string  `json:"structName"`
 	PackageName        string  `json:"packageName"`
 	Abbreviation       string  `json:"abbreviation"`
-	Description		   string `json:"description"`
+	Description        string  `json:"description"`
 	AutoCreateApiToSql bool    `json:"autoCreateApiToSql"`
 	Fields             []Field `json:"fields"`
 }
 
 type Field struct {
-	FieldName  string `json:"fieldName"`
-	FieldDesc  string `json:"fieldDesc"`
-	FieldType  string `json:"fieldType"`
-	FieldJson  string `json:"fieldJson"`
-	Comment    string `json:"comment"`
-	ColumnName string `json:"columnName"`
-	FieldSearchType  string `json:"fieldSearchType"`
+	FieldName       string `json:"fieldName"`
+	FieldDesc       string `json:"fieldDesc"`
+	FieldType       string `json:"fieldType"`
+	FieldJson       string `json:"fieldJson"`
+	Comment         string `json:"comment"`
+	ColumnName      string `json:"columnName"`
+	FieldSearchType string `json:"fieldSearchType"`
 }

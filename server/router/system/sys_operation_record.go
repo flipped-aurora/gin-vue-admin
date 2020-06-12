615eea3cdcc9ea81e428b3277a12df880b19db2b
pixel <303176530@qq.com>
2020-06-12 16:43:47 +0800

    操作日志不再记录自己的操作

--- server/router/system/sys_operation_record.go
+++ server/router/system/sys_operation_record.go
@@ -2,7 +2,6 @@ package system
 
 import (
 	v1 "github.com/flipped-aurora/gin-vue-admin/api/v1"
-	"github.com/flipped-aurora/gin-vue-admin/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,7 +9,7 @@ type OperationRecordRouter struct {
 }
 
 func (s *OperationRecordRouter) InitSysOperationRecordRouter(Router *gin.RouterGroup) {
-	operationRecordRouter := Router.Group("sysOperationRecord").Use(middleware.OperationRecord())
+	operationRecordRouter := Router.Group("sysOperationRecord")
 	var authorityMenuApi = v1.ApiGroupApp.SystemApiGroup.OperationRecordApi
 	{
 		operationRecordRouter.POST("createSysOperationRecord", authorityMenuApi.CreateSysOperationRecord)             // 新建SysOperationRecord

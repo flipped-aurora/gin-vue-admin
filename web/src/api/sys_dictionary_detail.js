b340bf6cb51f2bd55225227fc5574a4e1d07d2f8
QM303176530 <303176530@qq.com>
2020-06-24 20:41:36 +0800

    增加字典管理功能

---
+++ web/src/api/sys_dictionary_detail.js
@@ -0,0 +1,84 @@
+import service from '@/utils/request'
+
+// @Tags SysDictionaryDetail
+// @Summary 创建SysDictionaryDetail
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body model.SysDictionaryDetail true "创建SysDictionaryDetail"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
+// @Router /sysDictionaryDetail/createSysDictionaryDetail [post]
+export const createSysDictionaryDetail = (data) => {
+     return service({
+         url: "/sysDictionaryDetail/createSysDictionaryDetail",
+         method: 'post',
+         data
+     })
+ }
+
+
+// @Tags SysDictionaryDetail
+// @Summary 删除SysDictionaryDetail
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body model.SysDictionaryDetail true "删除SysDictionaryDetail"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"删除成功"}"
+// @Router /sysDictionaryDetail/deleteSysDictionaryDetail [delete]
+ export const deleteSysDictionaryDetail = (data) => {
+     return service({
+         url: "/sysDictionaryDetail/deleteSysDictionaryDetail",
+         method: 'delete',
+         data
+     })
+ }
+
+// @Tags SysDictionaryDetail
+// @Summary 更新SysDictionaryDetail
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body model.SysDictionaryDetail true "更新SysDictionaryDetail"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"更新成功"}"
+// @Router /sysDictionaryDetail/updateSysDictionaryDetail [put]
+ export const updateSysDictionaryDetail = (data) => {
+     return service({
+         url: "/sysDictionaryDetail/updateSysDictionaryDetail",
+         method: 'put',
+         data
+     })
+ }
+
+
+// @Tags SysDictionaryDetail
+// @Summary 用id查询SysDictionaryDetail
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body model.SysDictionaryDetail true "用id查询SysDictionaryDetail"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"查询成功"}"
+// @Router /sysDictionaryDetail/findSysDictionaryDetail [get]
+ export const findSysDictionaryDetail = (params) => {
+     return service({
+         url: "/sysDictionaryDetail/findSysDictionaryDetail",
+         method: 'get',
+         params
+     })
+ }
+
+
+// @Tags SysDictionaryDetail
+// @Summary 分页获取SysDictionaryDetail列表
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body request.PageInfo true "分页获取SysDictionaryDetail列表"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
+// @Router /sysDictionaryDetail/getSysDictionaryDetailList [get]
+ export const getSysDictionaryDetailList = (params) => {
+     return service({
+         url: "/sysDictionaryDetail/getSysDictionaryDetailList",
+         method: 'get',
+         params
+     })
+ }

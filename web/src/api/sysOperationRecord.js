8a710dc4555ff6dddcfb408b0f6aeadf78988a24
QM303176530 <303176530@qq.com>
2020-07-02 15:06:20 +0800

    进行日志相关样式修改，删除无用接口

--- web/src/api/sysOperationRecord.js
+++ web/src/api/sysOperationRecord.js
@@ -1,23 +1,6 @@
 import service from '@/utils/request'
 
 // @Tags SysOperationRecord
-// @Summary 创建SysOperationRecord
-// @Security ApiKeyAuth
-// @accept application/json
-// @Produce application/json
-// @Param data body model.SysOperationRecord true "创建SysOperationRecord"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
-// @Router /sysOperationRecord/createSysOperationRecord [post]
-export const createSysOperationRecord = (data) => {
-     return service({
-         url: "/sysOperationRecord/createSysOperationRecord",
-         method: 'post',
-         data
-     })
- }
-
-
-// @Tags SysOperationRecord
 // @Summary 删除SysOperationRecord
 // @Security ApiKeyAuth
 // @accept application/json
@@ -32,40 +15,6 @@ export const createSysOperationRecord = (data) => {
          data
      })
  }
-
-// @Tags SysOperationRecord
-// @Summary 更新SysOperationRecord
-// @Security ApiKeyAuth
-// @accept application/json
-// @Produce application/json
-// @Param data body model.SysOperationRecord true "更新SysOperationRecord"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"更新成功"}"
-// @Router /sysOperationRecord/updateSysOperationRecord [put]
- export const updateSysOperationRecord = (data) => {
-     return service({
-         url: "/sysOperationRecord/updateSysOperationRecord",
-         method: 'put',
-         data
-     })
- }
-
-
-// @Tags SysOperationRecord
-// @Summary 用id查询SysOperationRecord
-// @Security ApiKeyAuth
-// @accept application/json
-// @Produce application/json
-// @Param data body model.SysOperationRecord true "用id查询SysOperationRecord"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"查询成功"}"
-// @Router /sysOperationRecord/findSysOperationRecord [get]
- export const findSysOperationRecord = (params) => {
-     return service({
-         url: "/sysOperationRecord/findSysOperationRecord",
-         method: 'get',
-         params
-     })
- }
-
 
 // @Tags SysOperationRecord
 // @Summary 分页获取SysOperationRecord列表

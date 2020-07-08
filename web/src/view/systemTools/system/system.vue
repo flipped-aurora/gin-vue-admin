55cea812b52afe2a72ed76c8210ff41db6658875
安徽许彦祖 <66503905+xy6409068@users.noreply.github.com>
2020-07-08 18:21:04 +0800

    Update system.vue
    修复更改端口号时清零问题

--- web/src/view/systemTools/system/system.vue
+++ web/src/view/systemTools/system/system.vue
@@ -8,7 +8,7 @@
       <el-form-item label="环境值">
         <el-input v-model="config.system.env"></el-input>
       </el-form-item>
-      <el-form-item label="端口值">
+      <el-form-item label.number="端口值">
         <el-input v-model="config.system.addr"></el-input>
       </el-form-item>
       <el-form-item label="数据库类型">

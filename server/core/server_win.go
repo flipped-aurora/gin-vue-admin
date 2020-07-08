44b06ec0ef3c78f95396020788f571e18e6afc8e
xunull <todogo@vip.163.com>
2020-07-09 00:35:47 +0800

    [server/core/server_win.go]: 添加信号监听,使用http.Shutdown 结束server

--- server/core/server_win.go
+++ server/core/server_win.go
@@ -3,17 +3,40 @@
 package core
 
 import (
+	"context"
+	"gin-vue-admin/global"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func initServer(address string, router *gin.Engine) server {
-	return &http.Server{
+	gServer := &http.Server{
 		Addr:           address,
 		Handler:        router,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
+
+	go func() {
+		sc := make(chan os.Signal, 1)
+		signal.Notify(sc, syscall.SIGTERM, syscall.SIGINT)
+		<-sc
+
+		if gServer != nil {
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			if err := gServer.Shutdown(ctx); err != nil {
+				global.GVA_LOG.Error("Server Shutdown Error: ", err.Error())
+			} else {
+				global.GVA_LOG.Info("Server Shutdown")
+			}
+		}
+	}()
+
+	return gServer
 }

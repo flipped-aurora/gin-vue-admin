a9401cf0a61bc9f7a174db4da0187e205aed49ba
klausY <emosick@qq.com>
2020-07-21 11:18:02 +0800

    版权标识细节调整

---
+++ web/src/view/layout/bottomInfo/bottomInfo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="bottom-info">
+    <div>
+      <span>Powered by</span>
+      <span>
+      <a href="https://github.com/flipped-aurora/gin-vue-admin">gin-vue-admin</a>
+      </span>
+      <el-divider direction="vertical"></el-divider>
+      <span>Copyright</span>
+      <span>
+      <a href="https://github.com/flipped-aurora">flipped-aurora团队</a>
+      </span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name:"BottomInfo"
+}
+</script>
+<style lang="scss">
+.bottom-info {
+  color: #888;
+  height: 30px;
+  line-height: 12px;
+  a {
+    color: #888;
+  }
+  div {
+    display: flex;
+    justify-content: center;
+    span{
+      margin: 0 3px;
+    }
+  }
+}
+</style>

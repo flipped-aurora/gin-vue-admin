ca8b5d7bb2d1972b7bd5c56b6b85332e132d8b80
SliverHorn <503551462@qq.com>
2020-08-15 17:45:20 +0800

    修复三首歌曲无法播放的问题

--- web/src/view/dashboard/component/musicPlayer.vue
+++ web/src/view/dashboard/component/musicPlayer.vue
@@ -19,21 +19,21 @@
                     {
                         name: '东西（Cover：林俊呈）',
                         artist: '纳豆',
-                        url: 'https://cdn.moefe.org/music/mp3/thing.mp3',
+                        url: 'http://music.163.com/song/media/outer/url?id=1321594530.mp3',
                         cover: 'https://p1.music.126.net/5zs7IvmLv7KahY3BFzUmrg==/109951163635241613.jpg?param=300y300', // prettier-ignore
                         lrc: 'https://cdn.moefe.org/music/lrc/thing.lrc',
                     },
                     {
                         name: '响喜乱舞（Cover：MARiA）',
                         artist: '泠鸢yousa',
-                        url: 'https://cdn.moefe.org/music/mp3/kyoukiranbu.mp3',
+                        url: 'http://music.163.com/song/media/outer/url?id=1318962459.mp3',
                         cover: 'https://p1.music.126.net/AUGVPQ_rVrngDH9ocQrn3Q==/109951163613037822.jpg?param=300y300', // prettier-ignore
                         lrc: 'https://cdn.moefe.org/music/lrc/kyoukiranbu.lrc',
                     },
                     {
                         name: '啵唧',
                         artist: 'Hanser',
-                        url: 'https://cdn.moefe.org/music/mp3/kiss.mp3',
+                        url: 'http://music.163.com/song/media/outer/url?id=1321424246.mp3',
                         cover: 'https://p1.music.126.net/K0-IPcIQ9QFvA0jXTBqoWQ==/109951163636756693.jpg?param=300y300', // prettier-ignore
                         lrc: 'https://cdn.moefe.org/music/lrc/kiss.lrc',
                     },

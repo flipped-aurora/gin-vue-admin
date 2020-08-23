9cb2a470fb24b8f7e60a3bb45577b872207da9b8
QM303176530 <303176530@qq.com>
2020-08-23 15:27:00 +0800

    gormv2升级 第一阶段

--- server/model/sys_operation_record.go
+++ server/model/sys_operation_record.go
@@ -18,6 +18,6 @@ type SysOperationRecord struct {
 	ErrorMessage string        `json:"error_message" form:"error_message" gorm:"column:error_message;comment:''"`
 	Body         string        `json:"body" form:"body" gorm:"column:body;comment:'请求Body'"`
 	Resp         string        `json:"resp" form:"resp" gorm:"column:resp;comment:'响应Body'"`
-	UserId       int           `json:"user_id" form:"user_id" gorm:"column:user_id;comment:''"`
+	UserID       int           `json:"user_id" form:"user_id" gorm:"column:user_id;comment:''"`
 	User         SysUser       `json:"user"`
 }

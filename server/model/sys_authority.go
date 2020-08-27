88e7b46f325db408dd2ad5a8bcfd00c0dfe269a7
pixel <303176530@qq.com>
2020-08-27 12:56:14 +0800

    防止自动建表失败 角色id主键长度限制为100

--- server/model/sys_authority.go
+++ server/model/sys_authority.go
@@ -8,7 +8,7 @@ type SysAuthority struct {
 	CreatedAt       time.Time
 	UpdatedAt       time.Time
 	DeletedAt       *time.Time     `sql:"index"`
-	AuthorityId     string         `json:"authorityId" gorm:"not null;unique;primary_key" gorm:"comment:'角色ID'"`
+	AuthorityId     string         `json:"authorityId" gorm:"not null;unique;primary_key;comment:'角色ID';type:varchar(100)"`
 	AuthorityName   string         `json:"authorityName" gorm:"comment:'角色名'"`
 	ParentId        string         `json:"parentId" gorm:"comment:'父角色ID'"`
 	DataAuthorityId []SysAuthority `json:"dataAuthorityId" gorm:"many2many:sys_data_authority_id;association_jointable_foreignkey:data_authority_id"`

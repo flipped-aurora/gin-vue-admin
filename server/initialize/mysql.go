f53f8ddc65607c7c9c15c2176d9eef00b39976c1
QM303176530 <303176530@qq.com>
2020-08-29 15:55:35 +0800

    增加mysql过滤，修改角色id长度 模板长度修改为size模式

--- server/initialize/mysql.go
+++ server/initialize/mysql.go
@@ -13,7 +13,7 @@ func Mysql() {
 	admin := global.GVA_CONFIG.Mysql
 	mysqlConfig := mysql.Config{
 		DSN:                       admin.Username + ":" + admin.Password + "@(" + admin.Path + ")/" + admin.Dbname + "?" + admin.Config, // DSN data source name
-		DefaultStringSize:         256,                                                                                                  // string 类型字段的默认长度
+		DefaultStringSize:         191,                                                                                                  // string 类型字段的默认长度
 		DisableDatetimePrecision:  true,                                                                                                 // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
 		DontSupportRenameIndex:    true,                                                                                                 // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
 		DontSupportRenameColumn:   true,                                                                                                 // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列

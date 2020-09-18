cbdf1cd92e672a803d32559720426dd8508e4dde
pixel <303176530@qq.com>
2020-09-18 15:41:26 +0800

    取消comment后携带的''

--- server/model/exa_customer.go
+++ server/model/exa_customer.go
@@ -6,9 +6,9 @@ import (
 
 type ExaCustomer struct {
 	gorm.Model
-	CustomerName       string  `json:"customerName" form:"customerName" gorm:"comment:'客户名'"`
-	CustomerPhoneData  string  `json:"customerPhoneData" form:"customerPhoneData" gorm:"comment:'客户手机号'"`
-	SysUserID          uint    `json:"sysUserId" form:"sysUserId" gorm:"comment:'管理ID'"`
-	SysUserAuthorityID string  `json:"sysUserAuthorityID" form:"sysUserAuthorityID" gorm:"comment:'管理角色ID'"`
-	SysUser            SysUser `json:"sysUser" form:"sysUser" gorm:"comment:'管理详情'"`
+	CustomerName       string  `json:"customerName" form:"customerName" gorm:"comment:客户名"`
+	CustomerPhoneData  string  `json:"customerPhoneData" form:"customerPhoneData" gorm:"comment:客户手机号"`
+	SysUserID          uint    `json:"sysUserId" form:"sysUserId" gorm:"comment:管理ID"`
+	SysUserAuthorityID string  `json:"sysUserAuthorityID" form:"sysUserAuthorityID" gorm:"comment:管理角色ID"`
+	SysUser            SysUser `json:"sysUser" form:"sysUser" gorm:"comment:管理详情"`
 }

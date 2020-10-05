ffeebaabf481d740a36a9d70ddd2a44306223fa6
SliverHorn <503551462@qq.com>
2020-10-05 13:34:39 +0800

    添加os的Compiler的获取

--- server/utils/server.go
+++ server/utils/server.go
@@ -54,6 +54,7 @@ type Disk struct {
 func InitOS() (o Os) {
 	o.GOOS = runtime.GOOS
 	o.NumCPU = runtime.NumCPU()
+	o.Compiler = runtime.Compiler
 	o.GoVersion = runtime.Version()
 	o.NumGoroutine = runtime.NumGoroutine()
 	return o

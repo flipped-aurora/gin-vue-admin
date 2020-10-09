fa2bc99a875be951aea91f4141ba5cf9809462a5
pixel <303176530@qq.com>
2020-10-09 17:42:11 +0800

    修改工作流基础功能

--- web/src/view/workflow/workflowCreate/workflowCreate.vue
+++ web/src/view/workflow/workflowCreate/workflowCreate.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <wfd-gva ref="wfd" :data="demoData" :height="600" :users="candidateUsers" :groups="candidateGroups" :categorys="categorys" :lang="lang" />
+    <el-button size="small" style="float:right;margin-top:6px;margin-right:6px;" @click="saveXML">导出XML</el-button>
+    <el-button size="small" style="float:right;margin-top:6px;margin-right:6px;" @click="saveImg">导出图片</el-button>
+    <el-button size="small" style="float:right;margin-top:6px;margin-right:6px;" @click="save">保存流程</el-button>
+    <wfd-gva ref="wfd" :data="demoData" :height="600" :users="users" :groups="groups" :categorys="categorys" :lang="lang" />
   </div>
 </template>
 <script>
@@ -16,13 +19,20 @@ export default {
     return {
       lang: "zh",
       demoData: {},
-      candidateUsers: [],
-      candidateGroups: []
+      users: [{id:'1',name:'审批人1'},{id:'2',name:'审批人2'},{id:'3',name:'审批人3'}],
+      groups: [{id:'1',name:'组1'},{id:'2',name:'组2'},{id:'3',name:'组3'}],
+      categorys:[{id:'1',name:'分类1'},{id:'2',name:'分类2'},{id:'3',name:'分类3'}]
     }
   },
   methods:{
     save(){
       console.log(this.$refs['wfd'].graph.save())
+    },
+    saveXML(){
+      console.log(this.$refs['wfd'].graph.saveXML())
+    },
+    saveImg(){
+      console.log(this.$refs['wfd'].graph.saveImg())
     }
   }
 }

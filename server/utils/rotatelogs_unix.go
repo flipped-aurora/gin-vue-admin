54321f5a2719754be8ef62a86fe741cd0b24d3d6
SliverHorn <503551462@qq.com>
2020-10-23 11:04:29 +0800

    Fix the error that windows cannot create soft connection

---
+++ server/utils/rotatelogs_unix.go
@@ -0,0 +1,26 @@
+// +build !windows
+
+package utils
+
+import (
+	"gin-vue-admin/global"
+	zaprotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"go.uber.org/zap/zapcore"
+	"os"
+	"path"
+	"time"
+)
+
+// GetWriteSyncer zap logger中加入file-rotatelogs
+func GetWriteSyncer() (zapcore.WriteSyncer, error) {
+	fileWriter, err := zaprotatelogs.New(
+		path.Join(global.GVA_CONFIG.Zap.Director, "%Y-%m-%d.log"),
+		zaprotatelogs.WithLinkName(global.GVA_CONFIG.Zap.LinkName),
+		zaprotatelogs.WithMaxAge(7*24*time.Hour),
+		zaprotatelogs.WithRotationTime(24*time.Hour),
+	)
+	if global.GVA_CONFIG.Zap.LogInConsole {
+		return zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(fileWriter)), err
+	}
+	return zapcore.AddSync(fileWriter), err
+}

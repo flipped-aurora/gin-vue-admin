e1176824d90d42c3fc324052f8fa00da39afa2f8
QM303176530 <303176530@qq.com>
2020-10-24 17:04:04 +0800

    工作流插件参与进入项目本体

---
+++ web/src/components/gva-wfd/components/DetailPanel/MailTaskDetail.vue
@@ -0,0 +1,54 @@
+<template>
+    <div :data-clazz="model.clazz">
+        <div class="panelTitle">{{i18n['mailTask']}}</div>
+        <div class="panelBody">
+            <DefaultDetail :model="model" :onChange="onChange" :readOnly="readOnly" />
+            <div class="panelRow">
+                <div>{{i18n['mailTask.to']}}：</div>
+                <el-input style="width:90%; font-size:12px"
+                          :disabled="readOnly"
+                          :value="model.to"
+                          @input="(value) => {onChange('to', value)}" />
+            </div>
+            <div class="panelRow">
+                <div>{{i18n['mailTask.subject']}}：</div>
+                <el-input style="width:90%; font-size:12px"
+                          :disabled="readOnly"
+                          :value="model.subject"
+                          @input="(value) => {onChange('subject', value)}" />
+            </div>
+            <div class="panelRow">
+                <div>{{i18n['mailTask.content']}}：</div>
+                <el-input style="width:90%; font-size:12px"
+                          type="textarea"
+                          :rows="4"
+                          :disabled="readOnly"
+                          :value="model.content"
+                          @input="(value) => {onChange('content', value)}" />
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+  import DefaultDetail from "./DefaultDetail";
+  export default {
+    inject: ['i18n'],
+    components: {
+      DefaultDetail
+    },
+    props: {
+      model: {
+        type:Object,
+        default: ()=>({}),
+      },
+      onChange: {
+        type: Function,
+        default: ()=>{}
+      },
+      readOnly:{
+        type: Boolean,
+        default: false,
+      }
+    },
+  }
+</script>
---
+++ web/src/components/gva-wfd/components/DetailPanel/SignalEventDetail.vue
@@ -0,0 +1,45 @@
+<template>
+    <div :data-clazz="model.clazz">
+        <div class="panelTitle">{{i18n['signalEvent']}}</div>
+        <div class="panelBody">
+            <DefaultDetail :model="model" :onChange="onChange" :readOnly="readOnly" />
+            <div class="panelRow">
+                <div>{{i18n['signalEvent.signal']}}：</div>
+                <el-select style="width:90%; font-size: 12px"
+                           :placeholder="i18n['signalEvent.signal']"
+                           :value="model.signal"
+                           :disabled="readOnly"
+                           @change="(e) => { onChange('signal', e) }">
+                    <el-option v-for="signal in signalDefs" :key="signal.id" :label="signal.name" :value="signal.id" />
+                </el-select>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+  import DefaultDetail from "./DefaultDetail";
+  export default {
+    inject: ['i18n'],
+    components: {
+      DefaultDetail
+    },
+    props: {
+      model: {
+        type:Object,
+        default: ()=>({}),
+      },
+      onChange: {
+        type: Function,
+        default: ()=>{}
+      },
+      readOnly:{
+        type: Boolean,
+        default: false,
+      },
+      signalDefs: {
+        type: Array,
+        default: ()=>([]),
+      },
+    },
+  }
+</script>

bef9b485a153196cce0df6a7b0998efa196a0ca4
QM303176530 <303176530@qq.com>
2020-10-25 10:42:34 +0800

    版本号更新

--- web/src/main.js
+++ web/src/main.js
@@ -66,7 +66,7 @@ Vue.prototype.$echarts = echarts;
 
 console.log(`
        欢迎使用 Gin-Vue-Admin
-       当前版本:V2.3.3
+       当前版本:V2.3.4
        默认自动化文档地址:http://127.0.0.1%s/swagger/index.html
        默认前端文件运行地址:http://127.0.0.1:8080
 `)

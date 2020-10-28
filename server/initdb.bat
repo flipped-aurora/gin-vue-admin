4a462d3614f98bc947888d9a61870a8a771c4c05
huxin55 <huxin3@jd.com>
2020-10-28 16:40:12 +0800

    LINUX下数据初始化

--- server/initdb.bat
+++ server/initdb.bat
@@ -1,12 +1,12 @@
-@echo ##########Formatting code#########################################
+echo ##########Formatting code#########################################
 go fmt ./ && go vet ./
-@echo ##########Format the code successfully############################
-@echo ##########Compiling gva.exe#######################################
+echo ##########Format the code successfully############################
+echo ##########Compiling gva.exe#######################################
 go build -o gva.exe cmd/main.go
-@echo ##########Successfully compiled gva.exe###########################
-@echo ##########Initializing data using gva.exe#########################
-gva.exe initdb
-@echo ##########Use gva.exe to initialize data successfully#############
-@echo ##########Deleting gva.exe########################################
-del gva.exe
-@echo ##########Deleting gva.exe successfully###########################
+echo ##########Successfully compiled gva.exe###########################
+echo ##########Initializing data using gva.exe#########################
+sh gva.exe initdb
+echo ##########Use gva.exe to initialize data successfully#############
+echo ##########Deleting gva.exe########################################
+rm -rf  gva.exe
+echo ##########Deleting gva.exe successfully###########################

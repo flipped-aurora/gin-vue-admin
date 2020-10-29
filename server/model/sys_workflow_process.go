10d8d7f28aea19726a0edbcfed98cd99eb12308c
pixel <303176530@qq.com>
2020-10-29 11:23:34 +0800

    修改gorm.Model为自定义的全局global.GVA_MODEL 方便自定义基础参数

--- server/model/sys_workflow_process.go
+++ server/model/sys_workflow_process.go
@@ -1,10 +1,12 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gin-vue-admin/global"
+)
 
 // 工作流流转表
 type SysWorkFlowProcess struct {
-	gorm.Model
+	global.GVA_MODEL
 	ApplicationID  uint   `json:"applicationID" gorm:"comment:当前工作流所属申请的ID"` // 当前工作流所属申请的ID
 	CurrentNode    string `json:"currentNode" gorm:"comment:当前进度节点"`         // 当前进度节点
 	HistoricalNode string `json:"historicalNode" gorm:"comment:上一个进度节点"`     // 上一个进度节点

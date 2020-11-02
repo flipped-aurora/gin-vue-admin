4fc661a7519dea485417ac91aaf0b5c5d3e795f4
pixel <303176530@qq.com>
2020-11-02 11:19:29 +0800

    自动化代码不再记录日志

--- server/router/sys_auto_code.go
+++ server/router/sys_auto_code.go
@@ -9,8 +9,7 @@ import (
 func InitAutoCodeRouter(Router *gin.RouterGroup) {
 	AutoCodeRouter := Router.Group("autoCode").
 		Use(middleware.JWTAuth()).
-		Use(middleware.CasbinHandler()).
-		Use(middleware.OperationRecord())
+		Use(middleware.CasbinHandler())
 	{
 		AutoCodeRouter.POST("createTemp", v1.CreateTemp) // 创建自动化代码
 		AutoCodeRouter.GET("getTables", v1.GetTables)    // 获取对应数据库的表

96380bd0314856a2d9d2e657e729955a688080ba
SliverHorn <sliver_horn@qq.com>
2020-11-08 02:43:11 +0800

    server/api/v1层代码swagger备注错误修改及代码优化

--- server/api/v1/sys_auto_code.go
+++ server/api/v1/sys_auto_code.go
@@ -3,35 +3,29 @@ package v1
 import (
 	"fmt"
 	"gin-vue-admin/global"
-	"gin-vue-admin/global/response"
 	"gin-vue-admin/model"
+	"gin-vue-admin/model/response"
 	"gin-vue-admin/service"
 	"gin-vue-admin/utils"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"net/url"
 	"os"
 )
 
-// @Tags SysApi
+// @Tags AutoCode
 // @Summary 自动代码模板
 // @Security ApiKeyAuth
 // @accept application/json
 // @Produce application/json
-// @Param data body model.AutoCodeStruct true "创建自动代码"
+// TODO @Param data body model.AutoCodeStruct true "创建自动代码"
 // @Success 200 {string} string "{"success":true,"data":{},"msg":"创建成功"}"
 // @Router /autoCode/createTemp [post]
 func CreateTemp(c *gin.Context) {
 	var a model.AutoCodeStruct
 	_ = c.ShouldBindJSON(&a)
-	AutoCodeVerify := utils.Rules{
-		"Abbreviation": {utils.NotEmpty()},
-		"StructName":   {utils.NotEmpty()},
-		"PackageName":  {utils.NotEmpty()},
-		"Fields":       {utils.NotEmpty()},
-	}
-	WKVerifyErr := utils.Verify(a, AutoCodeVerify)
-	if WKVerifyErr != nil {
-		response.FailWithMessage(WKVerifyErr.Error(), c)
+	if err := utils.Verify(a, utils.AutoCodeVerify); err != nil {
+		response.FailWithMessage(err.Error(), c)
 		return
 	}
 	if a.AutoCreateApiToSql {
@@ -49,7 +43,7 @@ func CreateTemp(c *gin.Context) {
 				Method:      "DELETE",
 			},
 			{
-				Path:        "/" + a.Abbreviation + "/" + "delete" + a.StructName+"ByIds",
+				Path:        "/" + a.Abbreviation + "/" + "delete" + a.StructName + "ByIds",
 				Description: "批量删除" + a.Description,
 				ApiGroup:    a.Abbreviation,
 				Method:      "DELETE",
@@ -74,10 +68,10 @@ func CreateTemp(c *gin.Context) {
 			},
 		}
 		for _, v := range apiList {
-			errC := service.AutoCreateApi(v)
-			if errC != nil {
+			if err := service.AutoCreateApi(v); err != nil {
+				global.GVA_LOG.Error("自动化创建失败!请自行清空垃圾数据!", zap.Any("err", err))
 				c.Writer.Header().Add("success", "false")
-				c.Writer.Header().Add("msg", url.QueryEscape(fmt.Sprintf("自动化创建失败，%v，请自行清空垃圾数据", errC)))
+				c.Writer.Header().Add("msg", url.QueryEscape("自动化创建失败!请自行清空垃圾数据!"))
 				return
 			}
 		}
@@ -96,60 +90,54 @@ func CreateTemp(c *gin.Context) {
 	}
 }
 
-// @Tags SysApi
+// @Tags AutoCode
 // @Summary 获取当前数据库所有表
 // @Security ApiKeyAuth
 // @accept application/json
 // @Produce application/json
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"创建成功"}"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
 // @Router /autoCode/getTables [get]
-
 func GetTables(c *gin.Context) {
 	dbName := c.DefaultQuery("dbName", global.GVA_CONFIG.Mysql.Dbname)
 	err, tables := service.GetTables(dbName)
 	if err != nil {
-		response.FailWithMessage(fmt.Sprintf("查询table失败，%v", err), c)
+		global.GVA_LOG.Error("查询table失败!", zap.Any("err", err))
+		response.FailWithMessage("查询table失败", c)
 	} else {
-		response.OkWithData(gin.H{
-			"tables": tables,
-		}, c)
+		response.OkWithDetailed(gin.H{"tables": tables}, "获取成功", c)
 	}
 }
 
-// @Tags SysApi
+// @Tags AutoCode
 // @Summary 获取当前所有数据库
 // @Security ApiKeyAuth
 // @accept application/json
 // @Produce application/json
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"创建成功"}"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
 // @Router /autoCode/getDatabase [get]
 func GetDB(c *gin.Context) {
-	err, dbs := service.GetDB()
-	if err != nil {
-		response.FailWithMessage(fmt.Sprintf("查询table失败，%v", err), c)
+	if err, dbs := service.GetDB(); err != nil {
+		global.GVA_LOG.Error("获取失败!", zap.Any("err", err))
+		response.FailWithMessage("获取失败", c)
 	} else {
-		response.OkWithData(gin.H{
-			"dbs": dbs,
-		}, c)
+		response.OkWithDetailed(gin.H{"dbs": dbs}, "获取成功", c)
 	}
 }
 
-// @Tags SysApi
+// @Tags AutoCode
 // @Summary 获取当前表所有字段
 // @Security ApiKeyAuth
 // @accept application/json
 // @Produce application/json
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"创建成功"}"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
 // @Router /autoCode/getColumn [get]
 func GetColumn(c *gin.Context) {
 	dbName := c.DefaultQuery("dbName", global.GVA_CONFIG.Mysql.Dbname)
 	tableName := c.Query("tableName")
-	err, columns := service.GetColumn(tableName, dbName)
-	if err != nil {
-		response.FailWithMessage(fmt.Sprintf("查询table失败，%v", err), c)
+	if err, columns := service.GetColumn(tableName, dbName); err != nil {
+		global.GVA_LOG.Error("获取失败!", zap.Any("err", err))
+		response.FailWithMessage("获取失败", c)
 	} else {
-		response.OkWithData(gin.H{
-			"columns": columns,
-		}, c)
+		response.OkWithDetailed(gin.H{"columns": columns}, "获取成功", c)
 	}
 }

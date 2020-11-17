fae875a9c965142bf9fe8eb69e91f8b32a4a7f2a
pixel <303176530@qq.com>
2020-11-17 17:35:23 +0800

    增加初始化参数

---
+++ web/src/view/workflow/userList/need.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+import {getMyNeed} from "@/api/workflowProcess"
+export default {
+    async created(){
+        const res = await getMyNeed()
+        if(res.code == 0){
+            console.log(res.data)
+        }
+    }
+}
+</script>
---
+++ web/src/view/workflow/userList/started.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+import {getMyStated} from "@/api/workflowProcess"
+export default {
+    async created(){
+        const res = await getMyStated()
+        if(res.code == 0){
+            console.log(res.data)
+        }
+    }
+}
+</script>

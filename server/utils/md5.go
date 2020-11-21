1e85e03713b525ab6eaf33e6f8dc369ea1b674bc
SliverHorn <sliver_horn@qq.com>
2020-11-21 11:07:05 +0800

    utils备注统一化, 删除多余的函数

--- server/utils/md5.go
+++ server/utils/md5.go
@@ -5,6 +5,12 @@ import (
 	"encoding/hex"
 )
 
+//@author: [piexlmax](https://github.com/piexlmax)
+//@function: MD5V
+//@description: md5加密
+//@param: str []byte
+//@return: string
+
 func MD5V(str []byte) string {
 	h := md5.New()
 	h.Write(str)
--- server/utils/rotatelogs_windows.go
+++ server/utils/rotatelogs_windows.go
@@ -9,7 +9,11 @@ import (
 	"time"
 )
 
-// GetWriteSyncer zap logger中加入file-rotatelogs
+//@author: [SliverHorn](https://github.com/SliverHorn)
+//@function: GetWriteSyncer
+//@description: zap logger中加入file-rotatelogs
+//@return: zapcore.WriteSyncer, error
+
 func GetWriteSyncer() (zapcore.WriteSyncer, error) {
 	fileWriter, err := zaprotatelogs.New(
 		path.Join(global.GVA_CONFIG.Zap.Director, "%Y-%m-%d.log"),

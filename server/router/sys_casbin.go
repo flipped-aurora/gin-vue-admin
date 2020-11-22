591ec31153cff8da1968e6cde5449b6f5636975e
QM303176530 <303176530@qq.com>
2020-11-22 23:12:50 +0800

    日志记录中间件位置转移

--- server/router/sys_casbin.go
+++ server/router/sys_casbin.go
@@ -6,7 +6,7 @@ import (
 )
 
 func InitCasbinRouter(Router *gin.RouterGroup) {
-	CasbinRouter := Router.Group("casbin")
+	CasbinRouter := Router.Group("casbin").Use(middleware.OperationRecord())
 	{
 		CasbinRouter.POST("updateCasbin", v1.UpdateCasbin)
 		CasbinRouter.POST("getPolicyPathByAuthorityId", v1.GetPolicyPathByAuthorityId)
--- server/router/sys_email.go
+++ server/router/sys_email.go
@@ -6,7 +6,7 @@ import (
 )
 
 func InitEmailRouter(Router *gin.RouterGroup) {
-	UserRouter := Router.Group("email")
+	UserRouter := Router.Group("email").Use(middleware.OperationRecord())
 	{
 		UserRouter.POST("emailTest", v1.EmailTest) // 发送测试邮件
 	}

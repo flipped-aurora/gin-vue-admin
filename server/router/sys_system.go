0763c21cb72d266459ec433a738a303d3dc8bef8
QM303176530 <303176530@qq.com>
2020-11-22 23:12:50 +0800

    日志记录中间件位置转移

--- server/router/sys_system.go
+++ server/router/sys_system.go
@@ -6,7 +6,7 @@ import (
 )
 
 func InitSystemRouter(Router *gin.RouterGroup) {
-	SystemRouter := Router.Group("system")
+	SystemRouter := Router.Group("system").Use(middleware.OperationRecord())
 	{
 		SystemRouter.POST("getSystemConfig", v1.GetSystemConfig) // 获取配置文件内容
 		SystemRouter.POST("setSystemConfig", v1.SetSystemConfig) // 设置配置文件内容

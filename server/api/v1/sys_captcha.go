ceda9c3c9c2ec637fc4564634500d6242de93e82
hzzhangyile <hzzhangyile@corp.netease.com>
2020-12-03 20:02:06 +0800

    sys_captcha.go: 支持使用redis来存储图片验证码，按需开启

--- server/api/v1/sys_captcha.go
+++ server/api/v1/sys_captcha.go
@@ -3,11 +3,17 @@ package v1
 import (
 	"gin-vue-admin/global"
 	"gin-vue-admin/model/response"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/mojocn/base64Captcha"
 	"go.uber.org/zap"
 )
 
+// 如果需要多实例部署，可使用 Redis 来存储图片验证码。需初始化 Redis
+// 使用 `var store = NewDefaultRedisStore() ` 替换下面的 `var store = base64Captcha.DefaultMemStore` 即可
+//
+// var store = NewDefaultRedisStore()
 var store = base64Captcha.DefaultMemStore
 
 // @Tags Base
@@ -31,4 +37,46 @@ func Captcha(c *gin.Context) {
 			PicPath:   b64s,
 		}, "验证码获取成功", c)
 	}
+}
+
+func NewDefaultRedisStore() base64Captcha.Store {
+	return &RedisStore{
+		Expiration: time.Second * 180,
+		PreKey:     "CAPTCHA_KEY_",
+	}
+}
+
+type RedisStore struct {
+	Expiration time.Duration
+	PreKey     string
+}
+
+func (rs *RedisStore) Set(id string, value string) {
+	err := global.GVA_REDIS.Set(rs.PreKey+id, value, rs.Expiration).Err()
+	if err != nil {
+		global.GVA_LOG.Error("RedisStoreSetError!", zap.Any("err", err))
+	}
+}
+
+func (rs *RedisStore) Get(id string, clear bool) string {
+	key := rs.PreKey + id
+	val, err := global.GVA_REDIS.Get(key).Result()
+	if err != nil {
+		global.GVA_LOG.Error("RedisStoreGetError!", zap.Any("err", err))
+		return ""
+	}
+	if clear {
+		err := global.GVA_REDIS.Del(key).Err()
+		if err != nil {
+			global.GVA_LOG.Error("RedisStoreClearError!", zap.Any("err", err))
+			return ""
+		}
+	}
+	return val
+}
+
+func (rs *RedisStore) Verify(id, answer string, clear bool) bool {
+	key := rs.PreKey + id
+	v := rs.Get(key, clear)
+	return v == answer
 }

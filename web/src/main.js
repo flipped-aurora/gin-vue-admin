e56a58e8d236a345ae906a8c216e1f07d7363e54
QM303176530 <303176530@qq.com>
2020-12-13 12:42:38 +0800

    清理图标 form table 富文本 示例

--- web/src/main.js
+++ web/src/main.js
@@ -9,27 +9,8 @@ Vue.use(ElementUI);
 // 引入封装的router
 import router from '@/router/index'
 
-// canvas背景插件
-import vueParticleLine from 'vue-particle-line'
-import 'vue-particle-line/dist/vue-particle-line.css'
-Vue.use(vueParticleLine)
-
 // time line css
 import '../node_modules/timeline-vuejs/dist/timeline-vuejs.css'
-
-// 富文本插件
-import VueQuillEditor from 'vue-quill-editor'
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'quill/dist/quill.bubble.css'
-
-Vue.use(VueQuillEditor)
-
-// markdown插件
-import mavonEditor from 'mavon-editor'
-import 'mavon-editor/dist/css/index.css'
-
-Vue.use(mavonEditor)
 
 import '@/permission'
 import { store } from '@/store/index'

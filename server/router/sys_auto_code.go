3549f4cf1fb69c74beeec3efecd36f3ecb701d64
songzhibin97 <718428482@qq.com>
2021-02-11 19:00:06 +0800

    自动生成代码新增预览接口

--- server/router/sys_auto_code.go
+++ server/router/sys_auto_code.go
@@ -8,6 +8,7 @@ import (
 func InitAutoCodeRouter(Router *gin.RouterGroup) {
 	AutoCodeRouter := Router.Group("autoCode")
 	{
+		AutoCodeRouter.POST("preview", v1.PreviewTemp)   // 获取自动创建代码预览
 		AutoCodeRouter.POST("createTemp", v1.CreateTemp) // 创建自动化代码
 		AutoCodeRouter.GET("getTables", v1.GetTables)    // 获取对应数据库的表
 		AutoCodeRouter.GET("getDB", v1.GetDB)            // 获取数据库

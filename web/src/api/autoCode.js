11efbe7699df67f59f64a84ffe7e995adbbcc6c3
pixel <303176530@qq.com>
2021-02-13 16:22:50 +0800

    initdb默认string长度改为191

--- web/src/api/autoCode.js
+++ web/src/api/autoCode.js
@@ -1,16 +1,13 @@
 import service from '@/utils/request'
-// @Tags api
-// @Summary 分页获取角色列表
-// @Security ApiKeyAuth
-// @accept application/json
-// @Produce application/json
-// @Param data body modelInterface.PageInfo true "分页获取用户列表"
-// @Success 200 {string} json "{"success":true,"data":{},"msg":"获取成功"}"
-// @Router /api/getApiList [post]
-// {
-//  page     int
-//	pageSize int
-// }
+
+export const preview = (data) => {
+    return service({
+        url: "/autoCode/preview",
+        method: 'post',
+        data,
+    })
+}
+
 export const createTemp = (data) => {
     return service({
         url: "/autoCode/createTemp",
@@ -19,7 +16,6 @@ export const createTemp = (data) => {
         responseType: 'blob'
     })
 }
-
 
 // @Tags SysApi
 // @Summary 获取当前所有数据库

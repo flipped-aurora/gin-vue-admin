c410125c8fee640e10282ba5f765722cd6c5897b
xiaowei <403828237@qq.com>
2021-02-19 13:00:07 +0800

    NavigationDuplicated error
    NavigationDuplicated: Avoided redundant navigation to current location
    ![](https://img-blog.csdnimg.cn/20200901093646588.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3dlaXhpbl80MjQwMjg0NQ==,size_16,color_FFFFFF,t_70)

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -3,6 +3,13 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+//获取原型对象上的push函数
+const originalPush = Router.prototype.push
+//修改原型对象中的push方法
+Router.prototype.push = function push(location) {
+   return originalPush.call(this, location).catch(err => err)
+}
+
 const baseRouters = [{
         path: '/',
         redirect: '/login'

1158d20e37f6319b5fcdfca05819543187569bd0
pixel <303176530@qq.com>
2021-02-24 09:53:45 +0800

    增加快速设置父级菜单功能 修复本地上传文件用户使用头像存在问题的bug

--- web/src/view/superAdmin/menu/menu.vue
+++ web/src/view/superAdmin/menu/menu.vue
@@ -97,8 +97,9 @@
             v-model="form.parentId"
           ></el-cascader>
         </el-form-item>
-        <el-form-item label="文件路径" prop="component" style="width:30%">
+        <el-form-item label="文件路径" prop="component" style="width:60%">
           <el-input autocomplete="off" v-model="form.component"></el-input>
+          <span style="font-size:12px;margin-right:12px;">如果菜单包含子菜单，请创建router-view二级路由页面或者</span><el-button size="mini" @click="form.component = 'view/routerHolder.vue'">点我设置</el-button>
         </el-form-item>
         <el-form-item label="展示名称" prop="meta.title" style="width:30%">
           <el-input autocomplete="off" v-model="form.meta.title"></el-input>

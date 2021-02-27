625ff317fcc471361f6192b70de94e110234b948
pixel <303176530@qq.com>
2021-02-27 10:48:51 +0800

    对合并代码进行微调

--- server/config/oss.go
+++ server/config/oss.go
@@ -14,14 +14,13 @@ type Qiniu struct {
 	UseCdnDomains bool   `mapstructure:"use-cdn-domains" json:"useCdnDomains" yaml:"use-cdn-domains"`
 }
 
-
 type AliyunOSS struct {
 	Endpoint        string `mapstructure:"endpoint" json:"endpoint" yaml:"endpoint"`
 	AccessKeyId     string `mapstructure:"access-key-id" json:"accessKeyId" yaml:"access-key-id"`
 	AccessKeySecret string `mapstructure:"access-key-secret" json:"accessKeySecret" yaml:"access-key-secret"`
 	BucketName      string `mapstructure:"bucket-name" json:"bucketName" yaml:"bucket-name"`
 	BucketUrl       string `mapstructure:"bucket-url" json:"bucketUrl" yaml:"bucket-url"`
-
+}
 type TencentCOS struct {
 	Bucket     string `mapstructure:"bucket" json:"bucket" yaml:"bucket"`
 	Region     string `mapstructure:"region" json:"region" yaml:"region"`
@@ -29,5 +28,4 @@ type TencentCOS struct {
 	SecretKey  string `mapstructure:"secret-key" json:"secretKey" yaml:"secret-key"`
 	BaseURL    string `mapstructure:"base-url" json:"baseURL" yaml:"base-url"`
 	PathPrefix string `mapstructure:"path-prefix" json:"pathPrefix" yaml:"path-prefix"`
-
 }

7c474bd8a4716657fa422d2898c251ab172251c0
pixel <303176530@qq.com>
2021-03-04 19:38:07 +0800

    自动初始化整体调试完成

--- server/model/request/sys_init.go
+++ server/model/request/sys_init.go
@@ -3,7 +3,7 @@ package request
 type InitDB struct {
 	Host     string `json:"host"`
 	Port     string `json:"port"`
-	UserName string `json:"user_name" binding:"required"`
+	UserName string `json:"userName" binding:"required"`
 	Password string `json:"password"`
-	DBName   string `json:"db_name" binding:"required"`
+	DBName   string `json:"dbName" binding:"required"`
 }

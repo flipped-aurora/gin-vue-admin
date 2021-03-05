b74ea581729a51026018ef0dcf912e1f9986a92c
pixel <303176530@qq.com>
2021-03-05 20:30:02 +0800

    版本号更改提交

--- web/src/main.js
+++ web/src/main.js
@@ -47,7 +47,7 @@ Vue.prototype.$echarts = echarts;
 
 console.log(`
        欢迎使用 Gin-Vue-Admin
-       当前版本:V2.3.9
+       当前版本:V2.4.0
        加群方式:微信：shouzi_1994 QQ群：622360840
        默认自动化文档地址:http://127.0.0.1%s/swagger/index.html
        默认前端文件运行地址:http://127.0.0.1:8080

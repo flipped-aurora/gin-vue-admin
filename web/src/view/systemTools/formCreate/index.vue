7bf32941136caf92cefea2721ee21b920eb2b971
东邪黄药师 <cx282694@gmail.com>
2021-03-08 23:43:17 +0800

    集中配置端口

--- web/src/view/systemTools/formCreate/index.vue
+++ web/src/view/systemTools/formCreate/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="height:80vh">
-    <iframe width="100%" height="100%" :src="'http://127.0.0.1:8888/form-generator/#/'" frameborder="0"></iframe>
+    <iframe width="100%" height="100%" :src="url" frameborder="0"></iframe>
   </div>
 </template>
 
@@ -10,6 +10,7 @@ export default {
     name:"FormGenerator",
     data(){
       return{
+        url:'http://127.0.0.1:' + buildConf.goServerPort + '/form-generator/#/',
         basePath:path
       }
     },

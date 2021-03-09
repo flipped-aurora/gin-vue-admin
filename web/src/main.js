981ae4a6d0ff5d625c19bd583ce473cfc29dd177
pixel <303176530@qq.com>
2021-03-09 19:21:55 +0800

    清理多余包 element按需加载

--- web/src/main.js
+++ web/src/main.js
@@ -1,11 +1,112 @@
 import Vue from 'vue'
 import App from './App.vue'
-// 引入element
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-// 全局配置elementui的dialog不能通过点击遮罩层关闭
-ElementUI.Dialog.props.closeOnClickModal.default = false
-Vue.use(ElementUI);
+
+//  按需引入element
+import {
+    Button, 
+    Select, 
+    Dialog, 
+    Form, 
+    Input,
+    FormItem, 
+    Option, 
+    Loading, 
+    Message, 
+    Container, 
+    Card,
+    Dropdown,
+    DropdownMenu,
+    DropdownItem,
+    Row,
+    Col,
+    Menu,
+    Submenu,
+    MenuItem,
+    Aside,
+    Main,
+    Badge,
+    Header,
+    Tabs,
+    Breadcrumb,
+    BreadcrumbItem,
+    Scrollbar,
+    Avatar,
+    TabPane,
+    Divider,
+    Table,
+    TableColumn,
+    Cascader,
+    Checkbox,
+    CheckboxGroup,
+    Pagination,
+    Tag,
+    Drawer,
+    Tree,
+    Popover,
+    Switch,
+    Collapse,
+    CollapseItem,
+    Tooltip,
+    DatePicker,
+    InputNumber,
+    Steps,
+    Upload,
+    Progress
+} from 'element-ui';
+
+Vue.use(Button);
+Vue.use(Select);
+Vue.use(Dialog);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(Option);
+Vue.use(Container);
+Vue.use(Card);
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Badge);
+Vue.use(Header);
+Vue.use(Tabs);
+Vue.use(Breadcrumb);
+Vue.use(BreadcrumbItem);
+Vue.use(Avatar);
+Vue.use(TabPane);
+Vue.use(Divider);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Checkbox);
+Vue.use(Cascader);
+Vue.use(Tag);
+Vue.use(Pagination);
+Vue.use(Drawer);
+Vue.use(Tree);
+Vue.use(CheckboxGroup);
+Vue.use(Popover);
+Vue.use(InputNumber);
+Vue.use(Switch);
+Vue.use(Collapse);
+Vue.use(CollapseItem);
+Vue.use(Tooltip);
+Vue.use(DatePicker);
+Vue.use(Steps);
+Vue.use(Upload);
+Vue.use(Progress);
+Vue.use(Scrollbar);
+Vue.use(Loading.directive);
+
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$message = Message;
+Dialog.props.closeOnClickModal.default = false
+
 // 引入封装的router
 import router from '@/router/index'
 
@@ -41,9 +142,6 @@ export default new Vue({
     store
 }).$mount('#app')
 
-//引入echarts
-import echarts from 'echarts'
-Vue.prototype.$echarts = echarts;
 
 console.log(`
        欢迎使用 Gin-Vue-Admin

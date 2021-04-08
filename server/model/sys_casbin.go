5939a24f544da51c7de199dcc51c5eeed377a133
SliverHorn <45428434+SliverHorn@users.noreply.github.com>
2021-04-08 18:14:21 +0800

    Update sys_casbin.go

--- server/model/sys_casbin.go
+++ server/model/sys_casbin.go
@@ -1,7 +1,7 @@
 package model
 
 type CasbinModel struct {
-	Ptype       string `json:"ptype" gorm:"column:p_type"`
+	Ptype       string `json:"ptype" gorm:"column:ptype"`
 	AuthorityId string `json:"rolename" gorm:"column:v0"`
 	Path        string `json:"path" gorm:"column:v1"`
 	Method      string `json:"method" gorm:"column:v2"`

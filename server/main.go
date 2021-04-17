aa9cd298852414401c3ba535578ffacb099e8229
songzhibin97 <718428482@qq.com>
2021-04-17 14:50:44 +0800

    新增定时任务封装以及配置自动清理数据库功能
    新增
    - server/config/timer.go
    - server/initialize/timer.go
    - server/utils/db_automation.go
    - server/utils/timer/timed_task.go
    修改
    - server/config.yaml
    - server/config/config/go
    - server/global/global.go
    - server/main.go

--- server/main.go
+++ server/main.go
@@ -17,6 +17,7 @@ func main() {
 	global.GVA_VP = core.Viper()      // 初始化Viper
 	global.GVA_LOG = core.Zap()       // 初始化zap日志库
 	global.GVA_DB = initialize.Gorm() // gorm连接数据库
+	initialize.Timer()
 	if global.GVA_DB != nil {
 		initialize.MysqlTables(global.GVA_DB) // 初始化表
 		// 程序结束前关闭数据库链接

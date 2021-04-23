186aa894765d77024e438b9382761430f78ac30b
pixel <303176530@qq.com>
2021-04-23 15:47:42 +0800

    将配置提取

--- web/src/view/systemTools/formCreate/index.vue
+++ web/src/view/systemTools/formCreate/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div style="height:80vh">
-    <iframe width="100%" height="100%" :src="'http://127.0.0.1:8888/form-generator/#/'" frameborder="0"></iframe>
+    <iframe width="100%" height="100%" :src="basePath+':8888/form-generator/#/'" frameborder="0"></iframe>
   </div>
 </template>
 
 <script>
-var path = process.env.VUE_APP_BASE_API
+var path = process.env.VUE_APP_BASE_PATH
 export default {
     name:"FormGenerator",
     data(){

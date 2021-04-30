3e85645e44c54130200dfe6b09b5952cfbfe16d9
何秀钢 <xiugang.he@skyee360.com>
2021-04-30 14:14:00 +0800

    modified 增加结构体描述，完善自动化文档

--- server/model/exa_file_upload_download.go
+++ server/model/exa_file_upload_download.go
@@ -6,8 +6,8 @@ import (
 
 type ExaFileUploadAndDownload struct {
 	global.GVA_MODEL
-	Name string `json:"name" gorm:"comment:文件名"`
-	Url  string `json:"url" gorm:"comment:文件地址"`
-	Tag  string `json:"tag" gorm:"comment:文件标签"`
-	Key  string `json:"key" gorm:"comment:编号"`
+	Name string `json:"name" gorm:"comment:文件名"` // 文件名
+	Url  string `json:"url" gorm:"comment:文件地址"` // 文件地址
+	Tag  string `json:"tag" gorm:"comment:文件标签"` // 文件标签
+	Key  string `json:"key" gorm:"comment:编号"`   // 编号
 }

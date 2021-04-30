7d37b155fc6386b5342cbb7b19609581d35fa052
何秀钢 <xiugang.he@skyee360.com>
2021-04-30 14:14:00 +0800

    modified 增加结构体描述，完善自动化文档

--- server/model/request/sys_init.go
+++ server/model/request/sys_init.go
@@ -1,9 +1,9 @@
 package request
 
 type InitDB struct {
-	Host     string `json:"host"`
-	Port     string `json:"port"`
-	UserName string `json:"userName" binding:"required"`
-	Password string `json:"password"`
-	DBName   string `json:"dbName" binding:"required"`
+	Host     string `json:"host"`                        // 服务器地址
+	Port     string `json:"port"`                        // 数据库连接端口
+	UserName string `json:"userName" binding:"required"` // 数据库用户名
+	Password string `json:"password"`                    // 数据库密码
+	DBName   string `json:"dbName" binding:"required"`   // 数据库名
 }

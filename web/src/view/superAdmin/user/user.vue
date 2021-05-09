5b86699cab8717073bba423edaaa0c7ac7a79685
蒋吉兆 <303176530@qq.com>
2021-05-09 19:26:08 +0800

    统一登录校验 误删用户登陆菜单后不再白屏

--- web/src/view/superAdmin/user/user.vue
+++ web/src/view/superAdmin/user/user.vue
@@ -121,7 +121,7 @@ export default {
       rules: {
         username: [
           { required: true, message: "请输入用户名", trigger: "blur" },
-          { min: 6, message: "最低6位字符", trigger: "blur" }
+          { min: 5, message: "最低5位字符", trigger: "blur" }
         ],
         password: [
           { required: true, message: "请输入用户密码", trigger: "blur" },

be6663b710c04217ab196b7ae2b4949deeca49cc
Leonard Wang <wangdeyu1994@163.com>
2021-05-12 00:54:07 +0800

    修复在线修改系统配置的bug

--- server/core/viper.go
+++ server/core/viper.go
@@ -36,6 +36,7 @@ func Viper(path ...string) *viper.Viper {
 
 	v := viper.New()
 	v.SetConfigFile(config)
+	v.SetConfigType("yaml")
 	err := v.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))

fac46d38bf369106a066ee38261a46767e86ff26
何秀钢 <xiugang.he@skyee360.com>
2021-05-11 17:26:06 +0800

    modified 增加结构体描述，完善自动化文档

--- server/model/sys_authority.go
+++ server/model/sys_authority.go
@@ -5,12 +5,12 @@ import (
 )
 
 type SysAuthority struct {
-	CreatedAt       time.Time
-	UpdatedAt       time.Time
+	CreatedAt       time.Time      // 创建时间
+	UpdatedAt       time.Time      // 更新时间
 	DeletedAt       *time.Time     `sql:"index"`
 	AuthorityId     string         `json:"authorityId" gorm:"not null;unique;primary_key;comment:角色ID;size:90"` // 角色ID
-	AuthorityName   string         `json:"authorityName" gorm:"comment:角色名"` // 角色名
-	ParentId        string         `json:"parentId" gorm:"comment:父角色ID"` // 父角色ID
+	AuthorityName   string         `json:"authorityName" gorm:"comment:角色名"`                                    // 角色名
+	ParentId        string         `json:"parentId" gorm:"comment:父角色ID"`                                       // 父角色ID
 	DataAuthorityId []SysAuthority `json:"dataAuthorityId" gorm:"many2many:sys_data_authority_id"`
 	Children        []SysAuthority `json:"children" gorm:"-"`
 	SysBaseMenus    []SysBaseMenu  `json:"menus" gorm:"many2many:sys_authority_menus;"`

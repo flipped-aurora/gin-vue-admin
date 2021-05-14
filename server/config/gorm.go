0f45d3830a2738cdcdc382a4b893e28dd85f8938
何秀钢 <xiugang.he@skyee360.com>
2021-05-14 11:33:14 +0800

    modified 增加结构体描述，完善自动化文档

--- server/config/gorm.go
+++ server/config/gorm.go
@@ -1,14 +1,14 @@
 package config
 
 type Mysql struct {
-	Path         string `mapstructure:"path" json:"path" yaml:"path"` // 服务器地址:端口
-	Config       string `mapstructure:"config" json:"config" yaml:"config"`
-	Dbname       string `mapstructure:"db-name" json:"dbname" yaml:"db-name"`     // 数据库名
-	Username     string `mapstructure:"username" json:"username" yaml:"username"` // 数据库用户名
-	Password     string `mapstructure:"password" json:"password" yaml:"password"` // 数据库密码
-	MaxIdleConns int    `mapstructure:"max-idle-conns" json:"maxIdleConns" yaml:"max-idle-conns"`
-	MaxOpenConns int    `mapstructure:"max-open-conns" json:"maxOpenConns" yaml:"max-open-conns"`
-	LogMode      bool   `mapstructure:"log-mode" json:"logMode" yaml:"log-mode"`
+	Path         string `mapstructure:"path" json:"path" yaml:"path"`                             // 服务器地址:端口
+	Config       string `mapstructure:"config" json:"config" yaml:"config"`                       // 高级配置
+	Dbname       string `mapstructure:"db-name" json:"dbname" yaml:"db-name"`                     // 数据库名
+	Username     string `mapstructure:"username" json:"username" yaml:"username"`                 // 数据库用户名
+	Password     string `mapstructure:"password" json:"password" yaml:"password"`                 // 数据库密码
+	MaxIdleConns int    `mapstructure:"max-idle-conns" json:"maxIdleConns" yaml:"max-idle-conns"` // 空闲中的最大连接数
+	MaxOpenConns int    `mapstructure:"max-open-conns" json:"maxOpenConns" yaml:"max-open-conns"` // 打开到数据库的最大连接数
+	LogMode      bool   `mapstructure:"log-mode" json:"logMode" yaml:"log-mode"`                  // 是否开启Gorm全局日志
 	LogZap       string `mapstructure:"log-zap" json:"logZap" yaml:"log-zap"`
 }
 

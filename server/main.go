9ef71a066a1039dd304a6cdadaa48df5c4f9e52a
songzhibin97 <718428482@qq.com>
2021-06-03 14:44:28 +0800

    增加 go generate 懒人模式

--- server/main.go
+++ server/main.go
@@ -6,6 +6,11 @@ import (
 	"gin-vue-admin/initialize"
 )
 
+//go:generate go env -w GO111MODULE=on
+//go:generate go env -w GOPROXY=https://goproxy.cn,direct
+//go:generate go mod tidy
+//go:generate go mod download
+
 // @title Swagger Example API
 // @version 0.0.1
 // @description This is a sample Server pets

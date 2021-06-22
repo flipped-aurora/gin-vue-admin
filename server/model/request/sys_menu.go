47b8add661285ca69ad30d1537b489e9cd5eb656
songzhibin97 <718428482@qq.com>
2021-06-22 12:39:26 +0800

    fix:创建用户初始化权限,确保用户可用
    修改了:
    - server/api/v1/sys_authority.go
    - server/model/request/sys_casbin.go
    - server/model/request/sys_menu.go

--- server/model/request/sys_menu.go
+++ server/model/request/sys_menu.go
@@ -1,9 +1,25 @@
 package request
 
-import "gin-vue-admin/model"
+import (
+	"gin-vue-admin/model"
+)
 
 // Add menu authority info structure
 type AddMenuAuthorityInfo struct {
 	Menus       []model.SysBaseMenu
 	AuthorityId string // 角色ID
+}
+
+func DefaultMenu() []model.SysBaseMenu {
+	return []model.SysBaseMenu{{
+		ParentId:  "0",
+		Path:      "dashboard",
+		Name:      "dashboard",
+		Component: "view/dashboard/index.vue",
+		Sort:      1,
+		Meta: model.Meta{
+			Title: "仪表盘",
+			Icon:  "setting",
+		},
+	}}
 }

a56c3a0f32c9ddb06e8fbd30ed207df08833e0d6
songzhibin97 <718428482@qq.com>
2021-08-20 19:45:01 +0800

    fix:main

--- server/main.go
+++ server/main.go
@@ -1,38 +1,33 @@
 package main
 
-//import (
-//	"gin-vue-admin/core"
-//	"gin-vue-admin/global"
-//	"gin-vue-admin/initialize"
-//)
-//
-////go:generate go env -w GO111MODULE=on
-////go:generate go env -w GOPROXY=https://goproxy.cn,direct
-////go:generate go mod tidy
-////go:generate go mod download
-//
-//// @title Swagger Example API
-//// @version 0.0.1
-//// @description This is a sample Server pets
-//// @securityDefinitions.apikey ApiKeyAuth
-//// @in header
-//// @name x-token
-//// @BasePath /
-//func main() {
-//	global.GVA_VP = core.Viper()      // 初始化Viper
-//	global.GVA_LOG = core.Zap()       // 初始化zap日志库
-//	global.GVA_DB = initialize.Gorm() // gorm连接数据库
-//	initialize.Timer()
-//	if global.GVA_DB != nil {
-//		initialize.MysqlTables(global.GVA_DB) // 初始化表
-//		// 程序结束前关闭数据库链接
-//		db, _ := global.GVA_DB.DB()
-//		defer db.Close()
-//	}
-//	core.RunWindowsServer()
-//}
+import (
+	"gin-vue-admin/core"
+	"gin-vue-admin/global"
+	"gin-vue-admin/initialize"
+)
 
-var Plugin plugin
+//go:generate go env -w GO111MODULE=on
+//go:generate go env -w GOPROXY=https://goproxy.cn,direct
+//go:generate go mod tidy
+//go:generate go mod download
 
-type plugin struct {
+// @title Swagger Example API
+// @version 0.0.1
+// @description This is a sample Server pets
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name x-token
+// @BasePath /
+func main() {
+	global.GVA_VP = core.Viper()      // 初始化Viper
+	global.GVA_LOG = core.Zap()       // 初始化zap日志库
+	global.GVA_DB = initialize.Gorm() // gorm连接数据库
+	initialize.Timer()
+	if global.GVA_DB != nil {
+		initialize.MysqlTables(global.GVA_DB) // 初始化表
+		// 程序结束前关闭数据库链接
+		db, _ := global.GVA_DB.DB()
+		defer db.Close()
+	}
+	core.RunWindowsServer()
 }

4561d86609155d9fcef84a80863926a5fa28c499
songzhibin97 <718428482@qq.com>
2021-08-21 10:48:44 +0800

    feat:插件注册&差分so

--- server/utils/plugin/plugin.go
+++ server/utils/plugin/plugin.go
@@ -1,35 +1,12 @@
 package plugin
 
 import (
-	"plugin"
-	"sync"
-
 	"github.com/gin-gonic/gin"
 )
 
 const (
 	OnlyFuncName = "Plugin"
 )
-
-var ManagementPlugin = managementPlugin{mp: make(map[string]*plugin.Plugin)}
-
-type managementPlugin struct {
-	mp map[string]*plugin.Plugin
-	sync.Mutex
-}
-
-func (m *managementPlugin) SetPlugin(key string, p *plugin.Plugin) {
-	m.Lock()
-	defer m.Unlock()
-	m.mp[key] = p
-}
-
-func (m *managementPlugin) GetPlugin(key string) (p *plugin.Plugin, ok bool) {
-	m.Lock()
-	defer m.Unlock()
-	p, ok = m.mp[key]
-	return
-}
 
 // Plugin 插件模式接口化
 type Plugin interface {

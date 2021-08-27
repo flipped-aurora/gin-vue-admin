f5e0268d0fa3d2551905a20c05684b0c72556e2a
蒋吉兆 <303176530@qq.com>
2021-08-28 00:00:08 +0800

    插件优化

--- server/plugin/notify/global/global.go
+++ server/plugin/notify/global/global.go
@@ -2,8 +2,4 @@ package global
 
 import "github.com/flipped-aurora/gin-vue-admin/server/plugin/notify/config"
 
-var GlobalConfig_ = &config.DingDing{
-	Url:    "https://oapi.dingtalk.com/robot/send",
-	Token:  "77d18293e221b3b7bdae7330d19a213c3cbb20fa07de5f4c273c0363ae475c34",
-	Secret: "SEC38425b7bd57c6bca9ce20611c41b437994ed65e0ddbd42e840ffa7a8c7da2106",
-}
+var GlobalConfig_ = &config.DingDing{}

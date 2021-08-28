732143b9bbe5ae6291492db90e5823c05bed6f70
蒋吉兆 <303176530@qq.com>
2021-08-28 13:09:17 +0800

    更新插件仓库

--- server/initialize/plugin.go
+++ server/initialize/plugin.go
@@ -2,8 +2,8 @@ package initialize
 
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
-	//email "github.com/flipped-aurora/gva-plug-email"   // 在线仓库模式
-	"github.com/flipped-aurora/gin-vue-admin/server/plugin/email" // 本地插件仓库地址模式
+	email "github.com/flipped-aurora/gva-plugins/email" // 在线仓库模式go
+	//"github.com/flipped-aurora/gin-vue-admin/server/plugin/email" // 本地插件仓库地址模式
 	"github.com/flipped-aurora/gin-vue-admin/server/plugin/example_plugin"
 	"github.com/flipped-aurora/gin-vue-admin/server/utils/plugin"
 	"github.com/gin-gonic/gin"

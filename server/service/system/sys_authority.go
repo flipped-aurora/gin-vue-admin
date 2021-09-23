dc704074f2f07d731ffe686fabfdff6d96515a76
songzhibin97 <718428482@qq.com>
2021-09-23 10:34:48 +0800

    Update sys_authority.go

--- server/service/system/sys_authority.go
+++ server/service/system/sys_authority.go
@@ -122,7 +122,7 @@ func (authorityService *AuthorityService) GetAuthorityInfoList(info request.Page
 	limit := info.PageSize
 	offset := info.PageSize * (info.Page - 1)
 	db := global.GVA_DB.Model(&system.SysAuthority{})
-	err = db.Count(&total).Error
+	err = db.Where("parent_id = 0").Count(&total).Error
 	var authority []system.SysAuthority
 	err = db.Limit(limit).Offset(offset).Preload("DataAuthorityId").Where("parent_id = 0").Find(&authority).Error
 	if len(authority) > 0 {

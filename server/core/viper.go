ca38932fdad66e39c9fedf394ac1653ab0c5a616
songzhibin97 <718428482@qq.com>
2021-09-25 13:33:42 +0800

    feat:明确root语义

--- server/core/viper.go
+++ server/core/viper.go
@@ -56,9 +56,11 @@ func Viper(path ...string) *viper.Viper {
 	if err := v.Unmarshal(&global.GVA_CONFIG); err != nil {
 		fmt.Println(err)
 	}
+	// root 适配性
+	// 根据root位置去找到对应迁移位置,保证root路径有效
 	global.GVA_CONFIG.AutoCode.Root, _ = filepath.Abs("..")
 	global.BlackCache = local_cache.NewCache(
 		local_cache.SetDefaultExpire(time.Second * time.Duration(global.GVA_CONFIG.JWT.ExpiresTime)),
-		)
+	)
 	return v
 }

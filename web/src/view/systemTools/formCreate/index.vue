65001b32254c73f89983f8ea93a5fbdb2c9a2421
奇淼（piexlmax <303176530@qq.com>
2021-09-30 10:37:39 +0800

    [Feature]:#753 (#754)
    [Feature]:#753 (#754)

--- web/src/view/systemTools/formCreate/index.vue
+++ web/src/view/systemTools/formCreate/index.vue
@@ -1,16 +1,18 @@
 <template>
   <div style="height:80vh">
-    <iframe width="100%" height="100%" :src="basePath+':8888/form-generator/#/'" frameborder="0" />
+    <iframe width="100%" height="100%" :src="`${basePath}:${basePort}/form-generator/#/`" frameborder="0" />
   </div>
 </template>
 
 <script>
 var path = import.meta.env.VITE_BASE_PATH
+var port = import.meta.env.VITE_SERVER_PORT
 export default {
   name: 'FormGenerator',
   data() {
     return {
-      basePath: path
+      basePath: path,
+      basePort: port
     }
   }
 }

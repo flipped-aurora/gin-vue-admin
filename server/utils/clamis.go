24dc77b142ad1671bb4ab562d2579fb08dd948ca
piexl <303176530@qq.com>
2021-10-25 13:25:15 +0800

    修改从jwt获取用信息的方法

--- server/utils/clamis.go
+++ server/utils/clamis.go
@@ -7,46 +7,33 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
+func GetClaims(c *gin.Context) *systemReq.CustomClaims {
+	token := c.Request.Header.Get("x-token")
+	j := NewJWT()
+
+	claims, err := j.ParseToken(token)
+	if err != nil {
+		global.GVA_LOG.Error("从Gin的Context中获取从jwt解析信息失败, 请检查请求头是否存在x-token且claims是否为规定结构")
+	}
+	return claims
+}
+
 // 从Gin的Context中获取从jwt解析出来的用户ID
 func GetUserID(c *gin.Context) uint {
-	if claims, exists := c.Get("claims"); !exists {
-		global.GVA_LOG.Error("从Gin的Context中获取从jwt解析出来的用户ID失败, 请检查路由是否使用jwt中间件!")
-		return 0
-	} else {
-		waitUse := claims.(*systemReq.CustomClaims)
-		return waitUse.ID
-	}
+	return GetClaims(c).ID
 }
 
 // 从Gin的Context中获取从jwt解析出来的用户UUID
 func GetUserUuid(c *gin.Context) uuid.UUID {
-	if claims, exists := c.Get("claims"); !exists {
-		global.GVA_LOG.Error("从Gin的Context中获取从jwt解析出来的用户UUID失败, 请检查路由是否使用jwt中间件!")
-		return uuid.UUID{}
-	} else {
-		waitUse := claims.(*systemReq.CustomClaims)
-		return waitUse.UUID
-	}
+	return GetClaims(c).UUID
 }
 
 // 从Gin的Context中获取从jwt解析出来的用户角色id
 func GetUserAuthorityId(c *gin.Context) string {
-	if claims, exists := c.Get("claims"); !exists {
-		global.GVA_LOG.Error("从Gin的Context中获取从jwt解析出来的用户UUID失败, 请检查路由是否使用jwt中间件!")
-		return ""
-	} else {
-		waitUse := claims.(*systemReq.CustomClaims)
-		return waitUse.AuthorityId
-	}
+	return GetClaims(c).AuthorityId
 }
 
 // 从Gin的Context中获取从jwt解析出来的用户角色id
 func GetUserInfo(c *gin.Context) *systemReq.CustomClaims {
-	if claims, exists := c.Get("claims"); !exists {
-		global.GVA_LOG.Error("从Gin的Context中获取从jwt解析出来的用户UUID失败, 请检查路由是否使用jwt中间件!")
-		return nil
-	} else {
-		waitUse := claims.(*systemReq.CustomClaims)
-		return waitUse
-	}
+	return GetClaims(c)
 }

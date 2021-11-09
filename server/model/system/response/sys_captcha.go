0d06de2b41f2045a73ab0b4fcacd8dd1bcb4875d
piexl <303176530@qq.com>
2021-11-09 11:37:28 +0800

    调整验证码校验规则保证前后端配置一致 #794

--- server/model/system/response/sys_captcha.go
+++ server/model/system/response/sys_captcha.go
@@ -1,6 +1,7 @@
 package response
 
 type SysCaptchaResponse struct {
-	CaptchaId string `json:"captchaId"`
-	PicPath   string `json:"picPath"`
+	CaptchaId     string `json:"captchaId"`
+	PicPath       string `json:"picPath"`
+	CaptchaLength int    `json:"captchaLength""`
 }

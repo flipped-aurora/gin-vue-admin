9f096ec16a59d2341f29e29dcc4dfb20afe006b5
蒋吉兆 <303176530@qq.com>
2021-11-15 23:00:14 +0800

    增加pgsql适配（todo）

--- server/initialize/gorm.go
+++ server/initialize/gorm.go
@@ -1,18 +1,17 @@
 package initialize
 
 import (
+	"github.com/flipped-aurora/gin-vue-admin/server/initialize/internal"
+	"gorm.io/gorm/logger"
 	"os"
 
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
-	"github.com/flipped-aurora/gin-vue-admin/server/initialize/internal"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/autocode"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/example"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/system"
 
 	"go.uber.org/zap"
-	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 //@author: SliverHorn
@@ -24,6 +23,8 @@ func Gorm() *gorm.DB {
 	switch global.GVA_CONFIG.System.DbType {
 	case "mysql":
 		return GormMysql()
+	case "pgsql":
+		return GormPgSql()
 	default:
 		return GormMysql()
 	}
@@ -60,38 +61,6 @@ func MysqlTables(db *gorm.DB) {
 		os.Exit(0)
 	}
 	global.GVA_LOG.Info("register table success")
-}
-
-//@author: SliverHorn
-//@function: GormMysql
-//@description: 初始化Mysql数据库
-//@return: *gorm.DB
-
-func GormMysql() *gorm.DB {
-	m := global.GVA_CONFIG.Mysql
-	if m.Dbname == "" {
-		return nil
-	}
-	dsn := m.Username + ":" + m.Password + "@tcp(" + m.Path + ")/" + m.Dbname + "?" + m.Config
-	mysqlConfig := mysql.Config{
-		DSN:                       dsn,   // DSN data source name
-		DefaultStringSize:         191,   // string 类型字段的默认长度
-		DisableDatetimePrecision:  true,  // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
-		DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
-		DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
-		SkipInitializeWithVersion: false, // 根据版本自动配置
-	}
-	if db, err := gorm.Open(mysql.New(mysqlConfig), gormConfig()); err != nil {
-		//global.GVA_LOG.Error("MySQL启动异常", zap.Any("err", err))
-		//os.Exit(0)
-		//return nil
-		return nil
-	} else {
-		sqlDB, _ := db.DB()
-		sqlDB.SetMaxIdleConns(m.MaxIdleConns)
-		sqlDB.SetMaxOpenConns(m.MaxOpenConns)
-		return db
-	}
 }
 
 //@author: SliverHorn

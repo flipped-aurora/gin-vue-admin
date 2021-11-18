543603da026e88f8455f17567e0e6db9040d50b0
SliverHorn <503551462@qq.com>
2021-11-18 23:13:17 +0800

    fixed: - pgsql 修复初始化数据失败的bug - pgsql 获取角色菜单时 报错 - SysOperationRecord longtext 改为 text

--- server/service/system/sys_initdb.go
+++ server/service/system/sys_initdb.go
@@ -1,6 +1,8 @@
 package system
 
 import (
+	"database/sql"
+	"fmt"
 	adapter "github.com/casbin/gorm-adapter/v3"
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/example"
@@ -26,7 +28,7 @@ func (initDBService *InitDBService) InitDB(conf request.InitDB) error {
 }
 
 // initTables 初始化表
-// Author SliverHorn
+// Author [SliverHorn](https://github.com/SliverHorn)
 func (initDBService *InitDBService) initTables() error {
 	return global.GVA_DB.AutoMigrate(
 		system.SysApi{},
@@ -48,4 +50,26 @@ func (initDBService *InitDBService) initTables() error {
 		example.ExaFileUploadAndDownload{},
 	)
 
+}
+
+// createDatabase 创建数据库(mysql)
+// Author [SliverHorn](https://github.com/SliverHorn)
+// Author: [songzhibin97](https://github.com/songzhibin97)
+
+func (initDBService *InitDBService) createDatabase(dsn string, driver string, createSql string) error {
+	db, err := sql.Open(driver, dsn)
+	if err != nil {
+		return err
+	}
+	defer func(db *sql.DB) {
+		err = db.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(db)
+	if err = db.Ping(); err != nil {
+		return err
+	}
+	_, err = db.Exec(createSql)
+	return err
 }

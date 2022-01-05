5c00eb8e3361c37151fdc1d24a01fe9b6634a045
piexlmax <qimiaojiangjizhao@gmail.com>
2022-01-05 14:29:13 +0800

    修复删除气泡取消后无法再次弹出的问题

--- web/src/view/superAdmin/operation/sysOperationRecord.vue
+++ web/src/view/superAdmin/operation/sysOperationRecord.vue
@@ -91,7 +91,7 @@
         </el-table-column>
         <el-table-column align="left" label="按钮组">
           <template #default="scope">
-            <el-popover :visible="scope.row.visible" placement="top" width="160">
+            <el-popover v-model:visible="scope.row.visible" placement="top" width="160">
               <p>确定要删除吗？</p>
               <div style="text-align: right; margin-top: 8px;">
                 <el-button size="mini" type="text" @click="scope.row.visible = false">取消</el-button>

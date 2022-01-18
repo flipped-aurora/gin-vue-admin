a8141bda8959470b475ac94ca74380be11acc37e
songzhibin97 <718428482@qq.com>
2022-01-18 16:02:54 +0800

    feat:swagger 优化

--- server/api/v1/example/exa_breakpoint_continue.go
+++ server/api/v1/example/exa_breakpoint_continue.go
@@ -2,10 +2,11 @@ package example
 
 import (
 	"fmt"
-	"github.com/flipped-aurora/gin-vue-admin/server/model/example"
 	"io/ioutil"
 	"mime/multipart"
 	"strconv"
+
+	"github.com/flipped-aurora/gin-vue-admin/server/model/example"
 
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/common/response"
@@ -21,7 +22,7 @@ import (
 // @accept multipart/form-data
 // @Produce  application/json
 // @Param file formData file true "an example for breakpoint resume, 断点续传示例"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"切片创建成功"}"
+// @Success 200 {object} response.Response{msg=string} "断点续传到服务器"
 // @Router /fileUploadAndDownload/breakpointContinue [post]
 func (u *FileUploadAndDownloadApi) BreakpointContinue(c *gin.Context) {
 	fileMd5 := c.Request.FormValue("fileMd5")
@@ -80,7 +81,7 @@ func (u *FileUploadAndDownloadApi) BreakpointContinue(c *gin.Context) {
 // @accept multipart/form-data
 // @Produce  application/json
 // @Param file formData file true "Find the file, 查找文件"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"查找成功"}"
+// @Success 200 {object} response.Response{data=exampleRes.FileResponse,msg=string} "查找文件,返回包括文件详情"
 // @Router /fileUploadAndDownload/findFile [post]
 func (u *FileUploadAndDownloadApi) FindFile(c *gin.Context) {
 	fileMd5 := c.Query("fileMd5")
@@ -101,7 +102,7 @@ func (u *FileUploadAndDownloadApi) FindFile(c *gin.Context) {
 // @accept multipart/form-data
 // @Produce  application/json
 // @Param file formData file true "上传文件完成"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"file uploaded, 文件创建成功"}"
+// @Success 200 {object} response.Response{data=exampleRes.FilePathResponse,msg=string} "创建文件,返回包括文件路径"
 // @Router /fileUploadAndDownload/findFile [post]
 func (b *FileUploadAndDownloadApi) BreakpointContinueFinish(c *gin.Context) {
 	fileMd5 := c.Query("fileMd5")
@@ -121,14 +122,14 @@ func (b *FileUploadAndDownloadApi) BreakpointContinueFinish(c *gin.Context) {
 // @accept multipart/form-data
 // @Produce  application/json
 // @Param file formData file true "删除缓存切片"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"缓存切片删除成功"}"
+// @Success 200 {object} response.Response{msg=string} "删除切片"
 // @Router /fileUploadAndDownload/removeChunk [post]
 func (u *FileUploadAndDownloadApi) RemoveChunk(c *gin.Context) {
 	var file example.ExaFile
 	c.ShouldBindJSON(&file)
 	err := utils.RemoveChunk(file.FileMd5)
 	if err != nil {
-		response.FailWithMessage(err.Error(), c)
+		global.GVA_LOG.Error("缓存切片删除失败!", zap.Error(err))
 		return
 	}
 	err = fileUploadAndDownloadService.DeleteFileChunk(file.FileMd5, file.FileName, file.FilePath)
--- server/api/v1/system/sys_operation_record.go
+++ server/api/v1/system/sys_operation_record.go
@@ -19,7 +19,7 @@ type OperationRecordApi struct{}
 // @accept application/json
 // @Produce application/json
 // @Param data body system.SysOperationRecord true "创建SysOperationRecord"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
+// @Success 200 {object} response.Response{msg=string} "创建SysOperationRecord"
 // @Router /sysOperationRecord/createSysOperationRecord [post]
 func (s *OperationRecordApi) CreateSysOperationRecord(c *gin.Context) {
 	var sysOperationRecord system.SysOperationRecord
@@ -38,7 +38,7 @@ func (s *OperationRecordApi) CreateSysOperationRecord(c *gin.Context) {
 // @accept application/json
 // @Produce application/json
 // @Param data body system.SysOperationRecord true "SysOperationRecord模型"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"删除成功"}"
+// @Success 200 {object} response.Response{msg=string} "删除SysOperationRecord"
 // @Router /sysOperationRecord/deleteSysOperationRecord [delete]
 func (s *OperationRecordApi) DeleteSysOperationRecord(c *gin.Context) {
 	var sysOperationRecord system.SysOperationRecord
@@ -57,7 +57,7 @@ func (s *OperationRecordApi) DeleteSysOperationRecord(c *gin.Context) {
 // @accept application/json
 // @Produce application/json
 // @Param data body request.IdsReq true "批量删除SysOperationRecord"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"批量删除成功"}"
+// @Success 200 {object} response.Response{msg=string} "批量删除SysOperationRecord"
 // @Router /sysOperationRecord/deleteSysOperationRecordByIds [delete]
 func (s *OperationRecordApi) DeleteSysOperationRecordByIds(c *gin.Context) {
 	var IDS request.IdsReq
@@ -76,7 +76,7 @@ func (s *OperationRecordApi) DeleteSysOperationRecordByIds(c *gin.Context) {
 // @accept application/json
 // @Produce application/json
 // @Param data query system.SysOperationRecord true "Id"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"查询成功"}"
+// @Success 200 {object} response.Response{data=map[string]interface{},msg=string} "用id查询SysOperationRecord"
 // @Router /sysOperationRecord/findSysOperationRecord [get]
 func (s *OperationRecordApi) FindSysOperationRecord(c *gin.Context) {
 	var sysOperationRecord system.SysOperationRecord
@@ -99,7 +99,7 @@ func (s *OperationRecordApi) FindSysOperationRecord(c *gin.Context) {
 // @accept application/json
 // @Produce application/json
 // @Param data query request.SysOperationRecordSearch true "页码, 每页大小, 搜索条件"
-// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
+// @Success 200 {object} response.Response{data=response.PageResult,msg=string} "分页获取SysOperationRecord列表,返回包括列表,总数,页码,每页数量"
 // @Router /sysOperationRecord/getSysOperationRecordList [get]
 func (s *OperationRecordApi) GetSysOperationRecordList(c *gin.Context) {
 	var pageInfo systemReq.SysOperationRecordSearch

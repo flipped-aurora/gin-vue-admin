160d0d87ac5926aba25fd1c126ce03708b62f529
piexlmax <303176530@qq.com>
2022-02-14 10:45:19 +0800

    修复log-mode失效的问题

--- server/config/gorm_mysql.go
+++ server/config/gorm_mysql.go
@@ -16,3 +16,7 @@ type Mysql struct {
 func (m *Mysql) Dsn() string {
 	return m.Username + ":" + m.Password + "@tcp(" + m.Path + ":" + m.Port + ")/" + m.Dbname + "?" + m.Config
 }
+
+func (m *Mysql) GetLogMode() string {
+	return m.LogMode
+}

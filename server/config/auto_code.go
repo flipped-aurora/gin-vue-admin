a68b2c51cd3173e94546cdac992a6c506899fdbd
songzhibin97 <718428482@qq.com>
2022-03-27 16:52:11 +0800

    fix:977

--- server/config/auto_code.go
+++ server/config/auto_code.go
@@ -1,17 +1,17 @@
 package config
 
 type Autocode struct {
-	TransferRestart bool   `mapstructure:"transfer-restart" json:"transferRestart" yaml:"transfer-restart"`
+	TransferRestart bool   `mapstructure:"transfer-restart" json:"transfer-restart" yaml:"transfer-restart"`
 	Root            string `mapstructure:"root" json:"root" yaml:"root"`
 	Server          string `mapstructure:"server" json:"server" yaml:"server"`
-	SApi            string `mapstructure:"server-api" json:"serverApi" yaml:"server-api"`
-	SInitialize     string `mapstructure:"server-initialize" json:"serverInitialize" yaml:"server-initialize"`
-	SModel          string `mapstructure:"server-model" json:"serverModel" yaml:"server-model"`
-	SRequest        string `mapstructure:"server-request" json:"serverRequest"  yaml:"server-request"`
-	SRouter         string `mapstructure:"server-router" json:"serverRouter" yaml:"server-router"`
-	SService        string `mapstructure:"server-service" json:"serverService" yaml:"server-service"`
+	SApi            string `mapstructure:"server-api" json:"server-api" yaml:"server-api"`
+	SInitialize     string `mapstructure:"server-initialize" json:"server-initialize" yaml:"server-initialize"`
+	SModel          string `mapstructure:"server-model" json:"server-model" yaml:"server-model"`
+	SRequest        string `mapstructure:"server-request" json:"server-request"  yaml:"server-request"`
+	SRouter         string `mapstructure:"server-router" json:"server-router" yaml:"server-router"`
+	SService        string `mapstructure:"server-service" json:"server-service" yaml:"server-service"`
 	Web             string `mapstructure:"web" json:"web" yaml:"web"`
-	WApi            string `mapstructure:"web-api" json:"webApi" yaml:"web-api"`
-	WForm           string `mapstructure:"web-form" json:"webForm" yaml:"web-form"`
-	WTable          string `mapstructure:"web-table" json:"webTable" yaml:"web-table"`
+	WApi            string `mapstructure:"web-api" json:"web-api" yaml:"web-api"`
+	WForm           string `mapstructure:"web-form" json:"web-form" yaml:"web-form"`
+	WTable          string `mapstructure:"web-table" json:"web-table" yaml:"web-table"`
 }
--- server/config/config.go
+++ server/config/config.go
@@ -9,7 +9,7 @@ type Server struct {
 	System  System  `mapstructure:"system" json:"system" yaml:"system"`
 	Captcha Captcha `mapstructure:"captcha" json:"captcha" yaml:"captcha"`
 	// auto
-	AutoCode Autocode `mapstructure:"autoCode" json:"autoCode" yaml:"autoCode"`
+	AutoCode Autocode `mapstructure:"autocode" json:"autocode" yaml:"autocode"`
 	// gorm
 	Mysql  Mysql `mapstructure:"mysql" json:"mysql" yaml:"mysql"`
 	Pgsql  Pgsql `mapstructure:"pgsql" json:"pgsql" yaml:"pgsql"`
@@ -17,10 +17,10 @@ type Server struct {
 	// oss
 	Local      Local      `mapstructure:"local" json:"local" yaml:"local"`
 	Qiniu      Qiniu      `mapstructure:"qiniu" json:"qiniu" yaml:"qiniu"`
-	AliyunOSS  AliyunOSS  `mapstructure:"aliyun-oss" json:"aliyunOSS" yaml:"aliyun-oss"`
-	HuaWeiObs  HuaWeiObs  `mapstructure:"hua-wei-obs" json:"huaWeiObs" yaml:"hua-wei-obs"`
-	TencentCOS TencentCOS `mapstructure:"tencent-cos" json:"tencentCOS" yaml:"tencent-cos"`
-	AwsS3      AwsS3      `mapstructure:"aws-s3" json:"awsS3" yaml:"aws-s3"`
+	AliyunOSS  AliyunOSS  `mapstructure:"aliyun-oss" json:"aliyun-oss" yaml:"aliyun-oss"`
+	HuaWeiObs  HuaWeiObs  `mapstructure:"hua-wei-obs" json:"hua-wei-obs" yaml:"hua-wei-obs"`
+	TencentCOS TencentCOS `mapstructure:"tencent-cos" json:"tencent-cos" yaml:"tencent-cos"`
+	AwsS3      AwsS3      `mapstructure:"aws-s3" json:"aws-s3" yaml:"aws-s3"`
 
 	Excel Excel `mapstructure:"excel" json:"excel" yaml:"excel"`
 	Timer Timer `mapstructure:"timer" json:"timer" yaml:"timer"`

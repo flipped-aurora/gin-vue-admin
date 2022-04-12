15ed61d9e9d1a47a8fd907c86f47494037d90d87
piexlmax <303176530@qq.com>
2022-04-12 17:55:11 +0800

    微调媒体库样式

--- web/src/components/chooseImg/index.vue
+++ web/src/components/chooseImg/index.vue
@@ -1,19 +1,18 @@
 <template>
-  <el-drawer v-model="drawer" title="媒体库">
+  <el-drawer v-model="drawer" title="媒体库" size="650px">
     <div class="gva-btn-list">
       <upload-common
-          v-model:imageCommon="imageCommon"
-          class="upload-btn-media-library"
-          @on-success="open"
+        v-model:imageCommon="imageCommon"
+        class="upload-btn-media-library"
+        @on-success="open"
       />
       <upload-image
-          v-model:imageUrl="imageUrl"
-          :file-size="512"
-          :max-w-h="1080"
-          class="upload-btn-media-library"
-          @on-success="open"
+        v-model:imageUrl="imageUrl"
+        :file-size="512"
+        :max-w-h="1080"
+        class="upload-btn-media-library"
+        @on-success="open"
       />
-
       <el-form ref="searchForm" :inline="true" :model="search">
         <el-form-item label="">
           <el-input v-model="search.keyword" class="keyword" placeholder="请输入文件名或备注" />
@@ -23,36 +22,35 @@
           <el-button size="small" type="primary" icon="search" @click="open">查询</el-button>
         </el-form-item>
       </el-form>
-
     </div>
     <div class="media">
-      <div class="media-box"  v-for="(item,key) in picList">
-      <div class="header-img-box-list">
-        <el-image
+      <div v-for="(item,key) in picList" :key="key" class="media-box">
+        <div class="header-img-box-list">
+          <el-image
             :key="key"
             :src="(item.url && item.url.slice(0, 4) !== 'http')?path+item.url:item.url"
             @click="chooseImg(item.url,target,targetKey)"
-        >
-          <template #error>
-            <div class="header-img-box-list">
-              <el-icon>
-                <picture/>
-              </el-icon>
-            </div>
-          </template>
-        </el-image>
-      </div>
-        <div class="img-title" @click="editFileNameFunc(item)">{{item.name}}</div>
+          >
+            <template #error>
+              <div class="header-img-box-list">
+                <el-icon>
+                  <picture />
+                </el-icon>
+              </div>
+            </template>
+          </el-image>
+        </div>
+        <div class="img-title" @click="editFileNameFunc(item)">{{ item.name }}</div>
       </div>
     </div>
     <el-pagination
-        :current-page="page"
-        :page-size="pageSize"
-        :total="total"
-        :style="{'justify-content':'center'}"
-        layout="total, prev, pager, next, jumper"
-        @current-change="handleCurrentChange"
-        @size-change="handleSizeChange"
+      :current-page="page"
+      :page-size="pageSize"
+      :total="total"
+      :style="{'justify-content':'center'}"
+      layout="total, prev, pager, next, jumper"
+      @current-change="handleCurrentChange"
+      @size-change="handleSizeChange"
     />
   </el-drawer>
 </template>
@@ -131,8 +129,8 @@ const editFileNameFunc = async(row) => {
     inputPattern: /\S/,
     inputErrorMessage: '不能为空'
   }).then(async({ value }) => {
-    row.name = value;
-    //console.log(row)
+    row.name = value
+    // console.log(row)
     const res = await editFileName(row)
     if (res.code === 0) {
       ElMessage({
@@ -145,8 +143,8 @@ const editFileNameFunc = async(row) => {
     ElMessage({
       type: 'info',
       message: '取消修改'
-    });
-  });
+    })
+  })
 }
 
 defineExpose({ open })
@@ -182,10 +180,10 @@ defineExpose({ open })
       text-align: center;
       line-height: 120px;
       cursor: pointer;
-
+      overflow: hidden;
       .el-image__inner {
-        max-width: 118px;
-        max-height: 118px;
+        max-width: 120px;
+        max-height: 120px;
         vertical-align: middle;
         width: unset;
         height: unset;

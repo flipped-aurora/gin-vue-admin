19ab06c8b2d5071f6d968e661308af2e1c4418f6
hedeqiang <laravel_code@163.com>
2022-04-22 12:36:56 +0800

    Format Code

--- server/source/system/user.go
+++ server/source/system/user.go
@@ -49,8 +49,25 @@ func (i *initUser) InitializeData(ctx context.Context) (next context.Context, er
 	adminPassword := utils.BcryptHash("123456")
 
 	entities := []sysModel.SysUser{
-		{UUID: uuid.NewV4(), Username: "admin", Password: adminPassword, NickName: "超级管理员", HeaderImg: "https://qmplusimg.henrongyi.top/gva_header.jpg", AuthorityId: "888", Phone: "17611111111", Email: "333333333@qq.com"},
-		{UUID: uuid.NewV4(), Username: "a303176530", Password: password, NickName: "QMPlusUser", HeaderImg: "https:///qmplusimg.henrongyi.top/1572075907logo.png", AuthorityId: "9528", Phone: "17611111111", Email: "333333333@qq.com"},
+		{
+			UUID:        uuid.NewV4(),
+			Username:    "admin",
+			Password:    adminPassword,
+			NickName:    "超级管理员",
+			HeaderImg:   "https://qmplusimg.henrongyi.top/gva_header.jpg",
+			AuthorityId: "888",
+			Phone:       "17611111111",
+			Email:       "333333333@qq.com",
+		},
+		{
+			UUID:        uuid.NewV4(),
+			Username:    "a303176530",
+			Password:    password,
+			NickName:    "QMPlusUser",
+			HeaderImg:   "https:///qmplusimg.henrongyi.top/1572075907logo.png",
+			AuthorityId: "9528",
+			Phone:       "17611111111",
+			Email:       "333333333@qq.com"},
 	}
 	if err = global.GVA_DB.Create(&entities).Error; err != nil {
 		return ctx, errors.Wrap(err, sysModel.SysUser{}.TableName()+"表数据初始化失败!")

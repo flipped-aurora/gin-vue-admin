ff913962bc5b04bb14e064cb1f7d3b2e66487af4
星冉 <troy@zygd.site>
2022-04-28 21:28:26 +0800

    改进优化

--- web/src/core/config.js
+++ web/src/core/config.js
@@ -28,27 +28,27 @@ export const viteLogo = (env) => {
     )
     console.log(
       chalk.green(
-        `> GVA讨论社区：https://support.qq.com/products/371961`
+        `> GVA讨论社区: https://support.qq.com/products/371961`
       )
     )
     console.log(
       chalk.green(
-        `> 默认自动化文档地址:http://127.0.0.1:${env.VITE_SERVER_PORT}/swagger/index.html`
+        `> 默认自动化文档地址: http://127.0.0.1:${env.VITE_SERVER_PORT}/swagger/index.html`
       )
     )
     console.log(
       chalk.green(
-        `> 默认自动化文档地址:http://127.0.0.1:${env.VITE_SERVER_PORT}/swagger/index.html`
+        `> 默认自动化文档地址: http://127.0.0.1:${env.VITE_SERVER_PORT}/swagger/index.html`
       )
     )
     console.log(
       chalk.green(
-        `> 默认前端文件运行地址:http://127.0.0.1:${env.VITE_CLI_PORT}`
+        `> 默认前端文件运行地址: http://127.0.0.1:${env.VITE_CLI_PORT}`
       )
     )
     console.log(
       chalk.green(
-        `> 如果项目让您获得了收益，希望您能请团队喝杯可乐:https://www.github.com/flipped-aurora/gin-vue-admin/server.com/docs/coffee`
+        `> 如果项目让您获得了收益，希望您能请团队喝杯可乐: https://www.github.com/flipped-aurora/gin-vue-admin/server.com/docs/coffee`
       )
     )
     console.log('\n')

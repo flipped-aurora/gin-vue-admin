84f35f55d431e3ae2ab243d3ed092fea1f05fcc4
pnck <hio131@gmail.com>
2022-05-22 17:52:14 +0800

    fix: 修复多库模式不能使用 pg 的问题

--- server/model/system/request/sys_init.go
+++ server/model/system/request/sys_init.go
@@ -43,15 +43,17 @@ func (i *InitDB) PgsqlEmptyDsn() string {
 // Author [SliverHorn](https://github.com/SliverHorn)
 func (i *InitDB) ToMysqlConfig() config.Mysql {
 	return config.Mysql{
-		Path:         i.Host,
-		Port:         i.Port,
-		Dbname:       i.DBName,
-		Username:     i.UserName,
-		Password:     i.Password,
-		MaxIdleConns: 10,
-		MaxOpenConns: 100,
-		LogMode:      "error",
-		Config:       "charset=utf8mb4&parseTime=True&loc=Local",
+		GeneralDB: config.GeneralDB{
+			Path:         i.Host,
+			Port:         i.Port,
+			Dbname:       i.DBName,
+			Username:     i.UserName,
+			Password:     i.Password,
+			MaxIdleConns: 10,
+			MaxOpenConns: 100,
+			LogMode:      "error",
+			Config:       "charset=utf8mb4&parseTime=True&loc=Local",
+		},
 	}
 }
 
@@ -59,14 +61,16 @@ func (i *InitDB) ToMysqlConfig() config.Mysql {
 // Author [SliverHorn](https://github.com/SliverHorn)
 func (i *InitDB) ToPgsqlConfig() config.Pgsql {
 	return config.Pgsql{
-		Path:         i.Host,
-		Port:         i.Port,
-		Dbname:       i.DBName,
-		Username:     i.UserName,
-		Password:     i.Password,
-		MaxIdleConns: 10,
-		MaxOpenConns: 100,
-		LogMode:      "error",
-		Config:       "sslmode=disable TimeZone=Asia/Shanghai",
+		GeneralDB: config.GeneralDB{
+			Path:         i.Host,
+			Port:         i.Port,
+			Dbname:       i.DBName,
+			Username:     i.UserName,
+			Password:     i.Password,
+			MaxIdleConns: 10,
+			MaxOpenConns: 100,
+			LogMode:      "error",
+			Config:       "sslmode=disable TimeZone=Asia/Shanghai",
+		},
 	}
 }

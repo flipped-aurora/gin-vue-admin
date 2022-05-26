b86799ccddc68b5d44274d7a76ed17e422b2de52
pnck <hio131@gmail.com>
2022-05-26 14:45:20 +0800

    一些小修复 (#1085)
    * chore: 去掉初始化中误加的额外表
    
    * chore: 修复初始化不应使用的GVA_DB

--- server/model/system/sys_base_menu.go
+++ server/model/system/sys_base_menu.go
@@ -6,18 +6,18 @@ import (
 
 type SysBaseMenu struct {
 	global.GVA_MODEL
-	MenuLevel     uint                              `json:"-"`
-	ParentId      string                            `json:"parentId" gorm:"comment:父菜单ID"`     // 父菜单ID
-	Path          string                            `json:"path" gorm:"comment:路由path"`        // 路由path
-	Name          string                            `json:"name" gorm:"comment:路由name"`        // 路由name
-	Hidden        bool                              `json:"hidden" gorm:"comment:是否在列表隐藏"`     // 是否在列表隐藏
-	Component     string                            `json:"component" gorm:"comment:对应前端文件路径"` // 对应前端文件路径
-	Sort          int                               `json:"sort" gorm:"comment:排序标记"`          // 排序标记
-	Meta          `json:"meta" gorm:"comment:附加属性"` // 附加属性
-	SysAuthoritys []SysAuthority                    `json:"authoritys" gorm:"many2many:sys_authority_menus;"`
-	Children      []SysBaseMenu                     `json:"children" gorm:"-"`
-	Parameters    []SysBaseMenuParameter            `json:"parameters"`
-	MenuBtn       []SysBaseMenuBtn                  `json:"menuBtn"`
+	MenuLevel     uint                                       `json:"-"`
+	ParentId      string                                     `json:"parentId" gorm:"comment:父菜单ID"`     // 父菜单ID
+	Path          string                                     `json:"path" gorm:"comment:路由path"`        // 路由path
+	Name          string                                     `json:"name" gorm:"comment:路由name"`        // 路由name
+	Hidden        bool                                       `json:"hidden" gorm:"comment:是否在列表隐藏"`     // 是否在列表隐藏
+	Component     string                                     `json:"component" gorm:"comment:对应前端文件路径"` // 对应前端文件路径
+	Sort          int                                        `json:"sort" gorm:"comment:排序标记"`          // 排序标记
+	Meta          `json:"meta" gorm:"embedded;comment:附加属性"` // 附加属性
+	SysAuthoritys []SysAuthority                             `json:"authoritys" gorm:"many2many:sys_authority_menus;"`
+	Children      []SysBaseMenu                              `json:"children" gorm:"-"`
+	Parameters    []SysBaseMenuParameter                     `json:"parameters"`
+	MenuBtn       []SysBaseMenuBtn                           `json:"menuBtn"`
 }
 
 type Meta struct {

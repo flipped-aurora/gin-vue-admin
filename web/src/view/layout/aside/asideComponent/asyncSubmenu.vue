73d31d9edec419c59439981dcd62f0105be89ee3
piexlmax <303176530@qq.com>
2022-05-28 23:42:45 +0800

    添加侧边栏样式,增加变量配置,方便自定义主题

--- web/src/view/layout/aside/asideComponent/asyncSubmenu.vue
+++ web/src/view/layout/aside/asideComponent/asyncSubmenu.vue
@@ -17,12 +17,49 @@ export default {
 </script>
 
 <script setup>
-defineProps({
+import { ref, watch } from 'vue'
+const props = defineProps({
   routerInfo: {
     default: function() {
       return null
     },
     type: Object
+  },
+  isCollapse: {
+    default: function() {
+      return false
+    },
+    type: Boolean
+  },
+  theme: {
+    default: function() {
+      return {}
+    },
+    type: Object
   }
 })
+
+// const activeBackground = ref(props.theme.activeBackground)
+// const activeText = ref(props.theme.activeText)
+const normalText = ref(props.theme.normalText)
+// const hoverBackground = ref(props.theme.hoverBackground)
+// const hoverText = ref(props.theme.hoverText)
+
+watch(() => props.theme, () => {
+  // activeBackground.value = props.theme.activeBackground
+  // activeText.value = props.theme.activeText
+  normalText.value = props.theme.normalText
+  // hoverBackground.value = props.theme.hoverBackground
+  // hoverText.value = props.theme.hoverText
+})
+
 </script>
+
+<style lang="scss" scoped>
+.el-sub-menu{
+  ::v-deep(.el-sub-menu__title){
+      color: v-bind(normalText);
+  }
+}
+
+</style>

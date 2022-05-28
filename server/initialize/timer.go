d761210f74a97f74bba9572e3356a909a8a87ab9
songzhibin97 <718428482@qq.com>
2022-05-28 14:22:42 +0800

    feat: timer add second

--- server/initialize/timer.go
+++ server/initialize/timer.go
@@ -12,12 +12,15 @@ func Timer() {
 	if global.GVA_CONFIG.Timer.Start {
 		for i := range global.GVA_CONFIG.Timer.Detail {
 			go func(detail config.Detail) {
-				global.GVA_Timer.AddTaskByFunc("ClearDB", global.GVA_CONFIG.Timer.Spec, func() {
+				_, err := global.GVA_Timer.AddTaskByFunc("ClearDB", global.GVA_CONFIG.Timer.Spec, func() {
 					err := utils.ClearTable(global.GVA_DB, detail.TableName, detail.CompareField, detail.Interval)
 					if err != nil {
 						fmt.Println("timer error:", err)
 					}
 				})
+				if err != nil {
+					fmt.Println("add timer error:", err)
+				}
 			}(global.GVA_CONFIG.Timer.Detail[i])
 		}
 	}

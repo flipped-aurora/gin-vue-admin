ba98288a993ec5fd2a05e91f14cf76e7951a43e5
SliverHorn <503551462@qq.com>
2022-05-30 22:33:14 +0800

    feature: viper 读取gin自带的环境变量GIN_MODE,用于区分开发,测试,生产三个环境 #1098 (#1105)

---
+++ server/core/internal/constant.go
@@ -0,0 +1,9 @@
+package internal
+
+const (
+	ConfigEnv         = "GVA_CONFIG"
+	ConfigDefaultFile = "config.yaml"
+	ConfigTestFile    = "config.test.yaml"
+	ConfigDebugFile   = "config.debug.yaml"
+	ConfigReleaseFile = "config.release.yaml"
+)

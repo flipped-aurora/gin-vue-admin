fa9ff643192f841318252ff58ae893ab99d736f9
lin93h <1098039601@qq.com>
2022-05-30 21:44:45 +0800

    解决historyComponent无法删除问题 (#1108)
    * 修复historyComponent无法删除问题
    
    Co-authored-by: 奇淼（piexlmax <qimiaojiangjizhao@gmail.com>

--- web/src/view/layout/aside/historyComponent/history.vue
+++ web/src/view/layout/aside/historyComponent/history.vue
@@ -217,7 +217,7 @@ const removeTab = (tab) => {
   const index = historys.value.findIndex(
     (item) => getFmtString(item) === tab
   )
-  if (getFmtString(route) === tab) {
+  if (getFmtString(route) === tab)  {
     if (historys.value.length === 1) {
       router.push({ name: defaultRouter.value })
     } else {
@@ -259,6 +259,12 @@ watch(route, (to, now) => {
   setTab(to)
   sessionStorage.setItem('historys', JSON.stringify(historys.value))
   activeValue.value = window.sessionStorage.getItem('activeValue')
+})
+
+watch(() => historys.value, () => {
+  sessionStorage.setItem('historys', JSON.stringify(historys.value))
+}, {
+  deep: true
 })
 
 const initPage = () => {

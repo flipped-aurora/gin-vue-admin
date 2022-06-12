d66f82a1149044791241a01a5d316f02eba8dfb9
piexlmax <303176530@qq.com>
2022-06-12 15:34:47 +0800

    增加详情页签区分功能

---
+++ web/src/utils/fmtRouterTitle.js
@@ -0,0 +1,14 @@
+export const fmtTitle = (title, now) => {
+  const reg = /\$\{(.+?)\}/
+  const reg_g = /\$\{(.+?)\}/g
+  const result = title.match(reg_g)
+  console.log('result=', result)
+  if (result) {
+    result.forEach((item) => {
+      const key = item.match(reg)[1]
+      const value = now.params[key] || now.query[key]
+      title = title.replace(item, value)
+    })
+  }
+  return title
+}

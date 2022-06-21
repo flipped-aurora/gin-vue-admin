4fc265265b51e78d7a0fa52acebed10adf1c4075
yancunju <yancunju@autouc.com>
2022-06-21 18:48:35 +0800

    config/zap.go:fix "max-age" 错误

--- server/config/zap.go
+++ server/config/zap.go
@@ -1,8 +1,9 @@
 package config
 
 import (
-	"go.uber.org/zap/zapcore"
 	"strings"
+
+	"go.uber.org/zap/zapcore"
 )
 
 type Zap struct {
@@ -13,7 +14,7 @@ type Zap struct {
 	EncodeLevel   string `mapstructure:"encode-level" json:"encode-level" yaml:"encode-level"`       // 编码级
 	StacktraceKey string `mapstructure:"stacktrace-key" json:"stacktrace-key" yaml:"stacktrace-key"` // 栈名
 
-	MaxAge       int  `mapstructure:"mintage" json:"max-age" yaml:"max-age"`                      // 日志留存时间
+	MaxAge       int  `mapstructure:"max-age" json:"max-age" yaml:"max-age"`                      // 日志留存时间
 	ShowLine     bool `mapstructure:"show-line" json:"show-line" yaml:"show-line"`                // 显示行
 	LogInConsole bool `mapstructure:"log-in-console" json:"log-in-console" yaml:"log-in-console"` // 输出控制台
 }

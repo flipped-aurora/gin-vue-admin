1fb461934dcfdfd3d8a6739ef1c6f0b200f1f70c
spartacus <59353073@qq.com>
2022-06-22 11:39:55 +0800

    web/src/view/systemTools/system/system.vue定时任务复选框和字段修改 (#1131)

--- web/src/view/systemTools/system/system.vue
+++ web/src/view/systemTools/system/system.vue
@@ -324,7 +324,7 @@
 
         <el-collapse-item title="Timer(定时任务)" name="13">
           <el-form-item label="Start（是否启用）">
-            <el-select v-model="config.timer['w-table']" />
+            <el-checkbox v-model="config.timer['start']" />
           </el-form-item>
           <el-form-item label="Spec(CRON表达式)">
             <el-input v-model="config.timer.spec" />

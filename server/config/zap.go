cba2ed5424217ee8fa18d24ef8ffac1a42801ba5
yancunju <yancunju@autouc.com>
2022-06-22 12:26:45 +0800

    server/config/zap.go: fix max-age 名称错误

--- server/config/zap.go
+++ server/config/zap.go
@@ -13,7 +13,7 @@ type Zap struct {
 	EncodeLevel   string `mapstructure:"encode-level" json:"encode-level" yaml:"encode-level"`       // 编码级
 	StacktraceKey string `mapstructure:"stacktrace-key" json:"stacktrace-key" yaml:"stacktrace-key"` // 栈名
 
-	MaxAge       int  `mapstructure:"mintage" json:"max-age" yaml:"max-age"`                      // 日志留存时间
+	MaxAge       int  `mapstructure:"max-age" json:"max-age" yaml:"max-age"`                      // 日志留存时间
 	ShowLine     bool `mapstructure:"show-line" json:"show-line" yaml:"show-line"`                // 显示行
 	LogInConsole bool `mapstructure:"log-in-console" json:"log-in-console" yaml:"log-in-console"` // 输出控制台
 }

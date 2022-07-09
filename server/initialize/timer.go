d52d90917109847637ca14ac055f0fdb6b5a6d7f
奇淼（piexlmax <303176530@qq.com>
2022-07-09 14:27:04 +0800

    自动插件模式鉴定调整 (#1161)
    * fix: #1160
    
    * 自动插件模式鉴定调整
    
    Co-authored-by: songzhibin97 <718428482@qq.com>

--- server/initialize/timer.go
+++ server/initialize/timer.go
@@ -3,6 +3,8 @@ package initialize
 import (
 	"fmt"
 
+	"github.com/robfig/cron/v3"
+
 	"github.com/flipped-aurora/gin-vue-admin/server/config"
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/utils"
@@ -12,12 +14,16 @@ func Timer() {
 	if global.GVA_CONFIG.Timer.Start {
 		for i := range global.GVA_CONFIG.Timer.Detail {
 			go func(detail config.Detail) {
+				var option []cron.Option
+				if global.GVA_CONFIG.Timer.WithSeconds {
+					option = append(option, cron.WithSeconds())
+				}
 				_, err := global.GVA_Timer.AddTaskByFunc("ClearDB", global.GVA_CONFIG.Timer.Spec, func() {
 					err := utils.ClearTable(global.GVA_DB, detail.TableName, detail.CompareField, detail.Interval)
 					if err != nil {
 						fmt.Println("timer error:", err)
 					}
-				})
+				}, option...)
 				if err != nil {
 					fmt.Println("add timer error:", err)
 				}

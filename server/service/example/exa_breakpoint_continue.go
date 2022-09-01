540ff058c5f5ffb79a4f461db8047f772dcaacff
myface-wang <240298530@qq.com>
2022-09-01 17:11:20 +0800

    优化，合并update方法 (#1212)

--- server/service/example/exa_breakpoint_continue.go
+++ server/service/example/exa_breakpoint_continue.go
@@ -56,7 +56,11 @@ func (e *FileUploadAndDownloadService) CreateFileChunk(id uint, fileChunkPath st
 func (e *FileUploadAndDownloadService) DeleteFileChunk(fileMd5 string, filePath string) error {
 	var chunks []example.ExaFileChunk
 	var file example.ExaFile
-	err := global.GVA_DB.Where("file_md5 = ? ", fileMd5).First(&file).Update("IsFinish", true).Update("file_path", filePath).Error
+	err := global.GVA_DB.Where("file_md5 = ? ", fileMd5).First(&file).
+		Updates(map[string]interface{}{
+			"IsFinish":  true,
+			"file_path": filePath,
+		}).Error
 	if err != nil {
 		return err
 	}

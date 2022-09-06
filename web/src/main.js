aa81a2925713f8a66409d226d9cbc7b9e1ae95ea
ipanghu <bypanghu@163.com>
2022-09-06 10:17:05 +0800

    [Feature] 新增页面切换进度条 (#1213)
    新增页面切换进度条

--- web/src/main.js
+++ web/src/main.js
@@ -12,6 +12,19 @@ import run from '@/core/gin-vue-admin.js'
 import auth from '@/directive/auth'
 import { store } from '@/pinia'
 import App from './App.vue'
+/**
+ * @description 导入加载进度条，防止首屏加载时间过长，用户等待
+ *
+ * */
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+Nprogress.configure({ showSpinner: false, ease: 'ease', speed: 500 })
+Nprogress.start()
+
+/**
+ * 无需在这块结束，会在路由中间件中结束此块内容
+ * */
+
 const app = createApp(App)
 app.config.productionTip = false
 

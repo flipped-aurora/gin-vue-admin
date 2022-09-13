c99d8094a36e159b4621f489da1be3268bb4bd85
奇淼（piexlmax <303176530@qq.com>
2022-09-13 16:26:02 +0800

    整理配置文件，jwt过期时间和缓冲时间支持1d2h3m4s事件类型 (#1218)
    * feat: human duration
    
    * Update config.yaml
    
    * 调整配置文件结构
    
    Co-authored-by: songzhibin97 <718428482@qq.com>

--- server/main.go
+++ server/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"go.uber.org/zap"
+
 	"github.com/flipped-aurora/gin-vue-admin/server/core"
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/initialize"
-	"go.uber.org/zap"
 )
 
 //go:generate go env -w GO111MODULE=on
@@ -21,7 +22,8 @@ import (
 // @BasePath /
 func main() {
 	global.GVA_VP = core.Viper() // 初始化Viper
-	global.GVA_LOG = core.Zap()  // 初始化zap日志库
+	initialize.OtherInit()
+	global.GVA_LOG = core.Zap() // 初始化zap日志库
 	zap.ReplaceGlobals(global.GVA_LOG)
 	global.GVA_DB = initialize.Gorm() // gorm连接数据库
 	initialize.Timer()

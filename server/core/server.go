ee388b01db3932c6d45d6de1ced0fec1763594a8
piexlmax <303176530@qq.com>
2022-09-25 13:35:11 +0800

    升级版本2.5.4

--- server/core/server.go
+++ server/core/server.go
@@ -37,7 +37,7 @@ func RunWindowsServer() {
 
 	fmt.Printf(`
 	欢迎使用 gin-vue-admin
-	当前版本:v2.5.3b
+	当前版本:v2.5.4
     加群方式:微信号：shouzi_1994 QQ群：622360840
 	插件市场:https://plugin.gin-vue-admin.com
 	GVA讨论社区:https://support.qq.com/products/371961

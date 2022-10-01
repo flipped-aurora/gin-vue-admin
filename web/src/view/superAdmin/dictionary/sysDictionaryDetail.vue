1cbbce741a58b02a2abc6ec9bc38c62aa904bcf9
piexlmax <303176530@qq.com>
2022-10-01 17:07:58 +0800

    调整字典管理路由监听模式

--- web/src/view/superAdmin/dictionary/sysDictionaryDetail.vue
+++ web/src/view/superAdmin/dictionary/sysDictionaryDetail.vue
@@ -127,15 +127,17 @@ import {
   findSysDictionaryDetail,
   getSysDictionaryDetailList
 } from '@/api/sysDictionaryDetail' // 此处请自行替换地址
-import { ref, watch } from 'vue'
-import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+import { useRoute, onBeforeRouteUpdate } from 'vue-router'
 import { ElMessage } from 'element-plus'
 import { formatBoolean, formatDate } from '@/utils/format'
 const route = useRoute()
 
-watch(() => route.params.id, (id) => {
-  searchInfo.value.sysDictionaryID = Number(id)
-  getTableData()
+onBeforeRouteUpdate((to, form) => {
+  if (to.name === 'dictionaryDetail') {
+    searchInfo.value.sysDictionaryID = to.params.id
+    getTableData()
+  }
 })
 
 const formData = ref({

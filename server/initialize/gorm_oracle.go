4c9fe65e67c824a03acfbb22a87ee850d190e28d
dzwvip <dzwvip@qq.com>
2022-10-16 21:10:03 +0800

    修改Oracle为mysql

--- server/initialize/gorm_oracle.go
+++ server/initialize/gorm_oracle.go
@@ -1,27 +1,29 @@
 package initialize
 
 import (
-	"github.com/dzwvip/oracle"
+	//"github.com/dzwvip/oracle"
 	"github.com/flipped-aurora/gin-vue-admin/server/config"
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/initialize/internal"
-	_ "github.com/godror/godror"
+
+	//_ "github.com/godror/godror"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 // GormOracle 初始化oracle数据库
-
+// 如果需要Oracle库 放开import里的注释 把下方 mysql.Config 改为 oracle.Config ;  mysql.New 改为 oracle.New
 func GormOracle() *gorm.DB {
 	m := global.GVA_CONFIG.Oracle
 	if m.Dbname == "" {
 		return nil
 	}
-	oracleConfig := oracle.Config{
+	oracleConfig := mysql.Config{
 		DSN:               m.Dsn(), // DSN data source name
 		DefaultStringSize: 191,     // string 类型字段的默认长度
 
 	}
-	if db, err := gorm.Open(oracle.New(oracleConfig), internal.Gorm.Config()); err != nil {
+	if db, err := gorm.Open(mysql.New(oracleConfig), internal.Gorm.Config()); err != nil {
 		panic(err)
 	} else {
 		sqlDB, _ := db.DB()
@@ -31,17 +33,17 @@ func GormOracle() *gorm.DB {
 	}
 }
 
-// GormOracleByConfig 初始化Mysql数据库用过传入配置
+// GormOracleByConfig 初始化Oracle数据库用过传入配置
 func GormOracleByConfig(m config.Oracle) *gorm.DB {
 	if m.Dbname == "" {
 		return nil
 	}
-	oracleConfig := oracle.Config{
+	oracleConfig := mysql.Config{
 		DSN:               m.Dsn(), // DSN data source name
 		DefaultStringSize: 191,     // string 类型字段的默认长度
 
 	}
-	if db, err := gorm.Open(oracle.New(oracleConfig), internal.Gorm.Config()); err != nil {
+	if db, err := gorm.Open(mysql.New(oracleConfig), internal.Gorm.Config()); err != nil {
 		panic(err)
 	} else {
 		sqlDB, _ := db.DB()

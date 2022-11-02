75c25857be2a11d970ab41ff53f1cf8c42207eb2
Del Xiong <pandaplot@qq.com>
2022-11-02 17:24:31 +0800

    代码自动创建配置参数AutoCode.Root改为配置文件值优先

--- server/core/viper.go
+++ server/core/viper.go
@@ -69,6 +69,8 @@ func Viper(path ...string) *viper.Viper {
 	}
 
 	// root 适配性 根据root位置去找到对应迁移位置,保证root路径有效
-	global.GVA_CONFIG.AutoCode.Root, _ = filepath.Abs("..")
+	if global.GVA_CONFIG.AutoCode.Root == "" {
+		global.GVA_CONFIG.AutoCode.Root, _ = filepath.Abs("..")
+	}
 	return v
 }

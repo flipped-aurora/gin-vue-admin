79fc9493214b1f3f1c175e406229b41a62cb2b23
吴璟 <hypf2008@126.com>
2022-11-03 18:00:45 +0800

    修改拦截器逻辑，develop模式不走数据库权限验证

--- server/middleware/casbin_rbac.go
+++ server/middleware/casbin_rbac.go
@@ -14,21 +14,25 @@ var casbinService = service.ServiceGroupApp.SystemServiceGroup.CasbinService
 // 拦截器
 func CasbinHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		waitUse, _ := utils.GetClaims(c)
-		// 获取请求的PATH
-		obj := c.Request.URL.Path
-		// 获取请求方法
-		act := c.Request.Method
-		// 获取用户的角色
-		sub := strconv.Itoa(int(waitUse.AuthorityId))
-		e := casbinService.Casbin() // 判断策略中是否存在
-		success, _ := e.Enforce(sub, obj, act)
-		if global.GVA_CONFIG.System.Env == "develop" || success {
+		if global.GVA_CONFIG.System.Env == "develop" {
 			c.Next()
 		} else {
-			response.FailWithDetailed(gin.H{}, "权限不足", c)
-			c.Abort()
-			return
+			waitUse, _ := utils.GetClaims(c)
+			//获取请求的PATH
+			obj := c.Request.URL.Path
+			// 获取请求方法
+			act := c.Request.Method
+			// 获取用户的角色
+			sub := strconv.Itoa(int(waitUse.AuthorityId))
+			e := casbinService.Casbin() // 判断策略中是否存在
+			success, _ := e.Enforce(sub, obj, act)
+			if success {
+				c.Next()
+			} else {
+				response.FailWithDetailed(gin.H{}, "权限不足", c)
+				c.Abort()
+				return
+			}
 		}
 	}
 }

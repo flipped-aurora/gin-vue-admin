2e6f99edf0ddf2237b66e95882c1606d22bfddf8
coke <20228139+wangxudong123@users.noreply.github.com>
2022-11-22 11:26:31 +0800

    修改获取角色列表没有检查err,优化查询逻辑 (#1238)
    Co-authored-by: wangxd01 <wangxd01@mingyuanyun.com>

--- server/service/system/sys_authority.go
+++ server/service/system/sys_authority.go
@@ -148,7 +148,9 @@ func (authorityService *AuthorityService) GetAuthorityInfoList(info request.Page
 	limit := info.PageSize
 	offset := info.PageSize * (info.Page - 1)
 	db := global.GVA_DB.Model(&system.SysAuthority{})
-	err = db.Where("parent_id = ?", "0").Count(&total).Error
+	if err = db.Where("parent_id = ?", "0").Count(&total).Error; total == 0 || err != nil {
+		return
+	}
 	var authority []system.SysAuthority
 	err = db.Limit(limit).Offset(offset).Preload("DataAuthorityId").Where("parent_id = ?", "0").Find(&authority).Error
 	if len(authority) > 0 {

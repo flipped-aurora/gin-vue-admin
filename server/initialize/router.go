b6f3038a73a7841e4516388145d932e65771153b
andywu1998 <1078539713@qq.com>
2022-11-30 16:52:00 +0800

    bugfix: 通过修改配置文件使得跨域白名单正常生效 (#1296)
    * bugfix: 通过修改配置文件使得跨域白名单正常生效
    
    Co-authored-by: 奇淼（piexlmax <qimiaojiangjizhao@gmail.com>

--- server/initialize/router.go
+++ server/initialize/router.go
@@ -31,7 +31,7 @@ func Routers() *gin.Engine {
 	// Router.Use(middleware.LoadTls())  // 如果需要使用https 请打开此中间件 然后前往 core/server.go 将启动模式 更变为 Router.RunTLS("端口","你的cre/pem文件","你的key文件")
 	// 跨域，如需跨域可以打开下面的注释
 	// Router.Use(middleware.Cors()) // 直接放行全部跨域请求
-	//Router.Use(middleware.CorsByRules()) // 按照配置的规则放行跨域请求
+	// Router.Use(middleware.CorsByRules()) // 按照配置的规则放行跨域请求
 	//global.GVA_LOG.Info("use middleware cors")
 	Router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	global.GVA_LOG.Info("register swagger handler")

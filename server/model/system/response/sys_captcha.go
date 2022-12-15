93d6375be399ac561d4588ee51441e54756c9729
Yexk <yexk@yexk.cn>
2022-12-15 17:40:30 +0800

    feat: #807 自定义验证码开关，可以自行配置

--- server/model/system/response/sys_captcha.go
+++ server/model/system/response/sys_captcha.go
@@ -4,4 +4,5 @@ type SysCaptchaResponse struct {
 	CaptchaId     string `json:"captchaId"`
 	PicPath       string `json:"picPath"`
 	CaptchaLength int    `json:"captchaLength"`
+	OpenCaptcha   bool   `json:"openCaptcha"`
 }

ee17da60f755a89906cc06c33296fa452a4e17ee
znonymous <fly1029@yeah.net>
2023-01-13 11:39:33 +0800

    feat: global prefix in plugin router

--- server/initialize/plugin.go
+++ server/initialize/plugin.go
@@ -2,6 +2,7 @@ package initialize
 
 import (
 	"fmt"
+
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/middleware"
 	"github.com/flipped-aurora/gin-vue-admin/server/plugin/email"
@@ -17,9 +18,9 @@ func PluginInit(group *gin.RouterGroup, Plugin ...plugin.Plugin) {
 }
 
 func InstallPlugin(Router *gin.Engine) {
-	PublicGroup := Router.Group("")
+	PublicGroup := Router.Group(global.GVA_CONFIG.System.RouterPrefix)
 	fmt.Println("无鉴权插件安装==》", PublicGroup)
-	PrivateGroup := Router.Group("")
+	PrivateGroup := Router.Group(global.GVA_CONFIG.System.RouterPrefix)
 	fmt.Println("鉴权插件安装==》", PrivateGroup)
 	PrivateGroup.Use(middleware.JWTAuth()).Use(middleware.CasbinHandler())
 	//  添加跟角色挂钩权限的插件 示例 本地示例模式于在线仓库模式注意上方的import 可以自行切换 效果相同

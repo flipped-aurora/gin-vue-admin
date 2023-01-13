97f91a28b76e759ced319a9709872c11251f2f4b
znonymous <fly1029@yeah.net>
2023-01-13 14:00:47 +0800

    feat: remove plugin router prefix and add web router setting

--- server/initialize/plugin.go
+++ server/initialize/plugin.go
@@ -18,9 +18,9 @@ func PluginInit(group *gin.RouterGroup, Plugin ...plugin.Plugin) {
 }
 
 func InstallPlugin(Router *gin.Engine) {
-	PublicGroup := Router.Group(global.GVA_CONFIG.System.RouterPrefix)
+	PublicGroup := Router.Group("")
 	fmt.Println("无鉴权插件安装==》", PublicGroup)
-	PrivateGroup := Router.Group(global.GVA_CONFIG.System.RouterPrefix)
+	PrivateGroup := Router.Group("")
 	fmt.Println("鉴权插件安装==》", PrivateGroup)
 	PrivateGroup.Use(middleware.JWTAuth()).Use(middleware.CasbinHandler())
 	//  添加跟角色挂钩权限的插件 示例 本地示例模式于在线仓库模式注意上方的import 可以自行切换 效果相同

5b86bdf75f13022fc2d48a366d1eb00fa58e3a11
TPEngineer <nvhashida@icloud.com>
2023-03-25 20:04:21 +0800

    Update sys_authority_btn.go (#1386)
    增加 err 错误处理

--- server/api/v1/system/sys_authority_btn.go
+++ server/api/v1/system/sys_authority_btn.go
@@ -22,6 +22,10 @@ type AuthorityBtnApi struct{}
 func (a *AuthorityBtnApi) GetAuthorityBtn(c *gin.Context) {
 	var req request.SysAuthorityBtnReq
 	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
 	res, err := authorityBtnService.GetAuthorityBtn(req)
 	if err != nil {
 		global.GVA_LOG.Error("查询失败!", zap.Error(err))

6e2ef1e29c5276f7374bb5ee3fffb45acb2fe209
奇淼（piexlmax <303176530@qq.com>
2023-04-08 16:07:22 +0800

    Fixed (#1412)
    * 菜单项如果有参数则携带参数跳转
    
    * 剔除search冗余代码
    
    ---------
    
    Co-authored-by: Rowen <wucenglou@qq.com>

--- web/src/view/layout/search/search.vue
+++ web/src/view/layout/search/search.vue
@@ -28,7 +28,6 @@ export default {
 import Screenfull from '@/view/layout/screenfull/index.vue'
 import { emitter } from '@/utils/bus.js'
 import { ref } from 'vue'
-
 const reload = ref(false)
 const handleReload = () => {
   reload.value = true
@@ -43,7 +42,7 @@ const toService = () => {
 
 </script>
 <style scoped lang="scss">
-.reload{
+.reload {
   font-size: 18px;
 }
 
@@ -52,16 +51,20 @@ const toService = () => {
   width: 160px;
   margin-right: 32px;
   text-align: center;
-  ::v-deep(.el-input__wrapper){
-    .el-input__inner{
+
+  ::v-deep(.el-input__wrapper) {
+    .el-input__inner {
       border-bottom: 1px solid var(--el-color-info-light-7);
     }
+
     box-shadow: none !important;
   }
-  ::v-deep(.el-select .el-input .el-input__wrapper.is-focus){
+
+  ::v-deep(.el-select .el-input .el-input__wrapper.is-focus) {
     box-shadow: none !important;
   }
-::v-deep(.el-select .el-input.is-focus .el-input__wrapper){
+
+  ::v-deep(.el-select .el-input.is-focus .el-input__wrapper) {
     box-shadow: none !important;
   }
 }
@@ -69,22 +72,38 @@ const toService = () => {
 .reloading{
   animation:turn 0.5s linear infinite;
 }
+
 @keyframes turn {
-  0%{transform:rotate(0deg);}
-  25%{transform:rotate(90deg);}
-  50%{transform:rotate(180deg);}
-  75%{transform:rotate(270deg);}
-  100%{transform:rotate(360deg);}
+  0% {
+    transform: rotate(0deg);
+  }
+
+  25% {
+    transform: rotate(90deg);
+  }
+
+  50% {
+    transform: rotate(180deg);
+  }
+
+  75% {
+    transform: rotate(270deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
 }
 
 .service {
-  font-family: "gvaIcon",serif !important;
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 800;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
+  font-family: "gvaIcon", serif !important;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 800;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
 //小屏幕不显示
 @media (max-width: 750px) {
   .service {

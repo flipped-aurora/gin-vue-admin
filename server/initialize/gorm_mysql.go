584a3ccf9b93bacbf20f912241256986a9293547
Kafumio <linzehong1_2020@qq.com>
2023-06-06 14:41:24 +0800

    add model about sqlite

--- server/initialize/gorm_mysql.go
+++ server/initialize/gorm_mysql.go
@@ -20,7 +20,6 @@ func GormMysql() *gorm.DB {
 		DSN:                       m.Dsn(), // DSN data source name
 		DefaultStringSize:         191,     // string 类型字段的默认长度
 		SkipInitializeWithVersion: false,   // 根据版本自动配置
-
 	}
 	if db, err := gorm.Open(mysql.New(mysqlConfig), internal.Gorm.Config(m.Prefix, m.Singular)); err != nil {
 		return nil

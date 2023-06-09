a9d0531a565a4a09906c614f182eb0d5e82c6f30
piexlMax <qimiaojiangjizhao@gmail.com>
2023-06-09 19:47:50 +0800

    调整代码自动定位无法识别windows的bug

--- web/vitePlugin/codeServer/index.js
+++ web/vitePlugin/codeServer/index.js
@@ -3,9 +3,7 @@ import * as dotenv from 'dotenv'
 import * as fs from 'fs'
 
 const NODE_ENV = process.env.NODE_ENV || 'development'
-const envFiles = [
-  `.env.${NODE_ENV}`
-]
+const envFiles = [`.env.${NODE_ENV}`]
 for (const file of envFiles) {
   const envConfig = dotenv.parse(fs.readFileSync(file))
   for (const k in envConfig) {
@@ -18,18 +16,23 @@ export default function GvaPositionServer() {
     name: 'gva-position-server',
     apply: 'serve',
     configureServer(server) {
-      server.middlewares.use((req, res, next) => {
+      server.middlewares.use((req, _, next) => {
         if (req._parsedUrl.pathname === '/gvaPositionCode') {
-          const path = req._parsedUrl.query && req._parsedUrl.query.split('=')[1]
+          const path =
+            req._parsedUrl.query && req._parsedUrl.query.split('=')[1]
           if (path && path !== 'null') {
             if (process.env.VITE_EDITOR === 'webstorm') {
               const linePath = path.split(':')[1]
               const filePath = path.split(':')[0]
               const platform = os()
               if (platform === 'win32') {
-                child_process.exec(`webstorm64.exe  --line ${linePath} ${filePath}`)
+                child_process.exec(
+                  `webstorm64.exe  --line ${linePath} ${filePath}`
+                )
               } else {
-                child_process.exec(`webstorm64  --line ${linePath} ${filePath}`)
+                child_process.exec(
+                  `webstorm64  --line ${linePath} ${filePath}`
+                )
               }
             } else {
               child_process.exec('code -r -g ' + path)
@@ -38,7 +41,7 @@ export default function GvaPositionServer() {
         }
         next()
       })
-    }
+    },
   }
 }
 
@@ -46,14 +49,5 @@ function os() {
   'use strict'
   const os = require('os')
   const platform = os.platform()
-  switch (platform) {
-    case 'darwin':
-      return 'MacOSX'
-    case 'linux':
-      return 'Linux'
-    case 'win32':
-      return 'Windows'
-    default:
-      return '无法确定操作系统!'
-  }
+  return platform
 }

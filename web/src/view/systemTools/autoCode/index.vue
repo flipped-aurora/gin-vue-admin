2d76d1f50c3f363f154b4a35e89f7e33f8a96c06
piexlMax <qimiaojiangjizhao@gmail.com>
2023-06-11 13:49:44 +0800

    自动化代码搜索条件修正

--- web/src/view/systemTools/autoCode/index.vue
+++ web/src/view/systemTools/autoCode/index.vue
@@ -269,7 +269,7 @@
     </div>
     <!-- 组件弹窗 -->
     <el-dialog v-model="dialogFlag" width="70%" title="组件内容">
-      <FieldDialog v-if="dialogFlag" ref="fieldDialogNode" :dialog-middle="dialogMiddle" :typeOptions="typeOptions"/>
+      <FieldDialog v-if="dialogFlag" ref="fieldDialogNode" :dialog-middle="dialogMiddle" :typeOptions="typeOptions" :typeSearchOptions="typeSearchOptions" />
       <template #footer>
         <div class="dialog-footer">
           <el-button @click="closeDialog">取 消</el-button>
@@ -286,7 +286,7 @@
           <el-button type="primary" @click="copy">复制</el-button>
         </div>
       </template>
-      <PreviewCodeDialog v-if="previewFlag" ref="previewNode" :typeSearchOptions="typeSearchOptions" :preview-code="preViewCode" />
+      <PreviewCodeDialog v-if="previewFlag" ref="previewNode" :preview-code="preViewCode" />
       <template #footer>
         <div class="dialog-footer" style="padding-top:14px;padding-right:14px">
           <el-button type="primary" @click="previewFlag = false">确 定</el-button>

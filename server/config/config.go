ee0705b2b78fff7b991307ade5aa6acdfe1ad85c
Stephen Macbook14 <stephen@macbook.com>
2023-07-13 16:50:32 +0800

    (chore):Stephen Changes

--- server/config/config.go
+++ server/config/config.go
@@ -2,6 +2,7 @@ package config
 
 type Server struct {
 	JWT     JWT     `mapstructure:"jwt" json:"jwt" yaml:"jwt"`
+	TLJWT   JWT     `mapstructure:"jwt" json:"tljwt" yaml:"tljwt"`
 	Zap     Zap     `mapstructure:"zap" json:"zap" yaml:"zap"`
 	Redis   Redis   `mapstructure:"redis" json:"redis" yaml:"redis"`
 	Email   Email   `mapstructure:"email" json:"email" yaml:"email"`

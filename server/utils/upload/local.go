2b6cb775d8747922e3180d6bcdceed7767a065d0
Di Sheng <nuodi@hotmail.com>
2023-07-14 21:22:10 +0800

    FIX: globally fix typo 'Field' to 'failed' (#1471)

--- server/utils/upload/local.go
+++ server/utils/upload/local.go
@@ -36,8 +36,8 @@ func (*Local) UploadFile(file *multipart.FileHeader) (string, string, error) {
 	// 尝试创建此路径
 	mkdirErr := os.MkdirAll(global.GVA_CONFIG.Local.StorePath, os.ModePerm)
 	if mkdirErr != nil {
-		global.GVA_LOG.Error("function os.MkdirAll() Filed", zap.Any("err", mkdirErr.Error()))
-		return "", "", errors.New("function os.MkdirAll() Filed, err:" + mkdirErr.Error())
+		global.GVA_LOG.Error("function os.MkdirAll() failed", zap.Any("err", mkdirErr.Error()))
+		return "", "", errors.New("function os.MkdirAll() failed, err:" + mkdirErr.Error())
 	}
 	// 拼接路径和文件名
 	p := global.GVA_CONFIG.Local.StorePath + "/" + filename
@@ -45,23 +45,23 @@ func (*Local) UploadFile(file *multipart.FileHeader) (string, string, error) {
 
 	f, openError := file.Open() // 读取文件
 	if openError != nil {
-		global.GVA_LOG.Error("function file.Open() Filed", zap.Any("err", openError.Error()))
-		return "", "", errors.New("function file.Open() Filed, err:" + openError.Error())
+		global.GVA_LOG.Error("function file.Open() failed", zap.Any("err", openError.Error()))
+		return "", "", errors.New("function file.Open() failed, err:" + openError.Error())
 	}
 	defer f.Close() // 创建文件 defer 关闭
 
 	out, createErr := os.Create(p)
 	if createErr != nil {
-		global.GVA_LOG.Error("function os.Create() Filed", zap.Any("err", createErr.Error()))
+		global.GVA_LOG.Error("function os.Create() failed", zap.Any("err", createErr.Error()))
 
-		return "", "", errors.New("function os.Create() Filed, err:" + createErr.Error())
+		return "", "", errors.New("function os.Create() failed, err:" + createErr.Error())
 	}
 	defer out.Close() // 创建文件 defer 关闭
 
 	_, copyErr := io.Copy(out, f) // 传输（拷贝）文件
 	if copyErr != nil {
-		global.GVA_LOG.Error("function io.Copy() Filed", zap.Any("err", copyErr.Error()))
-		return "", "", errors.New("function io.Copy() Filed, err:" + copyErr.Error())
+		global.GVA_LOG.Error("function io.Copy() failed", zap.Any("err", copyErr.Error()))
+		return "", "", errors.New("function io.Copy() failed, err:" + copyErr.Error())
 	}
 	return filepath, filename, nil
 }

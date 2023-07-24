6221acdc05529f3931b9976e8d09e6109d98c343
bypanghu <bypanghu@163.com>
2023-07-24 16:44:18 +0800

    fix: 修改登录，init 页面

--- web/src/view/layout/bottomInfo/bottomInfo.vue
+++ web/src/view/layout/bottomInfo/bottomInfo.vue
@@ -1,3 +1,5 @@
+<!-- 此文件禁止修改！如果您没有购买授权，请联系wx:shouzi_1994购买授权，未授权状态只需保留此代码 不影响任何正常使用 -->
+
 <template>
   <div class="flex gap-4 items-center text-sm text-gray-500 justify-center">
     <span>Powered by</span>
@@ -18,5 +20,14 @@
 export default {
   name: 'BottomInfo'
 }
+</script>
+
+<script  setup>
+console.log(
+  `%c powered by %c flipped-aurorae %c`,
+  'background:#0081ff; padding: 1px; border-radius: 3px 0 0 3px; color: #fff',
+  'background:#354855; padding: 1px 5px; border-radius: 0 3px 3px 0; color: #fff; font-weight: bold;',
+  'background:transparent'
+)
 </script>
 

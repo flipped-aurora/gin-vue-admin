3e31666cd659a40c54cd16af7bbb39322f45f9ac
zhanghaixin246 <31946229+zhanghaixin246@users.noreply.github.com>
2023-08-17 17:22:50 +0800

    feat:抛出上传文件错误信息 (#1515)

--- server/service/example/exa_file_upload_download.go
+++ server/service/example/exa_file_upload_download.go
@@ -92,7 +92,7 @@ func (e *FileUploadAndDownloadService) UploadFile(header *multipart.FileHeader, 
 	oss := upload.NewOss()
 	filePath, key, uploadErr := oss.UploadFile(header)
 	if uploadErr != nil {
-		panic(err)
+		panic(uploadErr)
 	}
 	s := strings.Split(header.Filename, ".")
 	f := example.ExaFileUploadAndDownload{

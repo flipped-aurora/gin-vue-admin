58fa81da2a4f9ed693b56ac79e68c1cc0313db96
andywu1998 <1078539713@qq.com>
2023-08-21 16:53:40 +0800

    fix(config)：修复内存对齐

--- server/config/db_list.go
+++ server/config/db_list.go
@@ -11,17 +11,17 @@ type DsnProvider interface {
 type GeneralDB struct {
 	Prefix       string `mapstructure:"prefix" json:"prefix" yaml:"prefix"`
 	Port         string `mapstructure:"port" json:"port" yaml:"port"`
-	Config       string `mapstructure:"config" json:"config" yaml:"config"`
-	Dbname       string `mapstructure:"db-name" json:"db-name" yaml:"db-name"`
-	Username     string `mapstructure:"username" json:"username" yaml:"username"`
-	Password     string `mapstructure:"password" json:"password" yaml:"password"`
+	Config       string `mapstructure:"config" json:"config" yaml:"config"`       // 高级配置
+	Dbname       string `mapstructure:"db-name" json:"db-name" yaml:"db-name"`    // 数据库名
+	Username     string `mapstructure:"username" json:"username" yaml:"username"` // 数据库密码
+	Password     string `mapstructure:"password" json:"password" yaml:"password"` // 数据库密码
 	Path         string `mapstructure:"path" json:"path" yaml:"path"`
-	Engine       string `mapstructure:"engine" json:"engine" yaml:"engine" default:"InnoDB"`
-	LogMode      string `mapstructure:"log-mode" json:"log-mode" yaml:"log-mode"`
-	MaxIdleConns int    `mapstructure:"max-idle-conns" json:"max-idle-conns" yaml:"max-idle-conns"`
-	MaxOpenConns int    `mapstructure:"max-open-conns" json:"max-open-conns" yaml:"max-open-conns"`
-	Singular     bool   `mapstructure:"singular" json:"singular" yaml:"singular"`
-	LogZap       bool   `mapstructure:"log-zap" json:"log-zap" yaml:"log-zap"`
+	Engine       string `mapstructure:"engine" json:"engine" yaml:"engine" default:"InnoDB"`        //数据库引擎，默认InnoDB
+	LogMode      string `mapstructure:"log-mode" json:"log-mode" yaml:"log-mode"`                   // 是否开启Gorm全局日志
+	MaxIdleConns int    `mapstructure:"max-idle-conns" json:"max-idle-conns" yaml:"max-idle-conns"` // 空闲中的最大连接数
+	MaxOpenConns int    `mapstructure:"max-open-conns" json:"max-open-conns" yaml:"max-open-conns"` // 打开到数据库的最大连接数
+	Singular     bool   `mapstructure:"singular" json:"singular" yaml:"singular"`                   //是否开启全局禁用复数，true表示开启
+	LogZap       bool   `mapstructure:"log-zap" json:"log-zap" yaml:"log-zap"`                      // 是否通过zap写入日志文件
 }
 
 type SpecializedDB struct {

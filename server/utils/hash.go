c4aeeb419ebe90c936ec1882f1d1bfce1e77b7e5
piexlMax <qimiaojiangjizhao@gmail.com>
2023-08-31 18:17:06 +0800

    [update] 清理了一些无用文件

--- server/utils/hash.go
+++ server/utils/hash.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -14,4 +16,16 @@ func BcryptHash(password string) string {
 func BcryptCheck(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+//@author: [piexlmax](https://github.com/piexlmax)
+//@function: MD5V
+//@description: md5加密
+//@param: str []byte
+//@return: string
+
+func MD5V(str []byte, b ...byte) string {
+	h := md5.New()
+	h.Write(str)
+	return hex.EncodeToString(h.Sum(b))
 }
--- web/src/utils/btnAuth.js
+++
@@ -1,6 +0,0 @@
-import { useRoute } from 'vue-router'
-import { reactive } from 'vue'
-export const useBtnAuth = () => {
-  const route = useRoute()
-  return route.meta.btns || reactive({})
-}

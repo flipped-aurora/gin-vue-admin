82df50f2f71707dfa8f72ab4dd23ef905100c687
wangzhiping <806454699@qq.com>
2023-09-13 19:58:38 +0800

    更新

--- server/core/server.go
+++ server/core/server.go
@@ -35,15 +35,6 @@ func RunWindowsServer() {
 	time.Sleep(10 * time.Microsecond)
 	global.GVA_LOG.Info("server run success on ", zap.String("address", address))
 
-	fmt.Printf(`
-	欢迎使用 gin-vue-admin
-	当前版本:v2.5.7
-    加群方式:微信号：shouzi_1994 QQ群：622360840
-	插件市场:https://plugin.gin-vue-admin.com
-	GVA讨论社区:https://support.qq.com/products/371961
-	默认自动化文档地址:http://127.0.0.1%s/swagger/index.html
-	默认前端文件运行地址:http://127.0.0.1:8080
-	如果项目让您获得了收益，希望您能请团队喝杯可乐:https://www.gin-vue-admin.com/coffee/index.html
-`, address)
+	fmt.Printf(`be服务启动成功`, address)
 	global.GVA_LOG.Error(s.ListenAndServe().Error())
 }

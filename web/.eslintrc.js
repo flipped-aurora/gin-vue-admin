636204bbaf7d77e7adadfef36af6e0c8386c986d
piexlMax <qimiaojiangjizhao@gmail.com>
2023-09-14 18:06:26 +0800

    修复eslint问题

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -20,16 +20,7 @@ module.exports = {
   },
   rules: {
     'vue/no-v-model-argument':'off',
-    'vue/max-attributes-per-line': [
-      2,
-      {
-        singleline: 10,
-        multiline: {
-          max: 1,
-          allowFirstLine: false
-        }
-      }
-    ],
+    'vue/max-attributes-per-line': 2,
     'vue/singleline-html-element-content-newline': 'off',
     'vue/multiline-html-element-content-newline': 'off',
     'vue/name-property-casing': ['error', 'PascalCase'],

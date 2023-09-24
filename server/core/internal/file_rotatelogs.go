2cb066982bf538258f3a490f13cc086dfcc32d32
SliverHorn <503551462@qq.com>
2023-09-24 14:35:54 +0800

    update: 去掉 github.com/lestrrat-go/file-rotatelogs 库的依赖, 实现zap.String("business", "xx")自定义业务日志数据路径, 输出文件路径为./global.GVA_CONFIG.Zap.Director/2006-01-02/xx/level(debug, info, warn, error, dpanic, panic, fatal).log (#1551)

--- server/core/internal/file_rotatelogs.go
+++ server/core/internal/file_rotatelogs.go
@@ -2,11 +2,8 @@ package internal
 
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
-	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 	"go.uber.org/zap/zapcore"
 	"os"
-	"path"
-	"time"
 )
 
 var FileRotatelogs = new(fileRotatelogs)
@@ -15,15 +12,10 @@ type fileRotatelogs struct{}
 
 // GetWriteSyncer 获取 zapcore.WriteSyncer
 // Author [SliverHorn](https://github.com/SliverHorn)
-func (r *fileRotatelogs) GetWriteSyncer(level string) (zapcore.WriteSyncer, error) {
-	fileWriter, err := rotatelogs.New(
-		path.Join(global.GVA_CONFIG.Zap.Director, "%Y-%m-%d", level+".log"),
-		rotatelogs.WithClock(rotatelogs.Local),
-		rotatelogs.WithMaxAge(time.Duration(global.GVA_CONFIG.Zap.MaxAge)*24*time.Hour), // 日志留存时间
-		rotatelogs.WithRotationTime(time.Hour*24),
-	)
+func (r *fileRotatelogs) GetWriteSyncer(level string) zapcore.WriteSyncer {
+	fileWriter := NewCutter(global.GVA_CONFIG.Zap.Director, level, WithCutterFormat("2006-01-02"))
 	if global.GVA_CONFIG.Zap.LogInConsole {
-		return zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(fileWriter)), err
+		return zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(fileWriter))
 	}
-	return zapcore.AddSync(fileWriter), err
+	return zapcore.AddSync(fileWriter)
 }

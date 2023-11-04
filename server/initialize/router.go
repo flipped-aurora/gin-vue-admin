3e5a26b95a4dc7c3a96f6f6abbc906b7e96e91bc
piexlMax <qimiaojiangjizhao@gmail.com>
2023-11-04 21:47:49 +0800

    修改开发环境的中间件

--- server/initialize/router.go
+++ server/initialize/router.go
@@ -15,13 +15,18 @@ import (
 // 初始化总路由
 
 func Routers() *gin.Engine {
-	
+
 	// 设置为发布模式
 	if global.GVA_CONFIG.System.Env == "public" {
 		gin.SetMode(gin.ReleaseMode) //DebugMode ReleaseMode TestMode
 	}
+
 	Router := gin.New()
-	
+
+	if global.GVA_CONFIG.System.Env != "public" {
+		Router.Use(gin.Logger(), gin.Recovery())
+	}
+
 	InstallPlugin(Router) // 安装插件
 	systemRouter := router.RouterGroupApp.System
 	exampleRouter := router.RouterGroupApp.Example

686ada1e4928984032fde44e571928e8a7e7eab9
piexlMax <qimiaojiangjizhao@gmail.com>
2024-01-08 19:54:54 +0800

    [fixed]:调整自动化表格TemplateInfo字段类型为text防止超长

--- server/model/system/sys_export_template.go
+++ server/model/system/sys_export_template.go
@@ -8,8 +8,8 @@ import (
 // 导出模板 结构体  SysExportTemplate
 type SysExportTemplate struct {
 	global.GVA_MODEL
-	Name         string `json:"name" form:"name" gorm:"column:name;comment:模板名称;"`                      //模板名称
-	TableName    string `json:"tableName" form:"tableName" gorm:"column:table_name;comment:表名称;"`       //表名称
-	TemplateID   string `json:"templateID" form:"templateID" gorm:"column:template_id;comment:模板标识;"`   //模板标识
-	TemplateInfo string `json:"templateInfo" form:"templateInfo" gorm:"column:template_info;comment:;"` //模板信息
+	Name         string `json:"name" form:"name" gorm:"column:name;comment:模板名称;"`                       //模板名称
+	TableName    string `json:"tableName" form:"tableName" gorm:"column:table_name;comment:表名称;"`        //表名称
+	TemplateID   string `json:"templateID" form:"templateID" gorm:"column:template_id;comment:模板标识;"`    //模板标识
+	TemplateInfo string `json:"templateInfo" form:"templateInfo" gorm:"column:template_info;type:text;"` //模板信息
 }

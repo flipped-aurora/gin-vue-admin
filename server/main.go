bd83cd326796cde9a6cb8044f9332a1863c8fb13
jiewangsu <40425187+jiewangsu@users.noreply.github.com>
2024-01-10 11:44:32 +0800

    Update main.go
    ini

--- server/main.go
+++ server/main.go
@@ -21,6 +21,7 @@ import (
 // @name                        x-token
 // @BasePath                    /
 func main() {
+	//11
 	global.GVA_VP = core.Viper() // 初始化Viper
 	initialize.OtherInit()
 	global.GVA_LOG = core.Zap() // 初始化zap日志库

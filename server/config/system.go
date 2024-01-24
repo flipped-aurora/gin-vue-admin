189219c673a90bd43d96b8797c2e7f51041c1b92
piexlmax（奇淼 <qimiaojiangjizhao@gmail.com>
2024-01-24 15:59:02 +0800

    typo修复

--- server/config/system.go
+++ server/config/system.go
@@ -10,5 +10,5 @@ type System struct {
 	LimitTimeIP   int    `mapstructure:"iplimit-time" json:"iplimit-time" yaml:"iplimit-time"`
 	UseMultipoint bool   `mapstructure:"use-multipoint" json:"use-multipoint" yaml:"use-multipoint"` // 多点登录拦截
 	UseRedis      bool   `mapstructure:"use-redis" json:"use-redis" yaml:"use-redis"`                // 使用redis
-	UseMongo      bool   `mapstructure:"use-mongo" json:"use-mongo" yaml:"use-mongo"`                // 使用redis
+	UseMongo      bool   `mapstructure:"use-mongo" json:"use-mongo" yaml:"use-mongo"`                // 使用mongo
 }

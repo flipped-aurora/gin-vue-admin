d6d6385b7f5131716fa350533977e3aa68dbc83e
YJDDB <djy1349678887@163.com>
2024-02-03 20:20:02 +0800

    Update:隐私保护

--- app/global/global.go
+++ app/global/global.go
@@ -1,14 +1,14 @@
 package global
 
 // mysql配置
-const DSN = "jk-competition:njhFLS3Bbe8AJZEs@tcp(123.207.73.185:3306)/jk-competition?charset=utf8mb4&parseTime=True&loc=Local"
+const DSN = ""
 
 // JWT配置
 const JWTKey = "ab_project"
 
 // 小程序配置
-const APPID = "wxa554745366849234"
-const SECRET = "3217677b2f40b5e4cb08f1117a9ded36"
+const APPID = ""
+const SECRET = ""
 
 // 用户身份
 const UserModelPuTong = 0     //普通用户

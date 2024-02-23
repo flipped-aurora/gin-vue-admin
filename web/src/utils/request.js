b64385037f51aacc179f4c3d6781b115242be36a
lushan <shan_lu@foxitsoftware.com>
2024-02-23 11:04:07 +0800

    fix the word spelling error for acitveAxios

--- web/src/utils/request.js
+++ web/src/utils/request.js
@@ -8,23 +8,23 @@ const service = axios.create({
   baseURL: import.meta.env.VITE_BASE_API,
   timeout: 99999
 })
-let acitveAxios = 0
+let activeAxios = 0
 let timer
 const showLoading = () => {
-  acitveAxios++
+  activeAxios++
   if (timer) {
     clearTimeout(timer)
   }
   timer = setTimeout(() => {
-    if (acitveAxios > 0) {
+    if (activeAxios > 0) {
       emitter.emit('showLoading')
     }
   }, 400)
 }
 
 const closeLoading = () => {
-  acitveAxios--
-  if (acitveAxios <= 0) {
+  activeAxios--
+  if (activeAxios <= 0) {
     clearTimeout(timer)
     emitter.emit('closeLoading')
   }

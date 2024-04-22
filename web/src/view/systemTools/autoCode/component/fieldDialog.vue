3b33fe83e6fa1b7ab81165fb51cbf6b2fb9c420f
pixelmaxQM <flipped_aurora@163.com>
2024-04-22 22:04:29 +0800

    feature:代码生成器可拖拽行，修复pgsql从库创建代码的bug，增加默认值属性。

--- web/src/view/systemTools/autoCode/component/fieldDialog.vue
+++ web/src/view/systemTools/autoCode/component/fieldDialog.vue
@@ -129,6 +129,12 @@
           />
         </el-select>
       </el-form-item>
+      <el-form-item label="默认值">
+        <el-input
+          v-model="middleDate.defaultValue"
+          placeholder="请输入默认值"
+        />
+      </el-form-item>
       <el-form-item label="主键">
         <el-checkbox v-model="middleDate.primaryKey" />
       </el-form-item>

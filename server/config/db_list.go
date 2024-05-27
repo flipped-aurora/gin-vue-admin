0a3a7d447dba49300c6ac56221177ca1023257d9
SliverHorn <caiwei.lai@jutze.com.cn>
2024-05-27 10:20:07 +0800

    Update: 1.config gorm相关移除DBBASE实现改为GeneralDB实现LogLevel函数 2.移除initializel/internal/logger实现,改为gorm_logger_writer实现gorm的logger通过zap输出到文件或者控制台 3.优化initializel/internal/gorm代码

--- server/config/db_list.go
+++ server/config/db_list.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"gorm.io/gorm/logger"
+	"strings"
+)
+
 type DsnProvider interface {
 	Dsn() string
 }
@@ -16,12 +21,27 @@ type GeneralDB struct {
 	Username     string `mapstructure:"username" json:"username" yaml:"username"` // 数据库密码
 	Password     string `mapstructure:"password" json:"password" yaml:"password"` // 数据库密码
 	Path         string `mapstructure:"path" json:"path" yaml:"path"`
-	Engine       string `mapstructure:"engine" json:"engine" yaml:"engine" default:"InnoDB"`        //数据库引擎，默认InnoDB
+	Engine       string `mapstructure:"engine" json:"engine" yaml:"engine" default:"InnoDB"`        // 数据库引擎，默认InnoDB
 	LogMode      string `mapstructure:"log-mode" json:"log-mode" yaml:"log-mode"`                   // 是否开启Gorm全局日志
 	MaxIdleConns int    `mapstructure:"max-idle-conns" json:"max-idle-conns" yaml:"max-idle-conns"` // 空闲中的最大连接数
 	MaxOpenConns int    `mapstructure:"max-open-conns" json:"max-open-conns" yaml:"max-open-conns"` // 打开到数据库的最大连接数
-	Singular     bool   `mapstructure:"singular" json:"singular" yaml:"singular"`                   //是否开启全局禁用复数，true表示开启
+	Singular     bool   `mapstructure:"singular" json:"singular" yaml:"singular"`                   // 是否开启全局禁用复数，true表示开启
 	LogZap       bool   `mapstructure:"log-zap" json:"log-zap" yaml:"log-zap"`                      // 是否通过zap写入日志文件
+}
+
+func (c GeneralDB) LogLevel() logger.LogLevel {
+	switch strings.ToLower(c.LogMode) {
+	case "silent", "Silent":
+		return logger.Silent
+	case "error", "Error":
+		return logger.Error
+	case "warn", "Warn":
+		return logger.Warn
+	case "info", "Info":
+		return logger.Info
+	default:
+		return logger.Info
+	}
 }
 
 type SpecializedDB struct {

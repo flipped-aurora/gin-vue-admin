e1983539fc59f48b5d6d9d016a563ab25b6ede98
xuedinge <781408517@qq.com>
2024-06-15 03:08:42 +0800

    小程序得到响应的时候需要data.data,加一个V2公共Response把所有放在同一层级

--- server/model/common/response/response.go
+++ server/model/common/response/response.go
@@ -2,6 +2,7 @@ package response
 
 import (
 	"net/http"
+	"reflect"
 
 	"github.com/gin-gonic/gin"
 )
@@ -60,4 +61,66 @@ func NoAuth(message string, c *gin.Context) {
 
 func FailWithDetailed(data interface{}, message string, c *gin.Context) {
 	Result(ERROR, data, message, c)
+}
+
+type ResponseV2 struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+}
+
+func ResultV2(code int, data interface{}, msg string, c *gin.Context) {
+	responseData := map[string]interface{}{
+		"code": code,
+		"msg":  msg,
+	}
+
+	// 反射遍历 data 并添加到 responseData 中
+	if data != nil {
+		val := reflect.ValueOf(data)
+		typ := reflect.TypeOf(data)
+
+		for i := 0; i < val.NumField(); i++ {
+			fieldName := typ.Field(i).Tag.Get("json")
+			if fieldName == "" {
+				fieldName = typ.Field(i).Name
+			}
+			responseData[fieldName] = val.Field(i).Interface()
+		}
+	}
+	c.JSON(http.StatusOK, responseData)
+}
+
+func OkV2(c *gin.Context) {
+	ResultV2(SUCCESS, map[string]interface{}{}, "操作成功", c)
+}
+
+func OkWithMessageV2(message string, c *gin.Context) {
+	ResultV2(SUCCESS, map[string]interface{}{}, message, c)
+}
+
+func OkWithDataV2(data interface{}, c *gin.Context) {
+	ResultV2(SUCCESS, data, "查询成功", c)
+}
+
+func OkWithDetailedV2(data interface{}, message string, c *gin.Context) {
+	ResultV2(SUCCESS, data, message, c)
+}
+
+func FailV2(c *gin.Context) {
+	ResultV2(ERROR, map[string]interface{}{}, "操作失败", c)
+}
+
+func FailWithMessageV2(message string, c *gin.Context) {
+	ResultV2(ERROR, map[string]interface{}{}, message, c)
+}
+
+func NoAuthV2(message string, c *gin.Context) {
+	c.JSON(http.StatusUnauthorized, ResponseV2{
+		Code: 7,
+		Msg:  message,
+	})
+}
+
+func FailWithDetailedV2(data interface{}, message string, c *gin.Context) {
+	ResultV2(ERROR, data, message, c)
 }

df9ca8bd0387ade04523c1db0c5330b42a77d75b
pixelMax(奇淼 <flipped_aurora@163.com>
2024-06-17 18:34:12 +0800

    feature:组合式菜单结构

--- web/src/view/layout/index.vue
+++ web/src/view/layout/index.vue
@@ -12,7 +12,7 @@
     />
     <gva-header />
     <div class="flex flex-row w-full gva-container pt-16">
-      <gva-aside v-if="config.side_mode === 'normal' || (device === 'mobile' && config.side_mode == 'head' ) || (device === 'mobile' && config.side_mode == 'combinationMode' )" />
+      <gva-aside v-if="config.side_mode === 'normal' || (device === 'mobile' && config.side_mode == 'head' ) || (device === 'mobile' && config.side_mode == 'combination' )" />
       <gva-aside v-if="config.side_mode === 'combination' && device !== 'mobile'" mode="normal"/>
       <div class="flex-1 p-2 w-0 h-full">
         <gva-tabs v-if="config.showTabs" />

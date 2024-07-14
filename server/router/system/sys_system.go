e325c840f7a8a9123f04924f21c59ed2e5ce84ec
SliverHorn <sliver_horn@qq.com>
2024-07-14 19:45:50 +0800

    fixed systemApi 重复声明

--- server/router/system/sys_system.go
+++ server/router/system/sys_system.go
@@ -11,7 +11,6 @@ func (s *SysRouter) InitSystemRouter(Router *gin.RouterGroup) {
 	sysRouter := Router.Group("system").Use(middleware.OperationRecord())
 	sysRouterWithoutRecord := Router.Group("system")
 
-	systemApi := v1.ApiGroupApp.SystemApiGroup.SystemApi
 	{
 		sysRouter.POST("setSystemConfig", systemApi.SetSystemConfig) // 设置配置文件内容
 		sysRouter.POST("reloadSystem", systemApi.ReloadSystem)       // 重启服务

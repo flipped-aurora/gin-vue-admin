8d6525306c22ff3a7818614ee8cf2c0ab3ee4fe9
SliverHorn <sliver_horn@qq.com>
2024-07-16 10:07:41 +0800

    update: config Autocode 新增Module字段以及如果为空的情况下自动获取运行目录下的go.mod文件

--- server/config/auto_code.go
+++ server/config/auto_code.go
@@ -9,6 +9,7 @@ type Autocode struct {
 	Web    string `mapstructure:"web" json:"web" yaml:"web"`
 	Root   string `mapstructure:"root" json:"root" yaml:"root"`
 	Server string `mapstructure:"server" json:"server" yaml:"server"`
+	Module string `mapstructure:"module" json:"module" yaml:"module"`
 	AiPath string `mapstructure:"ai-path" json:"ai-path" yaml:"ai-path"`
 }
 

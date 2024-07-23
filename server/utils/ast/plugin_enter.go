a561e7dc2bfded26e062027b6a971537791a4319
pixelMax(奇淼 <flipped_aurora@163.com>
2024-07-23 15:56:17 +0800

    feature: 修复自动化插件会创建两个var的bug

--- server/utils/ast/plugin_enter.go
+++ server/utils/ast/plugin_enter.go
@@ -2,7 +2,9 @@ package ast
 
 import (
 	"go/ast"
+	"go/token"
 	"io"
+	"log"
 )
 
 // PluginEnter 插件化入口
@@ -85,72 +87,75 @@ func (a *PluginEnter) Rollback(file *ast.File) error {
 
 func (a *PluginEnter) Injection(file *ast.File) error {
 	_ = NewImport(a.ImportPath).Injection(file)
-	for i := 0; i < len(file.Decls); i++ {
-		v1, o1 := file.Decls[i].(*ast.GenDecl)
-		if o1 {
-			for j := 0; j < len(v1.Specs); j++ {
-				v2, o2 := v1.Specs[j].(*ast.ValueSpec)
-				if o2 {
-					for k := 0; k < len(v2.Values); k++ {
-						v3, o3 := v2.Values[k].(*ast.CallExpr)
-						if o3 {
-							for l := 0; l < len(v3.Args); l++ {
-								v4, o4 := v3.Args[l].(*ast.Ident)
-								if o4 {
-									v5, o5 := v4.Obj.Decl.(*ast.TypeSpec)
-									if o5 {
-										if v5.Name.Name != a.Type.Group() {
-											continue
-										}
-										v6, o6 := v5.Type.(*ast.StructType)
-										if o6 {
-											var has bool
-											for m := 0; m < len(v6.Fields.List); m++ {
-												v7, o7 := v6.Fields.List[m].Type.(*ast.Ident)
-												if len(v6.Fields.List[m].Names) >= 1 && v6.Fields.List[m].Names[0].Name == a.StructName && o7 && v7.Name == a.StructCamelName {
-													has = true
-													continue
-												}
-											}
-											if !has {
-												field := &ast.Field{
-													Names: []*ast.Ident{{Name: a.StructName}},
-													Type:  &ast.Ident{Name: a.StructCamelName},
-												}
-												v6.Fields.List = append(v6.Fields.List, field)
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-					if a.Type == TypePluginServiceEnter {
-						continue
-					}
-					var has bool
-					if len(v2.Names) >= 1 && v2.Names[0].Name == a.ModuleName {
+
+	has := false
+	hasVar := false
+	var firstStruct *ast.StructType
+	var varSpec *ast.GenDecl
+	ast.Inspect(file, func(n ast.Node) bool {
+		switch x := n.(type) {
+		case *ast.TypeSpec:
+			if s, ok := x.Type.(*ast.StructType); ok {
+				firstStruct = s
+				for _, field := range x.Type.(*ast.StructType).Fields.List {
+					if len(field.Names) > 0 && field.Names[0].Name == a.StructName {
 						has = true
-					}
-					if !has {
-						spec := &ast.ValueSpec{
-							Names: []*ast.Ident{{Name: a.ModuleName}},
-							Values: []ast.Expr{
-								&ast.SelectorExpr{
-									X: &ast.SelectorExpr{
-										X:   &ast.Ident{Name: a.PackageName},
-										Sel: &ast.Ident{Name: a.GroupName},
-									},
-									Sel: &ast.Ident{Name: a.ServiceName},
-								},
-							},
-						}
-						v1.Specs = append(v1.Specs, spec)
+						return false
 					}
 				}
 			}
 		}
+		return true
+	})
+
+	if !has {
+		field := &ast.Field{
+			Names: []*ast.Ident{{Name: a.StructName}},
+			Type:  &ast.Ident{Name: a.StructCamelName},
+		}
+		firstStruct.Fields.List = append(firstStruct.Fields.List, field)
 	}
+
+	if a.Type == TypePluginServiceEnter {
+		return nil
+	}
+
+	ast.Inspect(file, func(n ast.Node) bool {
+		genDecl, ok := n.(*ast.GenDecl)
+		if ok && genDecl.Tok == token.VAR {
+			log.Println("Found var declaration block")
+			for _, spec := range genDecl.Specs {
+				valueSpec, vsok := spec.(*ast.ValueSpec)
+				if vsok {
+					varSpec = genDecl
+					for _, name := range valueSpec.Names {
+						if name.Name == a.ModuleName {
+							hasVar = true
+							return false
+						}
+					}
+				}
+			}
+		}
+		return true
+	})
+
+	if !hasVar {
+		spec := &ast.ValueSpec{
+			Names: []*ast.Ident{{Name: a.ModuleName}},
+			Values: []ast.Expr{
+				&ast.SelectorExpr{
+					X: &ast.SelectorExpr{
+						X:   &ast.Ident{Name: a.PackageName},
+						Sel: &ast.Ident{Name: a.GroupName},
+					},
+					Sel: &ast.Ident{Name: a.ServiceName},
+				},
+			},
+		}
+		varSpec.Specs = append(varSpec.Specs, spec)
+	}
+
 	return nil
 }
 

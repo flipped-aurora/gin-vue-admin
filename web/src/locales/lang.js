1a643b913e5c823fb91847b81e03ff319b360e2e
krank <emosick@qq.com>
2024-08-05 16:33:27 +0800

    feature： loading模块 i18n

---
+++ web/src/locales/lang.js
@@ -0,0 +1,11 @@
+const i18n = {
+    en:{
+        "sysLoading": "System is loading, please wait..."
+    },
+    zh:{
+        "sysLoading": "系统正在加载，请稍候..."
+    },
+    ar:{
+        "sysLoading": "جارٍ تحميل النظام، يرجى الانتظار"
+    }
+}

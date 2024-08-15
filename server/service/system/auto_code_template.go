c70fedfb434f63f283309c0ac0343fea2f32d016
pixelMax(奇淼 <flipped_aurora@163.com>
2024-08-15 18:05:43 +0800

    fixed: 修复addFunction下前端api.js无法创建的bug。

--- server/service/system/auto_code_template.go
+++ server/service/system/auto_code_template.go
@@ -325,7 +325,7 @@ func (s *autoCodeTemplate) addTemplateToFile(t string, info request.AutoFunc) er
 		case "server.go":
 			target = filepath.Join(global.GVA_CONFIG.AutoCode.Root, global.GVA_CONFIG.AutoCode.Server, "plugin", info.Package, "service", info.HumpPackageName+".go")
 		case "api.js":
-			target = filepath.Join(global.GVA_CONFIG.AutoCode.Root, global.GVA_CONFIG.AutoCode.Web, "plugin", info.Package, "api", info.HumpPackageName+".js")
+			target = filepath.Join(global.GVA_CONFIG.AutoCode.Root, global.GVA_CONFIG.AutoCode.Web, "plugin", info.Package, "api", info.PackageName+".js")
 		}
 	}
 

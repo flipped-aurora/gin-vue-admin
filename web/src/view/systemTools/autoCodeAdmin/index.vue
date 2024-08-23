27edfac161e2b3d3791ab6a71831c9e5a3752b17
chenlianghong <clh021@gmail.com>
2024-08-23 18:14:40 +0800

    fix: add funcName is sure UpperChar (#1860)
    * fix: add funcName is sure UpperChar

--- web/src/view/systemTools/autoCodeAdmin/index.vue
+++ web/src/view/systemTools/autoCodeAdmin/index.vue
@@ -206,6 +206,7 @@ import { useRouter } from "vue-router";
 import { ElMessage, ElMessageBox } from "element-plus";
 import { ref } from "vue";
 import { formatDate } from "@/utils/format";
+import { toUpperCase } from "@/utils/stringFun"
 
 defineOptions({
   name: "AutoCodeAdmin",
@@ -261,6 +262,9 @@ const closeFunc = () => {
 };
 
 const runFunc = async () =>{
+  // 首字母自动转换为大写
+  autoFunc.value.funcName = toUpperCase(autoFunc.value.funcName)
+
   const res = await addFunc(autoFunc.value)
   if (res.code === 0) {
     ElMessage.success("增加方法成功");

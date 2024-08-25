4643d90265d07604307ad497d0839c712fd34742
v_llxjliu <v_llxjliu@tencent.com>
2024-08-25 18:27:17 +0800

    cmd

--- server/testrunner/main.go
+++ server/testrunner/main.go
@@ -1,11 +1,33 @@
 package main
 
-import "github.com/flipped-aurora/gin-vue-admin/server/sdk/runner"
+import (
+	"bytes"
+	"fmt"
+	"github.com/flipped-aurora/gin-vue-admin/server/sdk/runner"
+	"os/exec"
+	"time"
+)
 
 func main() {
 	r := runner.New()
 	r.AddCmd("run_go_version", func(ctx *runner.Context) {
 		request := ctx.Request
+
+		now := time.Now()
+		cmd := exec.Command("go", "env")
+
+		var out bytes.Buffer
+		cmd.Stdout = &out
+
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		fmt.Println("Output:", out.String())
+		fmt.Println("cost:", time.Since(now))
+
 		ctx.Response(request + "-->" + "response")
 	})
 

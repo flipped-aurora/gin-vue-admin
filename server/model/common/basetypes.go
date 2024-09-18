0fd9e951b95f81e8bfd2180eccf546efb76a75ff
pixelmaxQM <flipped_aurora@163.com>
2024-09-18 22:48:41 +0800

    fixed: 对JSONMap进行多数据库不同断言兼容

--- server/model/common/basetypes.go
+++ server/model/common/basetypes.go
@@ -8,7 +8,6 @@ import (
 
 type JSONMap map[string]interface{}
 
-// 为了兼容无json类型的数据库实现JSONMap类型的Scan和Value方法
 func (m JSONMap) Value() (driver.Value, error) {
 	if m == nil {
 		return nil, nil
@@ -16,18 +15,20 @@ func (m JSONMap) Value() (driver.Value, error) {
 	return json.Marshal(m)
 }
 
-// Scan makes the JSONMap implement the sql.Scanner interface.
-// This method decodes a JSON-encoded value into the map structure.
 func (m *JSONMap) Scan(value interface{}) error {
 	if value == nil {
 		*m = make(map[string]interface{})
 		return nil
 	}
-	bytes, ok := value.([]byte)
-	if !ok {
-		return errors.New("Scan source was not []bytes")
+	var err error
+	switch value.(type) {
+	case []byte:
+		err = json.Unmarshal(value.([]byte), m)
+	case string:
+		err = json.Unmarshal([]byte(value.(string)), m)
+	default:
+		err = errors.New("basetypes.JSONMap.Scan: invalid value type")
 	}
-	err := json.Unmarshal(bytes, m)
 	if err != nil {
 		return err
 	}

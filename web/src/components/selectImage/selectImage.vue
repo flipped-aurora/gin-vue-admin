03330537fcbc8e1955f7b0e052b46a5f1412c3d0
pixelmaxQM <flipped_aurora@163.com>
2024-09-22 23:18:33 +0800

    feat: 优化图片选择器

--- web/src/components/selectImage/selectImage.vue
+++ web/src/components/selectImage/selectImage.vue
@@ -1,96 +1,96 @@
 <template>
   <div>
     <selectComponent
-      v-if="!multiple"
-      :model="model"
-      @chooseItem="openChooseImg"
-      @deleteItem="openChooseImg"
-    />
-    <div
-      v-else
-      class="w-full gap-4 flex flex-wrap"
-    >
-      <selectComponent
-        v-for="(item, index) in multipleValue"
-        :key="index"
-        :model="item"
-        @chooseItem="openChooseImg"
-        @deleteItem="deleteImg(index)"
-      />
-      <selectComponent
-        v-if="multipleValue.length < maxUpdateCount || maxUpdateCount === 0"
+        v-if="!props.multiple"
+        :model="model"
         @chooseItem="openChooseImg"
         @deleteItem="openChooseImg"
+    />
+    <div
+        v-else
+        class="w-full gap-4 flex flex-wrap"
+    >
+      <selectComponent
+          v-for="(item, index) in model"
+          :key="index"
+          :model="item"
+          @chooseItem="openChooseImg"
+          @deleteItem="deleteImg(index)"
+      />
+      <selectComponent
+          v-if="model.length < props.maxUpdateCount || props.maxUpdateCount === 0"
+          @chooseItem="openChooseImg"
+          @deleteItem="openChooseImg"
       />
     </div>
 
     <el-drawer
-      v-model="drawer"
-      title="媒体库"
-      size="650px"
+        v-model="drawer"
+        title="媒体库"
+        size="650px"
     >
       <warning-bar
-        title="点击“文件名/备注”可以编辑文件名或者备注内容。"
+          title="点击“文件名/备注”可以编辑文件名或者备注内容。"
       />
       <div class="gva-btn-list gap-2">
         <upload-common
-          :image-common="imageCommon"
-          @on-success="getImageList"
+            :image-common="imageCommon"
+            @on-success="getImageList"
         />
         <upload-image
-          :image-url="imageUrl"
-          :file-size="512"
-          :max-w-h="1080"
-          @on-success="getImageList"
+            :image-url="imageUrl"
+            :file-size="512"
+            :max-w-h="1080"
+            @on-success="getImageList"
         />
         <el-input
-          v-model="search.keyword"
-          class="keyword"
-          placeholder="请输入文件名或备注"
+            v-model="search.keyword"
+            class="keyword"
+            placeholder="请输入文件名或备注"
         />
         <el-button
-          type="primary"
-          icon="search"
-          @click="getImageList"
+            type="primary"
+            icon="search"
+            @click="getImageList"
         >
           查询
         </el-button>
       </div>
       <div class="flex flex-wrap gap-4">
         <div
-          v-for="(item,key) in picList"
-          :key="key"
-          class="w-40"
+            v-for="(item,key) in picList"
+            :key="key"
+            class="w-40"
         >
           <div class="w-40 h-40 border rounded overflow-hidden border-dashed border-gray-300 cursor-pointer relative group">
             <el-image
-              :key="key"
-              :src="getUrl(item.url)"
-              fit="cover"
-              class="w-full h-full relative"
-              @click="chooseImg(item.url)"
+                :key="key"
+                :src="getUrl(item.url)"
+                fit="cover"
+                class="w-full h-full relative"
+                @click="chooseImg(item.url)"
             >
               <template #error>
                 <el-icon
-                  v-if="isVideoExt(item.url || '')"
-                  :size="32"
-                  class="absolute top-[calc(50%-16px)] left-[calc(50%-16px)]"
+                    v-if="isVideoExt(item.url || '')"
+                    :size="32"
+                    class="absolute top-[calc(50%-16px)] left-[calc(50%-16px)]"
                 >
                   <VideoPlay />
                 </el-icon>
                 <video
-                  v-if="isVideoExt(item.url || '')"
-                  class="w-full h-full object-cover"
-                  muted
-                  preload="metadata"
-                  @click="chooseImg(item.url)"
+                    v-if="isVideoExt(item.url || '')"
+                    class="w-full h-full object-cover"
+                    muted
+                    preload="metadata"
+                    @click="chooseImg(item.url)"
                 >
                   <source :src="getUrl(item.url) + '#t=1'">
                   您的浏览器不支持视频播放
                 </video>
                 <div
-                  v-else
-                  class="w-full h-full object-cover flex items-center justify-center"
+                    v-else
+                    class="w-full h-full object-cover flex items-center justify-center"
                 >
                   <el-icon :size="32">
                     <icon-picture />
@@ -103,31 +103,30 @@
             </div>
           </div>
           <div
-            class="overflow-hidden text-nowrap overflow-ellipsis text-center w-full"
-            @click="editFileNameFunc(item)"
+              class="overflow-hidden text-nowrap overflow-ellipsis text-center w-full"
+              @click="editFileNameFunc(item)"
           >
             {{ item.name }}
           </div>
         </div>
       </div>
       <el-pagination
-        :current-page="page"
-        :page-size="pageSize"
-        :total="total"
-        :style="{'justify-content':'center'}"
-        layout="total, prev, pager, next, jumper"
-        @current-change="handleCurrentChange"
-        @size-change="handleSizeChange"
+          :current-page="page"
+          :page-size="pageSize"
+          :total="total"
+          :style="{'justify-content':'center'}"
+          layout="total, prev, pager, next, jumper"
+          @current-change="handleCurrentChange"
+          @size-change="handleSizeChange"
       />
     </el-drawer>
   </div>
 </template>
 
 <script setup>
-
 import { getUrl, isVideoExt } from '@/utils/image'
-import { onMounted, ref } from 'vue'
-import { getFileList, editFileName,deleteFile } from '@/api/fileUploadAndDownload'
+import { ref } from 'vue'
+import { getFileList, editFileName, deleteFile } from '@/api/fileUploadAndDownload'
 import UploadImage from '@/components/upload/image.vue'
 import UploadCommon from '@/components/upload/common.vue'
 import WarningBar from '@/components/warningBar/warningBar.vue'
@@ -159,19 +158,11 @@ const props = defineProps({
     default: 0
   }
 })
-const multipleValue = ref([])
 
-onMounted(() => {
-  if (props.multiple) {
-    multipleValue.value = model.value
-  }
-})
 const deleteImg = (index) => {
-  multipleValue.value.splice(index, 1)
-  model.value = multipleValue.value
+  model.value.splice(index, 1)
 }
 
-// 分页
 const handleSizeChange = (val) => {
   pageSize.value = val
   getImageList()
@@ -181,6 +172,7 @@ const handleCurrentChange = (val) => {
   page.value = val
   getImageList()
 }
+
 const editFileNameFunc = async(row) => {
   ElMessageBox.prompt('请输入文件名或者备注', '编辑', {
     confirmButtonText: '确定',
@@ -233,13 +225,13 @@ const chooseImg = (url) => {
     }
   }
   if (props.multiple) {
-    multipleValue.value.push(url)
-    model.value = multipleValue.value
+    model.value.push(url)
   } else {
     model.value = url
   }
   drawer.value = false
 }
+
 const openChooseImg = async() => {
   if (model.value && !props.multiple) {
     model.value = ''
@@ -259,7 +251,7 @@ const getImageList = async() => {
   }
 }
 
-const deleteCheck = (item)=>{
+const deleteCheck = (item) => {
   ElMessageBox.confirm('是否删除该文件', '提示', {
     confirmButtonText: '确定',
     cancelButtonText: '取消',
@@ -282,4 +274,3 @@ const deleteCheck = (item)=>{
 }
 
 </script>
-

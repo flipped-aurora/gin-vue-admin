762e117a71871f32e67a7a70cd2d0be187a23b63
Raindrop <h_villiger@163.com>
2024-11-08 23:10:50 +0800

    fix: eslint 配置更新, 适配低版本 node

---
+++ web/eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import globals from "globals"
+
+export default [
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: "module",
+      globals: globals.node
+    },
+    rules: {
+      "vue/max-attributes-per-line" : 0,
+      "vue/no-v-model-argument" : 0
+    },
+  },
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/build/*.js', '**/src/assets/**', '**/public/**'],
+  },
+]

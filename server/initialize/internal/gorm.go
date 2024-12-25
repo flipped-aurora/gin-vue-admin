8b93ede95975c1560a75e8d12ef90055c32b40e7
piexlMax(奇淼 <qimiaojiangjizhao@gmail.com>
2024-12-25 13:51:02 +0800

    fixed: 修复sql日志无法加入统一zap输出的bug

--- server/initialize/internal/gorm.go
+++ server/initialize/internal/gorm.go
@@ -6,8 +6,6 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
-	"log"
-	"os"
 	"time"
 )
 
@@ -34,7 +32,7 @@ func (g *_gorm) Config(prefix string, singular bool) *gorm.Config {
 		general = global.GVA_CONFIG.Mysql.GeneralDB
 	}
 	return &gorm.Config{
-		Logger: logger.New(NewWriter(general, log.New(os.Stdout, "\r\n", log.LstdFlags)), logger.Config{
+		Logger: logger.New(NewWriter(general), logger.Config{
 			SlowThreshold: 200 * time.Millisecond,
 			LogLevel:      general.LogLevel(),
 			Colorful:      true,

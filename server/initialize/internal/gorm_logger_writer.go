1743c763d2ea21fd6baed108ab64a1dd9ff32d30
piexlMax(奇淼 <qimiaojiangjizhao@gmail.com>
2024-12-31 10:48:50 +0800

    feat: gorm的debug日志无论是否开启了zap都需要输出到控制台

--- server/initialize/internal/gorm_logger_writer.go
+++ server/initialize/internal/gorm_logger_writer.go
@@ -18,6 +18,11 @@ func NewWriter(config config.GeneralDB) *Writer {
 
 // Printf 格式化打印日志
 func (c *Writer) Printf(message string, data ...any) {
+
+	// 当有日志时候均需要输出到控制台
+	fmt.Printf(message, data...)
+
+	// 当开启了zap的情况，会打印到日志记录
 	if c.config.LogZap {
 		switch c.config.LogLevel() {
 		case logger.Silent:

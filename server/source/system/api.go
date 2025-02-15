f2ea36ac0d004776fb9bf70f91425a5ededa48b1
pixelmaxQM <flipped_aurora@163.com>
2025-02-15 16:58:33 +0800

    feat: 调整初始化数据

--- server/source/system/api.go
+++ server/source/system/api.go
@@ -183,8 +183,8 @@ func (i *initApi) InitializeData(ctx context.Context) (context.Context, error) {
 		{ApiGroup: "参数管理", Method: "GET", Path: "/sysParams/getSysParamsList", Description: "获取参数列表"},
 		{ApiGroup: "参数管理", Method: "GET", Path: "/sysParams/getSysParam", Description: "获取参数列表"},
 		{ApiGroup: "媒体库分类", Method: "GET", Path: "/attachmentCategory/getCategoryList", Description: "分类列表"},
-		{ApiGroup: "媒体库分类", Method: "GET", Path: "/attachmentCategory/addCategory", Description: "添加/编辑分类"},
-		{ApiGroup: "媒体库分类", Method: "GET", Path: "/attachmentCategory/deleteCategory", Description: "删除分类"},
+		{ApiGroup: "媒体库分类", Method: "POST", Path: "/attachmentCategory/addCategory", Description: "添加/编辑分类"},
+		{ApiGroup: "媒体库分类", Method: "POST", Path: "/attachmentCategory/deleteCategory", Description: "删除分类"},
 	}
 	if err := db.Create(&entities).Error; err != nil {
 		return ctx, errors.Wrap(err, sysModel.SysApi{}.TableName()+"表数据初始化失败!")

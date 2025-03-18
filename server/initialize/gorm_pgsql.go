4964518486f812c46fe1c2bd7bf1b7a4c2865071
ByteZhou-2018 <1843607154@qq.com>
2025-03-18 21:57:51 +0800

    fix pgsql return error to panic

--- server/initialize/gorm_pgsql.go
+++ server/initialize/gorm_pgsql.go
@@ -31,7 +31,7 @@ func initPgSqlDatabase(p config.Pgsql) *gorm.DB {
 		PreferSimpleProtocol: false,
 	}
 	if db, err := gorm.Open(postgres.New(pgsqlConfig), internal.Gorm.Config(p.Prefix, p.Singular)); err != nil {
-		return nil
+		panic(err)
 	} else {
 		sqlDB, _ := db.DB()
 		sqlDB.SetMaxIdleConns(p.MaxIdleConns)

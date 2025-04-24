a0543c185c8cfb1edce866e1ef0b83aef2163725
huiyifyj <jxfengyijie@gmail.com>
2025-04-24 14:28:10 +0800

    refactor: improve timer initialization and execution flow
    Remove goroutine wrapper in `Timer()` function, and move it to run after database setup in `main()` function.

--- server/initialize/timer.go
+++ server/initialize/timer.go
@@ -2,36 +2,33 @@ package initialize
 
 import (
 	"fmt"
-	"github.com/flipped-aurora/gin-vue-admin/server/task"
-
-	"github.com/robfig/cron/v3"
 
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"github.com/flipped-aurora/gin-vue-admin/server/task"
+	"github.com/robfig/cron/v3"
 )
 
 func Timer() {
-	go func() {
-		var option []cron.Option
-		option = append(option, cron.WithSeconds())
-		// 清理DB定时任务
-		_, err := global.GVA_Timer.AddTaskByFunc("ClearDB", "@daily", func() {
-			err := task.ClearTable(global.GVA_DB) // 定时任务方法定在task文件包中
-			if err != nil {
-				fmt.Println("timer error:", err)
-			}
-		}, "定时清理数据库【日志，黑名单】内容", option...)
+	var option []cron.Option
+	option = append(option, cron.WithSeconds())
+	// 清理DB定时任务
+	_, err := global.GVA_Timer.AddTaskByFunc("ClearDB", "@daily", func() {
+		err := task.ClearTable(global.GVA_DB) // 定时任务方法定在task文件包中
 		if err != nil {
-			fmt.Println("add timer error:", err)
+			fmt.Println("timer error:", err)
 		}
+	}, "定时清理数据库【日志，黑名单】内容", option...)
+	if err != nil {
+		fmt.Println("add timer error:", err)
+	}
 
-		// 其他定时任务定在这里 参考上方使用方法
+	// 其他定时任务定在这里 参考上方使用方法
 
-		//_, err := global.GVA_Timer.AddTaskByFunc("定时任务标识", "corn表达式", func() {
-		//	具体执行内容...
-		//  ......
-		//}, option...)
-		//if err != nil {
-		//	fmt.Println("add timer error:", err)
-		//}
-	}()
+	//_, err := global.GVA_Timer.AddTaskByFunc("定时任务标识", "corn表达式", func() {
+	//	具体执行内容...
+	//  ......
+	//}, option...)
+	//if err != nil {
+	//	fmt.Println("add timer error:", err)
+	//}
 }
--- server/main.go
+++ server/main.go
@@ -33,7 +33,6 @@ func main() {
 	global.GVA_LOG = core.Zap() // 初始化zap日志库
 	zap.ReplaceGlobals(global.GVA_LOG)
 	global.GVA_DB = initialize.Gorm() // gorm连接数据库
-	initialize.Timer()
 	initialize.DBList()
 	if global.GVA_DB != nil {
 		initialize.RegisterTables() // 初始化表
@@ -41,5 +40,7 @@ func main() {
 		db, _ := global.GVA_DB.DB()
 		defer db.Close()
 	}
+
+	go initialize.Timer()
 	core.RunWindowsServer()
 }

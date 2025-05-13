39a67090e839aae200213050194843703f41506c
piexlMax(奇淼 <qimiaojiangjizhao@gmail.com>
2025-05-13 19:20:59 +0800

    feat: 发布2.8.2 Beta版本

--- server/docs/docs.go
+++ server/docs/docs.go
@@ -9296,7 +9296,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "v2.8.1",
+	Version:          "v2.8.2",
 	Host:             "",
 	BasePath:         "",
 	Schemes:          []string{},

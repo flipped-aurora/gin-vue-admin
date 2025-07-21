83518d0e5b6cc6f7d2c9ba41213690454aaa3af0
pixelmaxQM <flipped_aurora@163.com>
2025-07-21 23:02:59 +0800

    feat(menu): 版本更新为2.8.4,给菜单增加按钮和参数的预制打包

--- server/core/server.go
+++ server/core/server.go
@@ -35,7 +35,7 @@ func RunServer() {
 
 	fmt.Printf(`
 	欢迎使用 gin-vue-admin
-	当前版本:v2.8.3
+	当前版本:v2.8.4
 	加群方式:微信号：shouzi_1994 QQ群：470239250
 	项目地址：https://github.com/flipped-aurora/gin-vue-admin
 	插件市场:https://plugin.gin-vue-admin.com

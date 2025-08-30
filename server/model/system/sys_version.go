2e8f5870d80d21019331701ca81a1df425dfb110
pixelmaxQM <flipped_aurora@163.com>
2025-08-31 00:16:32 +0800

    refactor(model): 调整版本数据字段的数据库类型注释
    为VersionData字段添加postgres和sqlite数据库特定的类型注释，提高跨数据库兼容性

--- server/model/system/sys_version.go
+++ server/model/system/sys_version.go
@@ -8,10 +8,10 @@ import (
 // 版本管理 结构体  SysVersion
 type SysVersion struct {
 	global.GVA_MODEL
-	VersionName *string `json:"versionName" form:"versionName" gorm:"comment:版本名称;column:version_name;size:255;" binding:"required"` //版本名称
-	VersionCode *string `json:"versionCode" form:"versionCode" gorm:"comment:版本号;column:version_code;size:100;" binding:"required"`   //版本号
-	Description *string `json:"description" form:"description" gorm:"comment:版本描述;column:description;size:500;"`                     //版本描述
-	VersionData *string `json:"versionData" form:"versionData" gorm:"comment:版本数据JSON;column:version_data;type:longtext;"`               //版本数据
+	VersionName *string `json:"versionName" form:"versionName" gorm:"comment:版本名称;column:version_name;size:255;" binding:"required"`                           //版本名称
+	VersionCode *string `json:"versionCode" form:"versionCode" gorm:"comment:版本号;column:version_code;size:100;" binding:"required"`                            //版本号
+	Description *string `json:"description" form:"description" gorm:"comment:版本描述;column:description;size:500;"`                                               //版本描述
+	VersionData *string `json:"versionData" form:"versionData" gorm:"comment:版本数据JSON;column:version_data;type:longtext;postgres:type:text;sqlite:type:text;"` //版本数据
 }
 
 // TableName 版本管理 SysVersion自定义表名 sys_versions

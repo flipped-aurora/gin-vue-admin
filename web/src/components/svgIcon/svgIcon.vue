23e5d7405fb9baa0180153cf73ab2d979bffb99e
Azir-11 <2075125282@qq.com>
2025-09-29 16:54:51 +0800

    feat: 更新 SvgIcon 组件，支持本地图标和 Iconify 图标、移除未使用的 unocss 依赖

--- web/src/components/svgIcon/svgIcon.vue
+++ web/src/components/svgIcon/svgIcon.vue
@@ -1,32 +1,44 @@
 <template>
-  <svg :class="svgClass" v-bind="$attrs" :color="color">
-    <use :xlink:href="'#' + name" rel="external nofollow" />
-  </svg>
+  <template v-if="localIcon">
+    <svg aria-hidden="true" width="1em" height="1em" v-bind="bindAttrs">
+      <use :xlink:href="'#' + localIcon" rel="external nofollow" />
+    </svg>
+  </template>
+  <template v-else-if="icon">
+    <Icon :icon="icon" v-bind="bindAttrs" />
+  </template>
 </template>
-<script setup>
-  import { computed } from 'vue'
-  const props = defineProps({
-    name: {
-      type: String,
-      required: true
-    },
-    color: {
-      type: String,
-      default: 'currentColor'
-    }
-  })
 
-  const svgClass = computed(() => {
-    if (props.name) {
-      return `svg-icon ${props.name}`
-    }
-    return 'svg-icon'
-  })
-</script>
-<style scoped>
-  .svg-icon {
-    @apply w-4 h-4;
-    fill: currentColor;
-    vertical-align: middle;
+<script setup>
+import { computed, useAttrs } from 'vue';
+import { Icon } from '@iconify/vue'
+
+/**
+ * 使用示例：
+ * 本地图标（所有可用的本地图标见控制台输出）：
+ * <SvgIcon localIcon="lock" class="text-red-500 text-3xl" />
+ * 
+ * 在线图标（相关查询网站：https://icones.js.org/ 或是：https://icon-sets.iconify.design/）：
+ * <SvgIcon icon="mingcute:love-fill" class="text-red-500 text-3xl" />
+ */
+defineProps({
+  // 通过 symbol id 使用本地注册的 svg 图标
+  localIcon: {
+    type: String,
+    required: false,
+    default: ''
+  },
+  // Iconify 图标名称, 例如: 'mdi:home'
+  icon: {
+    type: String,
+    required: false,
+    default: ''
   }
-</style>
+})
+const attrs = useAttrs();
+
+const bindAttrs = computed(() => ({
+  class: (attrs.class) || '',
+  style: (attrs.style) || ''
+}))
+</script>

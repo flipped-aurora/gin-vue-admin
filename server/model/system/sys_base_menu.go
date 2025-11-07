0be88bb442ec10359d3e19a7b8e6ec221d0feba9
feitianbubu <feitianbubu@qq.com>
2025-11-07 14:39:52 +0800

    fix: repeat alter gorm autoMigrate due to incorrect comments

--- server/model/system/sys_base_menu.go
+++ server/model/system/sys_base_menu.go
@@ -13,7 +13,7 @@ type SysBaseMenu struct {
 	Hidden        bool                   `json:"hidden" gorm:"comment:是否在列表隐藏"`      // 是否在列表隐藏
 	Component     string                 `json:"component" gorm:"comment:对应前端文件路径"` // 对应前端文件路径
 	Sort          int                    `json:"sort" gorm:"comment:排序标记"`              // 排序标记
-	Meta          `json:"meta" gorm:"embedded;comment:附加属性"`                            // 附加属性
+	Meta          `json:"meta" gorm:"embedded"`                                             // 附加属性
 	SysAuthoritys []SysAuthority         `json:"authoritys" gorm:"many2many:sys_authority_menus;"`
 	Children      []SysBaseMenu          `json:"children" gorm:"-"`
 	Parameters    []SysBaseMenuParameter `json:"parameters"`

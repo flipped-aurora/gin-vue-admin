8cedb98c5b20f1ad37f5fdd9b0e2ede3216a0345
Azir-11 <2075125282@qq.com>
2025-12-16 13:07:17 +0800

    fix: 更新插件链接

--- web/src/view/dashboard/components/pluginTable.vue
+++ web/src/view/dashboard/components/pluginTable.vue
@@ -29,7 +29,7 @@
       title: '组织管理插件:更方便管理组织，分配资源权限。',
       click_num: 523,
       hot: 263,
-      link: 'https://plugin.gin-vue-admin.com/#/layout/newPluginInfo?id=36'
+      link: 'https://plugin.gin-vue-admin.com/details/36'
     },
     {
       ranking: 2,
@@ -37,7 +37,7 @@
         'Kubernetes容器管理:，Kubernetes 原生资源管理，提供炫酷的YAML 编辑，Pod 终端，方便运维兄弟管理k8s资源',
       click_num: 416,
       hot: 223,
-      link: 'https://plugin.gin-vue-admin.com/#/layout/newPluginInfo?id=42'
+      link: 'https://plugin.gin-vue-admin.com/details/42'
     },
     {
       ranking: 3,
@@ -45,7 +45,7 @@
         '定时任务配置化管理:本插件用于对系统内部的定时任务进行配置化管理，可以配置自定义的函数和HTTP，可以配置cron和remark等等',
       click_num: 337,
       hot: 176,
-      link: 'https://plugin.gin-vue-admin.com/#/layout/newPluginInfo?id=67'
+      link: 'https://plugin.gin-vue-admin.com/details/67'
     },
     {
       ranking: 4,
@@ -53,14 +53,14 @@
         '官网CMS系统：基于Gin-Vue-Admin 和 插件市场客户端开发基座开发的企业官网类（cms）系统',
       click_num: 292,
       hot: 145,
-      link: 'https://plugin.gin-vue-admin.com/#/layout/newPluginInfo?id=69'
+      link: 'https://plugin.gin-vue-admin.com/details/69'
     },
     {
       ranking: 5,
       title: '微信支付插件：提供扫码支付功能（需自行对接业务）',
       click_num: 173,
       hot: 110,
-      link: 'https://plugin.gin-vue-admin.com/#/layout/newPluginInfo?id=28'
+      link: 'https://plugin.gin-vue-admin.com/details/28'
     },
     {
       ranking: 6,

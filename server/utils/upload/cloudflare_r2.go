0bcccd93d11cbdb627849313a5d167e35b5c8036
pixelmaxQM <flipped_aurora@163.com>
2026-02-24 16:21:42 +0800

    feat: 更新 AWS S3 和 Cloudflare R2 上传逻辑，迁移至 aws-sdk-go-v2

--- server/utils/upload/cloudflare_r2.go
+++ server/utils/upload/cloudflare_r2.go
@@ -1,16 +1,17 @@
 package upload
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"mime/multipart"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"go.uber.org/zap"
 )
@@ -18,8 +19,8 @@ import (
 type CloudflareR2 struct{}
 
 func (c *CloudflareR2) UploadFile(file *multipart.FileHeader) (fileUrl string, fileName string, err error) {
-	session := c.newSession()
-	client := s3manager.NewUploader(session)
+	client := c.newR2Client()
+	uploader := manager.NewUploader(client)
 
 	fileKey := fmt.Sprintf("%d_%s", time.Now().Unix(), file.Filename)
 	fileName = fmt.Sprintf("%s/%s", global.GVA_CONFIG.CloudflareR2.Path, fileKey)
@@ -30,56 +31,55 @@ func (c *CloudflareR2) UploadFile(file *multipart.FileHeader) (fileUrl string, f
 	}
 	defer f.Close() // 创建文件 defer 关闭
 
-	input := &s3manager.UploadInput{
+	_, err = uploader.Upload(context.TODO(), &s3.PutObjectInput{
 		Bucket: aws.String(global.GVA_CONFIG.CloudflareR2.Bucket),
 		Key:    aws.String(fileName),
 		Body:   f,
-	}
-
-	_, err = client.Upload(input)
+	})
 	if err != nil {
 		global.GVA_LOG.Error("function uploader.Upload() failed", zap.Any("err", err.Error()))
 		return "", "", err
 	}
 
-	return fmt.Sprintf("%s/%s", global.GVA_CONFIG.CloudflareR2.BaseURL,
-			fileName),
-		fileKey,
-		nil
+	return fmt.Sprintf("%s/%s", global.GVA_CONFIG.CloudflareR2.BaseURL, fileName), fileKey, nil
 }
 
 func (c *CloudflareR2) DeleteFile(key string) error {
-	session := newSession()
-	svc := s3.New(session)
+	client := c.newR2Client()
 	filename := global.GVA_CONFIG.CloudflareR2.Path + "/" + key
 	bucket := global.GVA_CONFIG.CloudflareR2.Bucket
 
-	_, err := svc.DeleteObject(&s3.DeleteObjectInput{
+	_, err := client.DeleteObject(context.TODO(), &s3.DeleteObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(filename),
 	})
 	if err != nil {
-		global.GVA_LOG.Error("function svc.DeleteObject() failed", zap.Any("err", err.Error()))
-		return errors.New("function svc.DeleteObject() failed, err:" + err.Error())
+		global.GVA_LOG.Error("function client.DeleteObject() failed", zap.Any("err", err.Error()))
+		return errors.New("function client.DeleteObject() failed, err:" + err.Error())
 	}
 
-	_ = svc.WaitUntilObjectNotExists(&s3.HeadObjectInput{
+	waiter := s3.NewObjectNotExistsWaiter(client)
+	_ = waiter.Wait(context.TODO(), &s3.HeadObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(filename),
-	})
+	}, 30*time.Second)
+
 	return nil
 }
 
-func (*CloudflareR2) newSession() *session.Session {
-	endpoint := fmt.Sprintf("%s.r2.cloudflarestorage.com", global.GVA_CONFIG.CloudflareR2.AccountID)
+func (*CloudflareR2) newR2Client() *s3.Client {
+	endpoint := fmt.Sprintf("https://%s.r2.cloudflarestorage.com", global.GVA_CONFIG.CloudflareR2.AccountID)
 
-	return session.Must(session.NewSession(&aws.Config{
-		Region:   aws.String("auto"),
-		Endpoint: aws.String(endpoint),
-		Credentials: credentials.NewStaticCredentials(
+	cfg, _ := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("auto"),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(
 			global.GVA_CONFIG.CloudflareR2.AccessKeyID,
 			global.GVA_CONFIG.CloudflareR2.SecretAccessKey,
 			"",
-		),
-	}))
+		)),
+	)
+
+	return s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.BaseEndpoint = aws.String(endpoint)
+	})
 }

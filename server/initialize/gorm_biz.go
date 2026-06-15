bcfaff264879bddc50fd0d05666b99137f44a8c0
PiexlMax(奇淼 <flipped_aurora@163.com>
2026-06-15 10:29:30 +0800

    清理无用引入

--- server/initialize/gorm_biz.go
+++ server/initialize/gorm_biz.go
@@ -2,7 +2,6 @@ package initialize
 
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
-	"github.com/flipped-aurora/gin-vue-admin/server/model/example"
 )
 
 func bizModel() error {
